ff64d4b3d7c7edf2ce2451947221787cee6d68bf
The_Slayer <brock.kerley@hotmail.com>
2018-06-30 23:11:10 +0930

    Changed item registry

--- main/java/net/journey/proxy/CommonProxy.java
+++ main/java/net/journey/proxy/CommonProxy.java
@@ -60,6 +60,7 @@ public class CommonProxy {
 	public void preInit(FMLPreInitializationEvent event) {
 		Config.init(event);
 		JourneyBlocks.init();
+		JourneyItems.init();
 		EntityRegistry.init();
 		JourneyChestGenerator.init();
 		//JourneyAchievements.init();
