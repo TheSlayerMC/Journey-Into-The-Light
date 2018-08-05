0bd18cb5a634bf756fa3796f7b3b6be69766b7b6
The_Slayer <brock.kerley@hotmail.com>
2018-08-05 12:22:46 +0930

    more mob work

--- main/java/net/journey/entity/mob/boss/EntityBlazier.java
+++ main/java/net/journey/entity/mob/boss/EntityBlazier.java
@@ -5,12 +5,8 @@ import net.journey.JourneyBlocks;
 import net.journey.JourneyItems;
 import net.journey.blocks.tileentity.TileEntityJourneyChest;
 import net.journey.entity.MobStats;
-import net.journey.entity.projectile.EntityDeathSkull;
-import net.journey.entity.projectile.EntityLightningBall;
 import net.journey.entity.projectile.EntityMagmaFireball;
 import net.journey.enums.EnumSounds;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.IRangedAttackMob;
 import net.minecraft.entity.ai.EntityAIBase;
@@ -22,13 +18,19 @@ import net.minecraft.entity.ai.EntityAIWander;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.monster.EntityBlaze;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.util.DamageSource;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -39,11 +41,12 @@ public class EntityBlazier extends EntityEssenceBoss implements IRangedAttackMob
 	private float heightOffset = 0.5F;
 	private int heightOffsetUpdateTime;
 	private int spawnTimer;
+    private static final DataParameter<Byte> ON_FIRE = EntityDataManager.<Byte>createKey(EntityBlazier.class, DataSerializers.BYTE);
 
 	public EntityBlazier(World w) {
 		super(w);
 		this.experienceValue = 10;
-		this.tasks.addTask(1, new EntityAIArrowAttack(this, 1.0D, 40, 20.0F));
+		//this.tasks.addTask(1, new EntityAIArrowAttack(this, 1.0D, 40, 20.0F));
         this.tasks.addTask(4, new EntityBlazier.AIFireballAttack());
         this.tasks.addTask(5, new EntityAIMoveTowardsRestriction(this, 1.0D));
         this.tasks.addTask(7, new EntityAIWander(this, 1.0D));
@@ -59,17 +62,17 @@ public class EntityBlazier extends EntityEssenceBoss implements IRangedAttackMob
 	@Override
 	protected void entityInit() {
 		super.entityInit();
-		this.dataWatcher.addObject(16, new Byte((byte)0));
+        this.dataManager.register(ON_FIRE, Byte.valueOf((byte)0));
 	}
 
 	@Override
 	@SideOnly(Side.CLIENT)
-	public int getBrightnessForRender(float p_70070_1_) {
+	public int getBrightnessForRender() {
 		return 15728880;
 	}
 
 	@Override
-	public float getBrightness(float p_70013_1_) {
+	public float getBrightness() {
 		return 1.0F;
 	}
 
@@ -80,30 +83,30 @@ public class EntityBlazier extends EntityEssenceBoss implements IRangedAttackMob
             this.motionY *= 0.6D;
         }
 
-        if (this.worldObj.isRemote) {
+        if (this.world.isRemote) {
             if (this.rand.nextInt(24) == 0 && !this.isSilent()) {
-                this.worldObj.playSound(this.posX + 0.5D, this.posY + 0.5D, this.posZ + 0.5D, "fire.fire", 1.0F + this.rand.nextFloat(), this.rand.nextFloat() * 0.7F + 0.3F, false);
+                this.world.playSound(this.posX + 0.5D, this.posY + 0.5D, this.posZ + 0.5D, SoundEvents.BLOCK_FIRE_AMBIENT, SoundCategory.AMBIENT, 1.0F + this.rand.nextFloat(), this.rand.nextFloat() * 0.7F + 0.3F, false);
             }
 
             for (int i = 0; i < 2; ++i) {
-                this.worldObj.spawnParticle(EnumParticleTypes.SMOKE_LARGE, this.posX + (this.rand.nextDouble() - 0.5D) * this.width, this.posY + this.rand.nextDouble() * this.height, this.posZ + (this.rand.nextDouble() - 0.5D) * this.width, 0.0D, 0.0D, 0.0D, new int[0]);
+                this.world.spawnParticle(EnumParticleTypes.SMOKE_LARGE, this.posX + (this.rand.nextDouble() - 0.5D) * this.width, this.posY + this.rand.nextDouble() * this.height, this.posZ + (this.rand.nextDouble() - 0.5D) * this.width, 0.0D, 0.0D, 0.0D, new int[0]);
             }
         }
 
         if(getHealth() <= 250) {
-        	if(spawnTimer == 0 && !worldObj.isRemote) {
-    			EntityBlaze z = new EntityBlaze(worldObj);
+        	if(spawnTimer == 0 && !world.isRemote) {
+    			EntityBlaze z = new EntityBlaze(world);
                 z.setLocationAndAngles(posX + 3, posY, posZ, this.rand.nextFloat() * 360.0F, 0.0F);
-                EntityBlaze z1 = new EntityBlaze(worldObj);
+                EntityBlaze z1 = new EntityBlaze(world);
                 z1.setLocationAndAngles(posX - 3, posY, posZ, this.rand.nextFloat() * 360.0F, 0.0F);
-                EntityBlaze z2 = new EntityBlaze(worldObj);
+                EntityBlaze z2 = new EntityBlaze(world);
                 z2.setLocationAndAngles(posX, posY, posZ + 3, this.rand.nextFloat() * 360.0F, 0.0F);
-                EntityBlaze z3 = new EntityBlaze(worldObj);
+                EntityBlaze z3 = new EntityBlaze(world);
                 z3.setLocationAndAngles(posX, posY, posZ - 3, this.rand.nextFloat() * 360.0F, 0.0F);
-                this.worldObj.spawnEntityInWorld(z);
-                this.worldObj.spawnEntityInWorld(z1);
-                this.worldObj.spawnEntityInWorld(z2);
-                this.worldObj.spawnEntityInWorld(z3);
+                this.world.spawnEntity(z);
+                this.world.spawnEntity(z1);
+                this.world.spawnEntity(z2);
+                this.world.spawnEntity(z3);
                 spawnTimer = 200;
     		}
         	spawnTimer--;
@@ -114,7 +117,7 @@ public class EntityBlazier extends EntityEssenceBoss implements IRangedAttackMob
 	@Override
 	protected void updateAITasks() {
 		if (this.isWet()) {
-            this.attackEntityFrom(DamageSource.drown, 1.0F);
+            this.attackEntityFrom(DamageSource.DROWN, 1.0F);
         }
 
         --this.heightOffsetUpdateTime;
@@ -144,14 +147,14 @@ public class EntityBlazier extends EntityEssenceBoss implements IRangedAttackMob
 	
 	@Override
 	public void onDeath(DamageSource damage) {
-		if(damage.getEntity() instanceof EntityPlayer) {
+		/*if(damage.getEntity() instanceof EntityPlayer) {
 			EntityPlayer p = (EntityPlayer)damage.getEntity();
 			p.triggerAchievement(JourneyAchievements.achievementBlazier); {
 			}
-		}
-		this.worldObj.setBlockState(new BlockPos((int)Math.floor(this.posX + 0), ((int)Math.floor(this.posY + 1)), ((int)Math.floor(this.posZ + 0))), JourneyBlocks.trophyBlaze.getStateFromMeta(5));
-		this.worldObj.setBlockState(new BlockPos((int)Math.floor(this.posX + 0), ((int)Math.floor(this.posY + 0)), ((int)Math.floor(this.posZ + 0))), JourneyBlocks.boilingChest.getStateFromMeta(5));
-		TileEntityJourneyChest te = (TileEntityJourneyChest)worldObj.getTileEntity(new BlockPos((int)Math.floor(this.posX + 0), ((int)Math.floor(this.posY + 0)), ((int)Math.floor(this.posZ + 0))));
+		}*/
+		this.world.setBlockState(new BlockPos((int)Math.floor(this.posX + 0), ((int)Math.floor(this.posY + 1)), ((int)Math.floor(this.posZ + 0))), JourneyBlocks.trophyBlaze.getStateFromMeta(5));
+		this.world.setBlockState(new BlockPos((int)Math.floor(this.posX + 0), ((int)Math.floor(this.posY + 0)), ((int)Math.floor(this.posZ + 0))), JourneyBlocks.boilingChest.getStateFromMeta(5));
+		TileEntityJourneyChest te = (TileEntityJourneyChest)world.getTileEntity(new BlockPos((int)Math.floor(this.posX + 0), ((int)Math.floor(this.posY + 0)), ((int)Math.floor(this.posZ + 0))));
 		switch(rand.nextInt(2)) {
 		case 0:
 			te.setInventorySlotContents(2, new ItemStack(JourneyItems.blazingBow, 1));
@@ -179,14 +182,14 @@ public class EntityBlazier extends EntityEssenceBoss implements IRangedAttackMob
 	}
 
 	public boolean isFlying() {
-		return (this.dataWatcher.getWatchableObjectByte(16) & 1) != 0;
+        return (((Byte)this.dataManager.get(ON_FIRE)).byteValue() & 1) != 0;
 	}
 
 	public void setFlying(boolean b) {
-		byte b0 = this.dataWatcher.getWatchableObjectByte(16);
+        byte b0 = ((Byte)this.dataManager.get(ON_FIRE)).byteValue();
 		if(b) b0 = (byte)(b0 | 1);
 		else b0 &= -2;
-		this.dataWatcher.updateObject(16, Byte.valueOf(b0));
+        this.dataManager.set(ON_FIRE, Byte.valueOf(b0));
 	}
 
 	@Override
@@ -216,162 +219,137 @@ public class EntityBlazier extends EntityEssenceBoss implements IRangedAttackMob
 
 	@Override
 	public Item getItemDropped() {
-		return Items.blaze_rod;
+		return Items.BLAZE_ROD;
 	}
 	
-    class AIFireballAttack extends EntityAIBase
-    {
+    class AIFireballAttack extends EntityAIBase {
         private EntityBlazier field_179469_a = EntityBlazier.this;
         private int field_179467_b;
         private int field_179468_c;
 
-        public AIFireballAttack()
-        {
+        public AIFireballAttack() {
             this.setMutexBits(3);
         }
 
         @Override
-		public boolean shouldExecute()
-        {
+		public boolean shouldExecute() {
             EntityLivingBase entitylivingbase = this.field_179469_a.getAttackTarget();
             return entitylivingbase != null && entitylivingbase.isEntityAlive();
         }
 
         @Override
-		public void startExecuting()
-        {
+		public void startExecuting() {
             this.field_179467_b = 0;
         }
 
         @Override
-		public void resetTask()
-        {
+		public void resetTask() {
             this.field_179469_a.setFlying(false);
         }
 
         @Override
-		public void updateTask()
-        {
+		public void updateTask() {
             --this.field_179468_c;
             EntityLivingBase entitylivingbase = this.field_179469_a.getAttackTarget();
-            double d0 = this.field_179469_a.getDistanceSqToEntity(entitylivingbase);
+            double d0 = this.field_179469_a.getDistanceSq(entitylivingbase);
 
-            if (d0 < 4.0D)
-            {
-                if (this.field_179468_c <= 0)
-                {
+            if (d0 < 4.0D) {
+                if (this.field_179468_c <= 0) {
                     this.field_179468_c = 20;
                     this.field_179469_a.attackEntityAsMob(entitylivingbase);
                 }
 
                 this.field_179469_a.getMoveHelper().setMoveTo(entitylivingbase.posX, entitylivingbase.posY, entitylivingbase.posZ, 1.0D);
             }
-            else if (d0 < 256.0D)
-            {
+            else if (d0 < 256.0D) {
                 double d1 = entitylivingbase.posX - this.field_179469_a.posX;
                 double d2 = entitylivingbase.getEntityBoundingBox().minY + entitylivingbase.height / 2.0F - (this.field_179469_a.posY + this.field_179469_a.height / 2.0F);
                 double d3 = entitylivingbase.posZ - this.field_179469_a.posZ;
 
-                if (this.field_179468_c <= 0)
-                {
+                if (this.field_179468_c <= 0) {
                     ++this.field_179467_b;
 
-                    if (this.field_179467_b == 1)
-                    {
+                    if (this.field_179467_b == 1) {
                         this.field_179468_c = 60;
                         this.field_179469_a.setFlying(true);
                     }
-                    else if (this.field_179467_b <= 4)
-                    {
+                    else if (this.field_179467_b <= 4) {
                         this.field_179468_c = 6;
-                    }
-                    else
-                    {
+                    } else {
                         this.field_179468_c = 100;
                         this.field_179467_b = 0;
                         this.field_179469_a.setFlying(false);
                     }
 
-                    if (this.field_179467_b > 1)
-                    {
-                        float f = MathHelper.sqrt_float(MathHelper.sqrt_double(d0)) * 0.5F;
-                        this.field_179469_a.worldObj.playAuxSFXAtEntity((EntityPlayer)null, 1009, new BlockPos((int)this.field_179469_a.posX, (int)this.field_179469_a.posY, (int)this.field_179469_a.posZ), 0);
+                    if (this.field_179467_b > 1) {
+                        float f = MathHelper.sqrt(MathHelper.sqrt(d0)) * 0.5F;
+                        this.field_179469_a.world.playBroadcastSound(1009, new BlockPos((int)this.field_179469_a.posX, (int)this.field_179469_a.posY, (int)this.field_179469_a.posZ), 0);
 
-                        for (int i = 0; i < 1; ++i)
-                        {
-                            EntityMagmaFireball entitysmallfireball = new EntityMagmaFireball(this.field_179469_a.worldObj, this.field_179469_a, d1 + this.field_179469_a.getRNG().nextGaussian() * f, d2, d3 + this.field_179469_a.getRNG().nextGaussian() * f);
+                        for (int i = 0; i < 1; ++i) {
+                            EntityMagmaFireball entitysmallfireball = new EntityMagmaFireball(this.field_179469_a.world, this.field_179469_a, d1 + this.field_179469_a.getRNG().nextGaussian() * f, d2, d3 + this.field_179469_a.getRNG().nextGaussian() * f);
                             entitysmallfireball.posY = this.field_179469_a.posY + this.field_179469_a.height / 2.0F + 0.5D;
-                            this.field_179469_a.worldObj.spawnEntityInWorld(entitysmallfireball);
+                            this.field_179469_a.world.spawnEntity(entitysmallfireball);
                         }
                     }
                 }
-
                 this.field_179469_a.getLookHelper().setLookPositionWithEntity(entitylivingbase, 10.0F, 10.0F);
-            }
-            else
-            {
-                this.field_179469_a.getNavigator().clearPathEntity();
+            } else {
+                this.field_179469_a.getNavigator().clearPath();
                 this.field_179469_a.getMoveHelper().setMoveTo(entitylivingbase.posX, entitylivingbase.posY, entitylivingbase.posZ, 1.0D);
             }
 
             super.updateTask();
         }
     }
+    
     @Override
-	public void attackEntityWithRangedAttack(EntityLivingBase e, float f1)
-    {
+	public void attackEntityWithRangedAttack(EntityLivingBase e, float f1) {
         this.launchWitherSkullToEntity(0, e);
 	}
     
-    private void launchWitherSkullToEntity(int var1, EntityLivingBase e)
-    {
+    private void launchWitherSkullToEntity(int var1, EntityLivingBase e) {
         this.launchWitherSkullToCoords(var1, e.posX, e.posY + e.getEyeHeight() * 0.5D, e.posZ, var1 == 0 && this.rand.nextFloat() < 0.001F);
-        
     }
     
-    private void launchWitherSkullToCoords(int var1, double f2, double f4, double f6, boolean f8)
-    {
-        this.worldObj.playAuxSFXAtEntity((EntityPlayer)null, 1014, new BlockPos(this), 0);
+    private void launchWitherSkullToCoords(int var1, double f2, double f4, double f6, boolean f8) {
+        this.world.playBroadcastSound(1014, new BlockPos(this), 0);
         double d3 = this.coordX(var1);
         double d4 = this.coordY(var1);
         double d5 = this.coordZ(var1);
         double d6 = f2 - d3;
         double d7 = f4 - d4;
         double d8 = f6 - d5;
-        EntityMagmaFireball entitydeathskull = new EntityMagmaFireball(this.worldObj, this, d6, d7, d8);
+        EntityMagmaFireball entitydeathskull = new EntityMagmaFireball(this.world, this, d6, d7, d8);
         entitydeathskull.posY = d4;
         entitydeathskull.posX = d3;
         entitydeathskull.posZ = d5;
-        this.worldObj.spawnEntityInWorld(entitydeathskull);
+        this.world.spawnEntity(entitydeathskull);
 	}
     
     private double coordX(int par1) {
         if (par1 <= 0) {  
             return this.posX;
-        }
-        else {
+        } else {
             float f = (this.renderYawOffset + 180 * (par1 - 1)) / 180.0F * (float)Math.PI;
             float f1 = MathHelper.cos(f);
             return this.posX + f1 * 1.3D;
         }
     }
 
-    private double coordY(int par1)
-    {
+    private double coordY(int par1) {
         return par1 <= 0 ? this.posY + 3.0D : this.posY + 2.2D;
     }
 
-    private double coordZ(int par1)
-    {
-        if (par1 <= 0)
-        {
+    private double coordZ(int par1) {
+        if (par1 <= 0) {
             return this.posZ;
-        }
-        else
-        {
+        } else {
             float f = (this.renderYawOffset + 180 * (par1 - 1)) / 180.0F * (float)Math.PI;
             float f1 = MathHelper.sin(f);
             return this.posZ + f1 * 1.3D;
         }
     }
+    
+	@Override
+	public void setSwingingArms(boolean swingingArms) { }
 }
