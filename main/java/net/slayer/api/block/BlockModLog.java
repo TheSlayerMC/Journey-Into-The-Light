23b3f9bfddaa17a101e8c3836cb823e54e7fe24b
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2018-08-30 20:54:25 -0700

    Why the fuck is it so complicated to make a functioning log?

--- main/java/net/slayer/api/block/BlockModLog.java
+++ main/java/net/slayer/api/block/BlockModLog.java
@@ -1,90 +1,93 @@
 package net.slayer.api.block;
 
+import net.journey.JITL;
+import net.journey.JourneyBlocks;
+import net.journey.JourneyItems;
+import net.journey.JourneyTabs;
+import net.journey.client.IHasModel;
+import net.journey.util.LangRegistry;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockLog;
 import net.minecraft.block.SoundType;
-import net.minecraft.block.properties.PropertyEnum;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.IStringSerializable;
-import net.minecraft.util.Rotation;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockAccess;
-import net.minecraft.world.World;
-import net.slayer.api.EnumMaterialTypes;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.slayer.api.SlayerAPI;
 
-public class BlockModLog extends BlockModPillar {
+public class BlockModLog extends BlockLog implements IHasModel {
 
-	public static final PropertyEnum<EnumAxis> LOG_AXIS = PropertyEnum.<EnumAxis>create("axis", EnumAxis.class);
+	protected String name;	
+	protected boolean isOpaque = true, isNormalCube = true;
 
-	public BlockModLog(String name, String finalName) {
-		super(name, finalName);
-        setHardness(2.0F);
-		setSoundType(SoundType.WOOD);
+	public BlockModLog(String name, String finalname) {
+		super();
+		this.name = name;
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		LangRegistry.addBlock(name, finalname);
+		setCreativeTab(JourneyTabs.blocks);
+		this.setHardness(2);
+		this.setSoundType(SoundType.WOOD);
+		this.setDefaultState(this.getDefaultState().withProperty(LOG_AXIS, EnumAxis.Y));
+		JourneyItems.items.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
 	}
 
 	@Override
-	public boolean isWood(IBlockAccess world, BlockPos pos) {
-		return true;
+	public BlockStateContainer createBlockState() {
+		return new BlockStateContainer(this, LOG_AXIS);
 	}
 
 	@Override
-	public boolean canSustainLeaves(IBlockState state, IBlockAccess world, BlockPos pos) {
-		return true;
-	}
+	public IBlockState getStateFromMeta(int meta) {
+		IBlockState iblockstate = getDefaultState();
 
-	@Override
-	public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY,
-			float hitZ, int meta, EntityLivingBase placer) {
-		return this.getStateFromMeta(meta).withProperty(LOG_AXIS, EnumAxis.fromFacingAxis(facing.getAxis()));
-	}
-
-	@Override
-	public IBlockState withRotation(IBlockState state, Rotation rot) {
-		switch (rot) {
-		case COUNTERCLOCKWISE_90:
-		case CLOCKWISE_90:
-			switch ((EnumAxis) state.getValue(LOG_AXIS)) {
-			case X:
-				return state.withProperty(LOG_AXIS, EnumAxis.Z);
-			case Z:
-				return state.withProperty(LOG_AXIS, EnumAxis.X);
-			default:
-				return state;
-			}
+		switch (meta & 12) {
+		case 0:
+			iblockstate = iblockstate.withProperty(LOG_AXIS, BlockLog.EnumAxis.Y);
+			break;
+		case 4:
+			iblockstate = iblockstate.withProperty(LOG_AXIS, BlockLog.EnumAxis.X);
+			break;
+		case 8:
+			iblockstate = iblockstate.withProperty(LOG_AXIS, BlockLog.EnumAxis.Z);
+			break;
 		default:
-			return state;
+			iblockstate = iblockstate.withProperty(LOG_AXIS, BlockLog.EnumAxis.NONE);
 		}
+
+		return iblockstate;
 	}
-	
-	public static enum EnumAxis implements IStringSerializable {
-		X("x"), Y("y"), Z("z"), NONE("none");
 
-		private final String name;
+	@Override
+	@SuppressWarnings("incomplete-switch")
+	public int getMetaFromState(IBlockState state) {
+		int i = 0;
 
-		private EnumAxis(String name) {
-			this.name = name;
+		switch (state.getValue(LOG_AXIS)) {
+		case X:
+			i |= 4;
+			break;
+		case Z:
+			i |= 8;
+			break;
+		case NONE:
+			i |= 12;
 		}
 
-		public String toString() {
-			return this.name;
-		}
+		return i;
+	}
 
-		public static EnumAxis fromFacingAxis(EnumFacing.Axis axis) {
-			switch (axis) {
-			case X:
-				return X;
-			case Y:
-				return Y;
-			case Z:
-				return Z;
-			default:
-				return NONE;
-			}
-		}
+	public Block addName(String name) {
+		JourneyBlocks.blockName.add(SlayerAPI.PREFIX + name);
+		return this;
+	}
 
-		public String getName() {
-			return this.name;
-		}
+	public void registerItemModel(Item itemBlock) {
+		JITL.proxy.registerItemRenderer(itemBlock, 0, name);
+	}
+
+	public Item createItemBlock() {
+		return new ItemBlock(this).setRegistryName(getRegistryName());
 	}
 }
