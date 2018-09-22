26fe39e4c72f1eadf4fbceae317a567ab53471c6
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2018-09-21 19:03:52 -0700

    removed orientation from logs until a future update

--- main/java/net/slayer/api/block/BlockModLog.java
+++ main/java/net/slayer/api/block/BlockModLog.java
@@ -12,73 +12,24 @@ import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.slayer.api.EnumMaterialTypes;
 import net.slayer.api.SlayerAPI;
 
-public class BlockModLog extends BlockLog {
-	protected String name;
+public class BlockModLog extends BlockMod {
 
 	public BlockModLog(String name, String finalName) {
-		LangRegistry.addBlock(name, finalName);
-		this.name = name;
-		setUnlocalizedName(name);
-		setCreativeTab(JourneyTabs.blocks);
-		this.setHardness(2);
-		this.setSoundType(SoundType.WOOD);
-		this.setDefaultState(this.getDefaultState().withProperty(LOG_AXIS, EnumAxis.Y));
-		setRegistryName(SlayerAPI.MOD_ID, name);
-		JourneyBlocks.blocks.add(this);
+		super(EnumMaterialTypes.WOOD, name, finalName, 3.0F);
 	}
 
 	@Override
-	public BlockStateContainer createBlockState() {
-		return new BlockStateContainer(this, LOG_AXIS);
+	public boolean isWood(IBlockAccess world, BlockPos pos) {
+		return true;
 	}
 
 	@Override
-	public IBlockState getStateFromMeta(int meta) {
-		IBlockState iblockstate = getDefaultState();
-
-		switch (meta & 12) {
-		case 0:
-			iblockstate = iblockstate.withProperty(LOG_AXIS, BlockLog.EnumAxis.Y);
-			break;
-		case 4:
-			iblockstate = iblockstate.withProperty(LOG_AXIS, BlockLog.EnumAxis.X);
-			break;
-		case 8:
-			iblockstate = iblockstate.withProperty(LOG_AXIS, BlockLog.EnumAxis.Z);
-			break;
-		default:
-			iblockstate = iblockstate.withProperty(LOG_AXIS, BlockLog.EnumAxis.NONE);
-		}
-
-		return iblockstate;
-	}
-
-	@Override
-	@SuppressWarnings("incomplete-switch")
-	public int getMetaFromState(IBlockState state) {
-		int i = 0;
-
-		switch (state.getValue(LOG_AXIS)) {
-		case X:
-			i |= 4;
-			break;
-		case Z:
-			i |= 8;
-			break;
-		case NONE:
-			i |= 12;
-		}
-
-		return i;
-	}
-
-	public void registerItemModel(Item itemBlock) {
-		JITL.proxy.registerItemRenderer(itemBlock, 0, name);
-	}
-
-	public Item createItemBlock() {
-		return new ItemBlock(this).setRegistryName(getRegistryName());
+	public boolean canSustainLeaves(IBlockState state, IBlockAccess world, BlockPos pos) {
+		return true;
 	}
 }
