682157496dbbe4c0d4e2d53dc9e1d45e1bb91d0c
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2018-10-06 23:04:18 -0700

    idk

--- main/java/net/journey/items/ItemModBow.java
+++ main/java/net/journey/items/ItemModBow.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.annotation.Nullable;
 
+import net.journey.JITL;
 import net.journey.JourneyItems;
 import net.journey.JourneyTabs;
 import net.journey.client.ItemDescription;
@@ -20,12 +21,15 @@ import net.minecraft.init.Enchantments;
 import net.minecraft.init.Items;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.item.EnumAction;
+import net.minecraft.item.IItemPropertyGetter;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemArrow;
 import net.minecraft.item.ItemStack;
 import net.minecraft.stats.StatList;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
@@ -55,6 +59,24 @@ public class ItemModBow extends ItemMod {
 		this.setMaxDamage(uses);
 		this.setFull3D();
 		this.ability = ability;
+		this.addPropertyOverride(new ResourceLocation("pull"), new IItemPropertyGetter() {
+			@SideOnly(Side.CLIENT)
+			public float apply(ItemStack stack, @Nullable World worldIn, @Nullable EntityLivingBase entityIn) {
+				if (entityIn == null) {
+					return 0.0F;
+				} else {
+					return entityIn.getActiveItemStack().getItem() != Items.BOW ? 0.0F
+							: (float) (stack.getMaxItemUseDuration() - entityIn.getItemInUseCount()) / 20.0F;
+				}
+			}
+		});
+		this.addPropertyOverride(new ResourceLocation("pulling"), new IItemPropertyGetter() {
+			@SideOnly(Side.CLIENT)
+			public float apply(ItemStack stack, @Nullable World worldIn, @Nullable EntityLivingBase entityIn) {
+				return entityIn != null && entityIn.isHandActive() && entityIn.getActiveItemStack() == stack ? 1.0F
+						: 0.0F;
+			}
+		});
 	}
 	
 	public ItemModBow(String name, String f, int uses, int damage, /**String damageString*/ Item arrow, String ability, Class<? extends EntityArrow> arrowEnt) {
@@ -68,123 +90,136 @@ public class ItemModBow extends ItemMod {
 		//this.damageString = damageString;
 		this.setMaxDamage(uses);
 		this.setFull3D();
+		this.addPropertyOverride(new ResourceLocation("pull"), new IItemPropertyGetter() {
+			@SideOnly(Side.CLIENT)
+			public float apply(ItemStack stack, @Nullable World worldIn, @Nullable EntityLivingBase entityIn) {
+				if (entityIn == null) {
+					return 0.0F;
+				} else {
+					return entityIn.getActiveItemStack().getItem() != Items.BOW ? 0.0F
+							: (float) (stack.getMaxItemUseDuration() - entityIn.getItemInUseCount()) / 20.0F;
+				}
+			}
+		});
+		this.addPropertyOverride(new ResourceLocation("pulling"), new IItemPropertyGetter() {
+			@SideOnly(Side.CLIENT)
+			public float apply(ItemStack stack, @Nullable World worldIn, @Nullable EntityLivingBase entityIn) {
+				return entityIn != null && entityIn.isHandActive() && entityIn.getActiveItemStack() == stack ? 1.0F
+						: 0.0F;
+			}
+		});
 	}
 
-    private ItemStack findAmmo(EntityPlayer player) {
-        if (this.isArrow(player.getHeldItem(EnumHand.OFF_HAND))) {
-            return player.getHeldItem(EnumHand.OFF_HAND);
-        }
-        else if (this.isArrow(player.getHeldItem(EnumHand.MAIN_HAND))) {
-            return player.getHeldItem(EnumHand.MAIN_HAND);
-        }
-        else {
-            for (int i = 0; i < player.inventory.getSizeInventory(); ++i) {
-                ItemStack itemstack = player.inventory.getStackInSlot(i);
+	private ItemStack findAmmo(EntityPlayer player) {
+		if (this.isArrow(player.getHeldItem(EnumHand.OFF_HAND))) {
+			return player.getHeldItem(EnumHand.OFF_HAND);
+		} else if (this.isArrow(player.getHeldItem(EnumHand.MAIN_HAND))) {
+			return player.getHeldItem(EnumHand.MAIN_HAND);
+		} else {
+			for (int i = 0; i < player.inventory.getSizeInventory(); ++i) {
+				ItemStack itemstack = player.inventory.getStackInSlot(i);
 
-                if (this.isArrow(itemstack)) {
-                    return itemstack;
-                }
-            }
+				if (this.isArrow(itemstack)) {
+					return itemstack;
+				}
+			}
 
-            return ItemStack.EMPTY;
-        }
-    }
+			return ItemStack.EMPTY;
+		}
+	}
 
     protected boolean isArrow(ItemStack stack) {return stack.getItem() instanceof ItemEssenceArrow;}
     
-    @Override
+	@Override
 	public void onPlayerStoppedUsing(ItemStack stack, World worldIn, EntityLivingBase entityLiving, int timeLeft) {
-        if (entityLiving instanceof EntityPlayer) {
-            EntityPlayer entityplayer = (EntityPlayer)entityLiving;
-            boolean flag = entityplayer.capabilities.isCreativeMode || EnchantmentHelper.getEnchantmentLevel(Enchantments.INFINITY, stack) > 0;
-            ItemStack itemstack = this.findAmmo(entityplayer);
+		if (entityLiving instanceof EntityPlayer) {
+			EntityPlayer entityplayer = (EntityPlayer) entityLiving;
+			boolean flag = entityplayer.capabilities.isCreativeMode
+					|| EnchantmentHelper.getEnchantmentLevel(Enchantments.INFINITY, stack) > 0;
+			ItemStack itemstack = this.findAmmo(entityplayer);
+			ItemArrow itemarrow = (ItemArrow) (itemstack.getItem() instanceof ItemArrow ? itemstack.getItem()
+					: Items.ARROW);
+			EntityArrow entityarrow = itemarrow.createArrow(worldIn, itemstack, entityplayer);
+			boolean flag1 = entityplayer.capabilities.isCreativeMode || (itemstack.getItem() instanceof ItemArrow
+					&& ((ItemArrow) itemstack.getItem()).isInfinite(itemstack, stack, entityplayer));
 
-            int i = this.getMaxItemUseDuration(stack) - timeLeft;
-            i = net.minecraftforge.event.ForgeEventFactory.onArrowLoose(stack, worldIn, entityplayer, i, !itemstack.isEmpty() || flag);
-            if (i < 0) return;
+			int i = this.getMaxItemUseDuration(stack) - timeLeft;
+			i = net.minecraftforge.event.ForgeEventFactory.onArrowLoose(stack, worldIn, entityplayer, i,
+					!itemstack.isEmpty() || flag);
+			if (i < 0)
+				return;
 
-            if (!itemstack.isEmpty() || flag) {
-                if (itemstack.isEmpty()) {
-                    itemstack = new ItemStack(JourneyItems.essenceArrow);
-                }
+			float f = getArrowVelocity(i);
+			
+			if (!itemstack.isEmpty() || flag) {
+				if (itemstack.isEmpty()) {
+					itemstack = new ItemStack(arrowItem);
+				}
 
-                float f = getArrowVelocity(i);
+				if ((double) f >= 0.1D) {
 
-                if ((double)f >= 0.1D) {
-                    boolean flag1 = entityplayer.capabilities.isCreativeMode || (itemstack.getItem() instanceof ItemEssenceArrow && ((ItemEssenceArrow) itemstack.getItem()).isInfinite(itemstack, stack, entityplayer));
+					if (!worldIn.isRemote) {
+						
+						} else {
+							entityarrow.shoot(entityplayer, entityplayer.rotationPitch, entityplayer.rotationYaw, 0.0F,
+									f * 3.0F, 1.0F);
+						}
+						if (f == 1.0F) {
+						} else {
+							entityarrow.setIsCritical(true);
+						}
+					}
 
-                    if (!worldIn.isRemote) {
-                        ItemEssenceArrow itemarrow = (ItemEssenceArrow)(itemstack.getItem() instanceof ItemEssenceArrow ? itemstack.getItem() : JourneyItems.essenceArrow);
-                        EntityArrow arrow = itemarrow.createArrow(worldIn, itemstack, entityplayer);
-						try {
-							arrow = arrowClass.getConstructor(World.class, EntityLivingBase.class).newInstance(worldIn, entityplayer);
-						} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
-								| InvocationTargetException | NoSuchMethodException | SecurityException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}//itemarrow.createArrow(worldIn, itemstack, entityplayer);
-                        arrow.shoot(entityplayer, entityplayer.rotationPitch, entityplayer.rotationYaw, 0.0F, f * 3.0F, 1.0F);
+					int j = EnchantmentHelper.getEnchantmentLevel(Enchantments.POWER, stack);
 
-                        if (f == 1.0F){
-                        	arrow.setIsCritical(true);
-                        }
+					int k = EnchantmentHelper.getEnchantmentLevel(Enchantments.PUNCH, stack);
 
-                        int j = EnchantmentHelper.getEnchantmentLevel(Enchantments.POWER, stack);
+					if (k > 0) {
+						entityarrow.setKnockbackStrength(k);
+					}
 
-                        if (j > 0){
-                        	arrow.setDamage(arrow.getDamage() + (double)j * 0.5D + 0.5D);
-                        }
+					if (EnchantmentHelper.getEnchantmentLevel(Enchantments.FLAME, stack) > 0) {
+						entityarrow.setFire(100);
+					}
 
-                        int k = EnchantmentHelper.getEnchantmentLevel(Enchantments.PUNCH, stack);
+					stack.damageItem(1, entityplayer);
 
-                        if (k > 0){
-                        	arrow.setKnockbackStrength(k);
-                        }
+					if (flag1
+							|| entityplayer.capabilities.isCreativeMode && (itemstack.getItem() == Items.SPECTRAL_ARROW
+									|| itemstack.getItem() == Items.TIPPED_ARROW)) {
+						entityarrow.pickupStatus = EntityArrow.PickupStatus.CREATIVE_ONLY;
+					}
 
-                        if (EnchantmentHelper.getEnchantmentLevel(Enchantments.FLAME, stack) > 0){
-                        	arrow.setFire(100);
-                        }
+					worldIn.spawnEntity(entityarrow);
+				}
 
-                        stack.damageItem(1, entityplayer);
+				worldIn.playSound((EntityPlayer) null, entityplayer.posX, entityplayer.posY, entityplayer.posZ,
+						SoundEvents.ENTITY_ARROW_SHOOT, SoundCategory.PLAYERS, 1.0F,
+						1.0F / (itemRand.nextFloat() * 0.4F + 1.2F) + f * 0.5F);
 
-                        if (flag1 || entityplayer.capabilities.isCreativeMode && (itemstack.getItem() == Items.SPECTRAL_ARROW || itemstack.getItem() == Items.TIPPED_ARROW)){
-                        	arrow.pickupStatus = EntityArrow.PickupStatus.CREATIVE_ONLY;
-                        }
+				if (!flag1 && !entityplayer.capabilities.isCreativeMode) {
+					itemstack.shrink(1);
 
-                        worldIn.spawnEntity(arrow);
-                    }
+					if (itemstack.isEmpty()) {
+						entityplayer.inventory.deleteStack(itemstack);
+					}
+				}
 
-                    worldIn.playSound((EntityPlayer)null, entityplayer.posX, entityplayer.posY, entityplayer.posZ, SoundEvents.ENTITY_ARROW_SHOOT, SoundCategory.PLAYERS, 1.0F, 1.0F / (itemRand.nextFloat() * 0.4F + 1.2F) + f * 0.5F);
+				entityplayer.addStat(StatList.getObjectUseStats(this));
+			}
+		}
 
-                    if (!flag1 && !entityplayer.capabilities.isCreativeMode)
-                    {
-                        itemstack.shrink(1);
+	public static float getArrowVelocity(int charge) {
+		float f = (float) charge / 20.0F;
+		f = (f * f + f * 2.0F) / 2.0F;
 
-                        if (itemstack.isEmpty())
-                        {
-                            entityplayer.inventory.deleteStack(itemstack);
-                        }
-                    }
+		if (f > 1.0F) {
+			f = 5.0F;
+		}
 
-                    entityplayer.addStat(StatList.getObjectUseStats(this));
-                }
-            }
-        }
-    }
+		return f;
+	}
 
-    public static float getArrowVelocity(int charge)
-    {
-        float f = (float)charge / 20.0F;
-        f = (f * f + f * 2.0F) / 3.0F;
-
-        if (f > 1.0F)
-        {
-            f = 1.0F;
-        }
-
-        return f;
-    }
-    
 	/* @Override
 	public ItemStack onItemUseFinish(ItemStack stack, World worldIn, EntityPlayer playerIn) {
 		return stack;
@@ -210,24 +245,23 @@ public class ItemModBow extends ItemMod {
 	}
 
 	@Override
-    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
-    {
-        ItemStack itemstack = playerIn.getHeldItem(handIn);
-        boolean flag = !this.findAmmo(playerIn).isEmpty();
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
+		ItemStack itemstack = playerIn.getHeldItem(handIn);
+		boolean flag = !this.findAmmo(playerIn).isEmpty();
 
-        ActionResult<ItemStack> ret = net.minecraftforge.event.ForgeEventFactory.onArrowNock(itemstack, worldIn, playerIn, handIn, flag);
-        if (ret != null) return ret;
+		ActionResult<ItemStack> ret = net.minecraftforge.event.ForgeEventFactory.onArrowNock(itemstack, worldIn,
+				playerIn, handIn, flag);
+		if (ret != null)
+			return ret;
 
-        if (!playerIn.capabilities.isCreativeMode && !flag)
-        {
-            return flag ? new ActionResult(EnumActionResult.PASS, itemstack) : new ActionResult(EnumActionResult.FAIL, itemstack);
-        }
-        else
-        {
-            playerIn.setActiveHand(handIn);
-            return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, itemstack);
-        }
-    }
+		if (!playerIn.capabilities.isCreativeMode && !flag) {
+			return flag ? new ActionResult(EnumActionResult.PASS, itemstack)
+					: new ActionResult(EnumActionResult.FAIL, itemstack);
+		} else {
+			playerIn.setActiveHand(handIn);
+			return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, itemstack);
+		}
+	}
 
 	/*@Override
 	@SideOnly(Side.CLIENT)
@@ -244,5 +278,10 @@ public class ItemModBow extends ItemMod {
 	@Override
 	public int getItemEnchantability() {
 		return 1;
+	}
+	
+	@Override
+	public void registerItemModel() {
+		JITL.proxy.registerItemRenderer(this, 0, name);
 	}
 }
--- main/java/net/slayer/api/block/BlockModBush.java
+++ main/java/net/slayer/api/block/BlockModBush.java
@@ -25,6 +25,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.common.EnumPlantType;
+import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.common.IPlantable;
 import net.minecraftforge.common.util.FakePlayer;
 import net.minecraftforge.fml.relauncher.Side;
@@ -34,11 +35,11 @@ import net.slayer.api.EnumMaterialTypes;
 public class BlockModBush extends BlockMod implements IPlantable, IGrowable {
 
 	private boolean isNether;
-	private Item berry;
-	
+	private ItemStack berry;
+
 	public static final PropertyInteger AGE = PropertyInteger.create("age", 0, 2);
 
-	public BlockModBush(String name, String finalName, Item berry, boolean isNether) {
+	public BlockModBush(String name, String finalName, ItemStack berry, boolean isNether) {
 		super(EnumMaterialTypes.LEAVES, name, finalName, 1.0F);
 		this.berry = berry;
 		this.isNether = isNether;
@@ -48,11 +49,6 @@ public class BlockModBush extends BlockMod implements IPlantable, IGrowable {
 		this.setCreativeTab(JourneyTabs.crops);
 	}
 
-	@Override
-	public boolean isPassable(IBlockAccess worldIn, BlockPos pos) {
-		return false;
-	}
-	
 	@Override
 	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess access, BlockPos pos) {
 		float f = 0.3F;
@@ -69,71 +65,43 @@ public class BlockModBush extends BlockMod implements IPlantable, IGrowable {
 		}
 		return null;
 	}
-
+	
 	@Override
 	public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
 		return null;
 	}
-
+	
 	@Override
 	public boolean canPlaceBlockAt(World w, BlockPos pos) {
 		Block block = w.getBlockState(pos.down()).getBlock();
-		if(isNether) {
+		if (isNether) {
 			return block == Blocks.NETHERRACK;
 		}
-		if(!isNether) {
-			return block == Blocks.GRASS;
+		if (!isNether) {
+			return block == Blocks.GRASS || block == Blocks.DIRT;
 		}
 		return false;
-	}
-
-	@Override
-	public void updateTick(World w, BlockPos pos, IBlockState state, Random rand) {
-		if (w.rand.nextInt(5) == 0) {
-			int age = state.getValue(AGE).intValue();
-			if (age < 2) {
-				w.setBlockState(pos, state.withProperty(AGE, Integer.valueOf(age + 1)), 2);
-			}		            
-		}
-	}
-
-	@Override
-	public IBlockState getStateForPlacement(World w, BlockPos pos, EnumFacing face, float x, float y, float z, int meta, EntityLivingBase placer) {
-		return this.getDefaultState().withProperty(AGE, Integer.valueOf(0));
-
 	}
 
 	@Override
 	public IBlockState getStateFromMeta(int meta) {
-		return this.getDefaultState().withProperty(AGE, Integer.valueOf((meta & 15) >> 2));
+		return this.getDefaultState().withProperty(AGE, Integer.valueOf(meta));
 	}
 
 	@Override
 	public int getMetaFromState(IBlockState state) {
-		int i = 0;
-		i = i | state.getValue(AGE).intValue() << 2;
-		return i;
+		return state.getValue(AGE).intValue();
 	}
 
 	@Override
 	protected BlockStateContainer createBlockState() {
-		return new BlockStateContainer(this, new IProperty[] {AGE});
-	}
-
-	@Override
-	@SideOnly(Side.CLIENT)
-	public BlockRenderLayer getBlockLayer() {
-		return BlockRenderLayer.CUTOUT_MIPPED;
-	}
-
-	@Override
-	public boolean isOpaqueCube(IBlockState state) {
-		return false;
+		return new BlockStateContainer(this, new IProperty[] { AGE });
 	}
 	
+
 	@Override
 	public boolean canGrow(World worldIn, BlockPos pos, IBlockState state, boolean isClient) {
-		return state.getValue(AGE).intValue() < 2;
+		return true;
 	}
 
 	@Override
@@ -142,48 +110,118 @@ public class BlockModBush extends BlockMod implements IPlantable, IGrowable {
 	}
 
 	@Override
+	public EnumPlantType getPlantType(IBlockAccess world, BlockPos pos) {
+		return null;
+	}
+
+	@Override
+	public IBlockState getPlant(IBlockAccess world, BlockPos pos) {
+		return this.getDefaultState();
+	}
+	
+	@Override
+	public boolean isPassable(IBlockAccess worldIn, BlockPos pos) {
+		return false;
+	}
+
+	@Override
 	public void grow(World worldIn, Random rand, BlockPos pos, IBlockState state) {
 		worldIn.setBlockState(pos, state.withProperty(AGE, Integer.valueOf(state.getValue(AGE).intValue() + 1)), 2);
 	}
 
 	@Override
-	public EnumPlantType getPlantType(IBlockAccess world, BlockPos pos) {
-		return EnumPlantType.Plains;
+	public void updateTick(World w, BlockPos pos, IBlockState state, Random rand) {
+		super.updateTick(w, pos, state, rand);
+		if (w.rand.nextInt(5) == 0) {
+			int age = state.getValue(AGE).intValue();
+			if (age < 2) {
+				w.setBlockState(pos, state.withProperty(AGE, Integer.valueOf(age + 1)), 2);
+			}	
+		}
 	}
 
-    @Override
-    public IBlockState getPlant(IBlockAccess world, BlockPos pos) {
-        IBlockState state = world.getBlockState(pos);
-        if (state.getBlock() != this) {
-            return this.getDefaultState();
-        }
-        return state;
-    }
+	@Override
+	public boolean canSustainPlant(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing direction, IPlantable plantable) {
+		if (plantable instanceof BlockModBush && state.getPropertyKeys().contains(AGE)) {
+			return (state.getValue(AGE) > 2);
+		}
 
-	public boolean canBlockStay(World world, BlockPos pos1) {
-		return this.canPlaceBlockAt(world, pos1);
+		return super.canSustainPlant(state, world, pos, direction, plantable);
 	}
-	
-    @Override
-    public int damageDropped(IBlockState state) {
-        return 0;
-    }
+
+	@Override
+	public boolean isFullCube(IBlockState state) {
+		int age = state.getValue(AGE).intValue();
+
+		if (age == 0) {
+			return false;
+		}
+		if (age == 1) {
+			return false;
+		}
+		if (age == 2) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public boolean isOpaqueCube(IBlockState state) {
+		return false;
+	}
     
 	@Override
-	public void harvestBlock(World w, EntityPlayer player, BlockPos pos, IBlockState state, TileEntity te,
-			ItemStack stackIn) {
-		int min = 1, max = 2;
-		
-		if (state.getValue(AGE) == 2 && !w.isRemote) {
-			int rand = w.rand.nextInt(max - min + 1) + min;
-			
-			player.addStat(StatList.getBlockStats(this), 1);
-			player.addExhaustion(0.025F);
-			w.setBlockState(pos, state.withProperty(AGE, 0), 1);
+	public int damageDropped(IBlockState state) {
+		return 0;
+	}
 
-			if (rand != 0) {
-				spawnAsEntity(w, pos, new ItemStack(berry, rand, 0));
+	@Override
+	public void onBlockClicked(World worldIn, BlockPos pos, EntityPlayer playerIn) {
+		if (worldIn.isRemote) {
+			return;
+		}
+
+		int age = worldIn.getBlockState(pos).getValue(AGE).intValue();
+
+		if (age == 2) {
+			worldIn.setBlockState(pos, this.getDefaultState().withProperty(AGE, Integer.valueOf(2)), 2);
+
+			ItemStack itemDrop = new ItemStack(this.berry.getItem(), 1, this.berry.getItemDamage());
+			EntityItem entityitem = new EntityItem(worldIn, playerIn.posX, playerIn.posY - 1.0D, playerIn.posZ,
+					itemDrop);
+
+			worldIn.spawnEntity(entityitem);
+
+			if (!(playerIn instanceof FakePlayer)) {
+				entityitem.onCollideWithPlayer(playerIn);
 			}
 		}
+	}
+
+	@Override
+	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn,
+			EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ) {
+		int age = state.getValue(AGE).intValue();
+
+		if (age == 2) {
+			if (worldIn.isRemote) {
+				return true;
+			}
+
+			worldIn.setBlockState(pos, this.getDefaultState().withProperty(AGE, Integer.valueOf(2)), 2);
+
+			ItemStack itemDrop = new ItemStack(this.berry.getItem(), 1, this.berry.getItemDamage());
+			EntityItem entityitem = new EntityItem(worldIn, playerIn.posX, playerIn.posY - 1.0D, playerIn.posZ,
+					itemDrop);
+
+			worldIn.spawnEntity(entityitem);
+
+			if (!(playerIn instanceof FakePlayer)) {
+				entityitem.onCollideWithPlayer(playerIn);
+			}
+			return true;
+		}
+		return false;
 	}
 }
