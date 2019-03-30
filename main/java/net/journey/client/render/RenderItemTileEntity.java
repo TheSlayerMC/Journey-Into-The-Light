36564288b26fe53ccd2c9bebd83fc0c79e9fab3a
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2019-03-29 17:10:16 -0700

    TileEntity item rendering started

---
+++ main/java/net/journey/client/render/RenderItemTileEntity.java
@@ -0,0 +1,28 @@
+package net.journey.client.render;
+
+import net.journey.blocks.tileentity.TileEntityJourneyChest;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockChest;
+import net.minecraft.client.renderer.tileentity.TileEntityItemStackRenderer;
+import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+public class RenderItemTileEntity extends TileEntityItemStackRenderer {
+	
+    private static final TileEntityJourneyChest JOURNEY_CHEST = new TileEntityJourneyChest();
+    
+    public static RenderItemTileEntity instance = new RenderItemTileEntity();
+
+	@Override
+	public void renderByItem(ItemStack itemStackIn) {
+		Item item = itemStackIn.getItem();
+		if (Block.getBlockFromItem(item) instanceof BlockChest) {
+			{
+				TileEntityRendererDispatcher.instance.render(JOURNEY_CHEST, 0, 0);
+			}
+		} else {
+			super.renderByItem(itemStackIn);
+		}
+	}
+}
