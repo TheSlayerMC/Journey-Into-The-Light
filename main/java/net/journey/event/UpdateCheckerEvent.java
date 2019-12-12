6e4e0689172811c68fe3d3b9453fb2cb01ef95fc
The_Slayer <brock.kerley@hotmail.com>
2019-12-12 19:21:18 +1030

    update  checker

--- main/java/net/journey/event/UpdateCheckerEvent.java
+++ main/java/net/journey/event/UpdateCheckerEvent.java
@@ -33,15 +33,14 @@ public class UpdateCheckerEvent {
 							SlayerAPI.addChatMessage(p, Colour.GOLD + "[|--------------------------------------------------|]"); 
 						}
 						if(UpdateChecker.isUpdateAvailable() && UpdateChecker.isOnline()) {
-							BufferedReader versionFile = new BufferedReader(new InputStreamReader(new URL("https://raw.githubusercontent.com/TheSlayerMC/Journey-1.12/master/VER.txt").openStream()));
+							BufferedReader versionFile = new BufferedReader(new InputStreamReader(new URL("https://raw.githubusercontent.com/TheSlayerMC/Journey-Into-The-Light/master/VER.txt").openStream()));
 							String curVersion = versionFile.readLine();
-							//BufferedReader changelogFile = new BufferedReader(new InputStreamReader(new URL("https://raw.githubusercontent.com/TheSlayerMC/Essence/master/main/resources/assets/essence/ingame_changelog.txt").openStream()));
-							//String changelog = changelogFile.readLine();
 							
 							SlayerAPI.addChatMessage(p, Colour.GOLD + "[|--------------------------------------------------|]"); 
 							SlayerAPI.addChatMessage(p, Colour.GOLD + "[Version: " + SlayerAPI.MOD_VERSION + "]");
 							SlayerAPI.addChatMessage(p, Colour.RED + "A Journey into the Light update is avaliable.");
 							SlayerAPI.addChatMessage(p, Colour.AQUA + "[New Version: " + curVersion + "]"); 
+							SlayerAPI.addChatMessage(p, Colour.AQUA + "You can get the new version at https://slayermods.net/index.php?title=Journey_Download"); 
 							SlayerAPI.addChatMessageWithColour(p, Colour.YELLOW, "Follow @EssenceMod on twitter for update teasers.");
 							SlayerAPI.addChatMessage(p, Colour.GOLD + "[|--------------------------------------------------|]"); 
 						}
