1d6dcf4c8350f91a8804e7296258c8fb8d9a48f6
The_Slayer <brock.kerley@hotmail.com>
2020-03-20 10:11:22 +1030

    more

--- main/java/net/journey/dimension/senterian/ChunkProviderSenterian.java
+++ main/java/net/journey/dimension/senterian/ChunkProviderSenterian.java
@@ -10,69 +10,88 @@ import net.journey.dimension.senterian.room.RoomBase;
 import net.journey.dimension.senterian.room.RoomHall;
 import net.journey.dimension.senterian.room.RoomSpawner1;
 import net.journey.dimension.senterian.room.SenterianCeiling;
+import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldEntitySpawner;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.Biome.SpawnListEntry;
 import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.ChunkPrimer;
 import net.minecraft.world.gen.IChunkGenerator;
-import net.minecraft.world.gen.feature.WorldGenerator;
 
 public class ChunkProviderSenterian implements IChunkGenerator {
 
-	private World world;
-	private Random rand;
-	private ArrayList rooms;
-    private ArrayList BossRooms;
-    private SenterianCeiling Ceiling;
-    private double[] buffer;
-    private Biome[] biomesForGeneration;
-    private int chunkX = 0;
-    private int chunkZ = 0;
-    private Map chunkTileEntityMap;
-    
-	public ChunkProviderSenterian(World world, long seed) {
-		this.world = world;
-        this.rand = new Random(seed);
-		rooms = new ArrayList(2);
-        BossRooms = new ArrayList(0);
-		//rooms.add(new RoomChest());
-		rooms.add(new RoomHall());
-		//rooms.add(new RoomNPC());
-		rooms.add(new RoomSpawner1());
-		
-        Ceiling = new SenterianCeiling();
-        this.chunkTileEntityMap = new HashMap();
+	public class ChunkCoords {
+		public final int chunkCoordX;
+		public final int chunkCoordZ;
+
+		public ChunkCoords(int X, int Z) {
+			this.chunkCoordX = X;
+			this.chunkCoordZ = Z;
+		}
+
+		public boolean equals(Object o) {
+			if (!(o instanceof ChunkCoords)) {
+				return false;
+			} else {
+				ChunkCoords chunkCoords = (ChunkCoords) o;
+				return chunkCoords.chunkCoordX == this.chunkCoordX
+						&& chunkCoords.chunkCoordZ == this.chunkCoordZ;
+			}
+		}
+
+		public int hashCode() {
+			return this.chunkCoordX + this.chunkCoordZ * 31;
+		}
 	}
-	
+
+	private ArrayList Rooms;
+	private ArrayList BossRooms;
+	private SenterianCeiling Ceiling;
+	private World worldObj;
+	private Random random;
+	private Map chunkTileEntityMap;
+    private Biome[] biomesForGeneration;
+
+	public ChunkProviderSenterian(World world, long seed) {
+
+		worldObj = world;
+		random = new Random(seed);
+
+		Rooms = new ArrayList(2);
+
+		Rooms.add(new RoomHall());
+		//Rooms.add(new RoomChest());
+		//Rooms.add(new RoomNPC());
+		Rooms.add(new RoomSpawner1());
+		/*Rooms.add(new RoomSpawner2());
+        Rooms.add(new RoomSpawner3());
+        Rooms.add(new RoomSpawner4());*/
+		Ceiling = new SenterianCeiling();
+		this.chunkTileEntityMap = new HashMap();
+	}
+
 	@Override
-	public Chunk generateChunk(int x, int z)  {
-		this.chunkX = x;
-        this.chunkZ = z;
-        this.rand.setSeed((long) x * 341873128712L + (long) z * 132897987541L);
-        this.biomesForGeneration = this.world.getBiomeProvider().getBiomes(this.biomesForGeneration, x * 16, z * 16, 16,
-                16);
+	public Chunk generateChunk(int chunkX, int chunkZ) {
+		SenterianChunkPrimer senterianChunk = new SenterianChunkPrimer();
+		 this.biomesForGeneration = this.worldObj.getBiomeProvider().getBiomes(this.biomesForGeneration, chunkX * 16, chunkZ * 16, 16, 16);
+		 
+		for (int i = 4; i > 0; i--) {
+			RoomBase room = (RoomBase) (Rooms.get(random.nextInt(2)));
+			if (room instanceof RoomHall && i >= 3)
+				room = (RoomBase) (Rooms.get(this.random.nextInt(1) + 1));
 
-        SenterianChunkPrimer chunkprimer = new SenterianChunkPrimer();
+			room.generate(senterianChunk, random, 0, i * 8, 0);
+		}
 
-        for (int i = 4; i > 0; i--) {
-        	RoomBase room = (RoomBase) (rooms.get(rand.nextInt(2)));
-            if (room instanceof RoomBase && i >= 3)
-                room = (RoomBase) (rooms.get(this.rand.nextInt(1) + 1));
+		Ceiling.generate(senterianChunk, random, 0, 40, 0);
 
-            room.generate(chunkprimer, rand, 0, i * 8, 0);
-        }
+		chunkTileEntityMap.put(new ChunkCoords(chunkX, chunkZ), senterianChunk.chunkTileEntityPositions);
 
-        Ceiling.generate(chunkprimer, rand, 0, 40, 0);
-
-        chunkTileEntityMap.put(new ChunkPos(chunkX, chunkZ), chunkprimer.chunkTileEntityPositions);
-
-        Chunk chunk = new Chunk(this.world, chunkprimer, x, z);
+        Chunk chunk = new Chunk(this.worldObj, senterianChunk, chunkX, chunkZ);
         byte[] abyte = chunk.getBiomeArray();
 
         for (int i = 0; i < abyte.length; ++i) {
@@ -80,71 +99,68 @@ public class ChunkProviderSenterian implements IChunkGenerator {
         }
 
         chunk.generateSkylightMap();
-        return chunk;
-	}
-	
-	@Override
-	public void recreateStructures(Chunk ch, int x, int z) {}
-
-	@Override
-	public List <SpawnListEntry> getPossibleCreatures(EnumCreatureType creatureType, BlockPos pos) {
-		Biome biome = this.world.getBiome(pos);
-		return biome.getSpawnableList(creatureType);
+		return chunk;
 	}
 
 	@Override
 	public void populate(int chunkX, int chunkZ) {
-		
-		 int x = chunkX * 16;
-	        int z = chunkZ * 16;
+		int x = chunkX * 16;
+		int z = chunkZ * 16;
+		Biome biome = this.worldObj.getBiome(new BlockPos(x + 16, z + 16, 0));
+		boolean flag = false;
+		this.random.setSeed(this.worldObj.getSeed());
+		long var8 = this.random.nextLong() / 2L * 2L + 1L;
+		long var10 = this.random.nextLong() / 2L * 2L + 1L;
+		this.random.setSeed(chunkX * var8 + chunkZ * var10 ^ this.worldObj.getSeed());
+		int roomToGenerate;
 
-	        BlockPos pos = new BlockPos(x, 0, z);
-	        ChunkPos chunkpos = new ChunkPos(chunkX, chunkZ);
+		Random rand = random;
+		Chunk chunk = this.worldObj.getChunkFromChunkCoords(chunkX, chunkZ);
 
-	        Biome biome = this.world.getBiome(pos.add(16, 0, 16));
-
-	        this.rand.setSeed(this.world.getSeed());
-	        long k = this.rand.nextLong() / 2L * 2L + 1L;
-	        long l = this.rand.nextLong() / 2L * 2L + 1L;
-	        this.rand.setSeed((long) chunkX * k + (long) chunkZ * l ^ this.world.getSeed());
-	        //biome.decorate(this.world, this.rand, pos);
-
-	        List<BlockPos> chunkTileEntityPositions = (List<BlockPos>) chunkTileEntityMap.get(chunkpos);
-	        if (chunkTileEntityPositions != null) {
-	            Chunk chunk = this.world.getChunkFromChunkCoords(chunkX, chunkZ);
-	            for (int i = 0; i < chunkTileEntityPositions.size(); i++) {
-	                BlockPos chunkPosition = chunkTileEntityPositions.get(i);
-	                IBlockState state = chunk.getBlockState(chunkPosition.getX(), chunkPosition.getY(),
-	                        chunkPosition.getZ());
-	                TileEntity te = state.getBlock().createTileEntity(this.world, state);
-	                this.world.setTileEntity(chunkPosition.add(x, 0, z), te);
-	            }
-	            chunkTileEntityMap.remove(chunkpos);
-	        }
-
-	        if ((chunkX & 1) == 1 && (chunkZ & 1) == 1 && this.rand.nextInt(500) == 0) {
-	            int roomToGenerate = rand.nextInt(2);
-	            int i = this.rand.nextInt(4) + 1;
-	            ((WorldGenerator) (BossRooms.get(roomToGenerate))).generate(this.world, rand, new BlockPos(x, i * 8, z));
-	            this.rand.setSeed((long) chunkX * k + (long) chunkZ * l ^ this.world.getSeed() * i << 2 | l);
-	        }
-
-	        WorldEntitySpawner.performWorldGenSpawning(this.world, biome, x + 8, z + 8, 16, 16, this.rand);
-		
+		ChunkCoords chunkCoords = new ChunkCoords(chunkX, chunkZ);
+		List<BlockPos> chunkTileEntityPositions = (List<BlockPos>)chunkTileEntityMap.get(chunkCoords);
+		if (chunkTileEntityPositions != null) {
+			for (int i = 0; i < chunkTileEntityPositions.size(); i++) {
+				BlockPos ChunkCoordIntPair = chunkTileEntityPositions.get(i);
+				IBlockState b = chunk.getBlockState(ChunkCoordIntPair.getX(), ChunkCoordIntPair.getZ(), i);
+				TileEntity te = b.getBlock().createTileEntity(this.worldObj, null);
+				this.worldObj.setTileEntity(new BlockPos(x + ChunkCoordIntPair.getX(), z + ChunkCoordIntPair.getZ(), i), te);
+			}
+			chunkTileEntityMap.remove(chunkCoords);
+		}
+		if ((chunkX & 1) == 1 && (chunkZ & 1) == 1) {
+			for(int i = 1; i < 4; i++) {
+				if(this.random.nextInt(30) == 0 || this.random.nextInt(30) == 0 || this.random.nextInt(30) == 0) {
+					roomToGenerate = rand.nextInt(2);
+					this.random.setSeed(chunkX * var8 + chunkZ * var10 ^ this.worldObj.getSeed() * i << 2 | var10);
+					break;
+				}
+			}
+		}
 	}
 
 	@Override
-	public boolean generateStructures(Chunk chunkIn, int x, int z) {
-		return false;
-	}
+    public List<SpawnListEntry> getPossibleCreatures(EnumCreatureType creatureType, BlockPos pos) {
+        Biome biome = this.worldObj.getBiome(pos);
+        return biome.getSpawnableList(creatureType);
+    }
 
-	@Override
-	public BlockPos getNearestStructurePos(World worldIn, String structureName, BlockPos position, boolean findUnexplored) {
-		return null;
-	}
+    @Override
+    public void recreateStructures(Chunk c, int x, int z) {
+    }
 
-	@Override
-	public boolean isInsideStructure(World worldIn, String structureName, BlockPos pos) {
-		return false;
-	}
+    @Override
+    public boolean generateStructures(Chunk chunkIn, int x, int z) {
+        return false;
+    }
+
+    @Override
+    public BlockPos getNearestStructurePos(World worldIn, String structureName, BlockPos position, boolean findUnexplored) {
+        return null;
+    }
+
+    @Override
+    public boolean isInsideStructure(World worldIn, String structureName, BlockPos pos) {
+        return false;
+    }
 }
