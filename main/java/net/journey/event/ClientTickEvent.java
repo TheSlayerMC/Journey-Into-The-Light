d3622ad27461692139f644e4cdd36edcaaec1ccd
The_Slayer <brock.kerley@hotmail.com>
2020-03-28 21:08:10 +1030

    Update ClientTickEvent.java

--- main/java/net/journey/event/ClientTickEvent.java
+++ main/java/net/journey/event/ClientTickEvent.java
@@ -31,7 +31,7 @@ public class ClientTickEvent {
 	private Item boots = null, body = null, legs = null, helmet = null;
 	public static final ResourceLocation TEXTURE  = new ResourceLocation(SlayerAPI.MOD_ID, "textures/gui/misc.png"); 
 
-	
+
 	@SubscribeEvent
 	public void clientTickEvent(PlayerTickEvent event) {
 		ItemStack stackBoots = event.player.inventory.armorItemInSlot(0);
@@ -56,7 +56,7 @@ public class ClientTickEvent {
 			}
 		}
 	}
-	
+
 	@SubscribeEvent
 	public void renderEvent(RenderTickEvent event) {
 		if(Config.showManaBar)
@@ -68,28 +68,26 @@ public class ClientTickEvent {
 		if (player != null) {
 			if (mc.currentScreen == null && player.getHeldItemMainhand() != null && player.getHeldItemMainhand().getItem() instanceof ItemStaff 
 					|| player.getHeldItemMainhand().getItem() instanceof ItemTeleport || player.getHeldItemMainhand().getItem() instanceof ItemGun) {
-				if (!player.capabilities.isCreativeMode) {
-					IEssence mana = player.getCapability(EssenceProvider.ESSENCE_CAP, null);
-					GL11.glPushMatrix();
-					GlStateManager.enableBlend();
-					GlStateManager.enableAlpha();
-					GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-					GuiIngame gig = mc.ingameGUI;
-					ScaledResolution scaledresolution = new ScaledResolution(mc);
-					mc.getTextureManager().bindTexture(TEXTURE);
-					int y = scaledresolution.getScaledHeight() - 30, x = 10;
-					gig.drawTexturedModalRect(x - 10, y + 10, 0, 177, 117, 19);
-					gig.drawTexturedModalRect(x - 6, y + 17, 0, 23, 109, 5);
-					for (int i = 0; i < mana.getEssenceValue(); i++) {
-						if (!(i >= 10)) {
-							x += 11;
-							gig.drawTexturedModalRect(x - 17, y + 17, 0, 0, 10, 5);
-						}
+				IEssence mana = player.getCapability(EssenceProvider.ESSENCE_CAP, null);
+				GL11.glPushMatrix();
+				GlStateManager.enableBlend();
+				GlStateManager.enableAlpha();
+				GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+				GuiIngame gig = mc.ingameGUI;
+				ScaledResolution scaledresolution = new ScaledResolution(mc);
+				mc.getTextureManager().bindTexture(TEXTURE);
+				int y = scaledresolution.getScaledHeight() - 30, x = 10;
+				gig.drawTexturedModalRect(x - 10, y + 10, 0, 177, 117, 19);
+				gig.drawTexturedModalRect(x - 6, y + 17, 0, 23, 109, 5);
+				for (int i = 0; i < mana.getEssenceValue(); i++) {
+					if (!(i >= 10)) {
+						x += 11;
+						gig.drawTexturedModalRect(x - 17, y + 17, 0, 0, 10, 5);
 					}
-					GlStateManager.disableAlpha();
-					GlStateManager.disableBlend();
-					GL11.glPopMatrix();
 				}
+				GlStateManager.disableAlpha();
+				GlStateManager.disableBlend();
+				GL11.glPopMatrix();
 			}
 		}
 	}
