96d2af4667ca2565abee29e4d2af8b5a956e5a40
The_Slayer <brock.kerley@hotmail.com>
2020-03-28 21:51:59 +1030

    Update ChunkProviderTerrania.java

--- main/java/net/journey/dimension/terrania/ChunkProviderTerrania.java
+++ main/java/net/journey/dimension/terrania/ChunkProviderTerrania.java
@@ -240,98 +240,98 @@ public class ChunkProviderTerrania implements IChunkGenerator {
     }
 
     private void generate(int x, int y, int z) {
-        double d0 = 684.412D;
-        double d1 = 684.412D;
-        double d2 = 512.0D;
-        double d3 = 512.0D;
-        this.gen4 = this.noiseGen6.generateNoiseOctaves(this.gen4, x, z, 5, 5, 200.0D, 200.0D, 0.5D);
-        this.gen1 = this.noiseGen3.generateNoiseOctaves(this.gen1, x, y, z, 5, 33, 5, 8.555150000000001D, 4.277575000000001D, 8.555150000000001D);
-        this.gen2 = this.noiseGen1.generateNoiseOctaves(this.gen2, x, y, z, 5, 33, 5, 684.412D, 684.412D, 684.412D);
-        this.gen3 = this.noiseGen2.generateNoiseOctaves(this.gen3, x, y, z, 5, 33, 5, 684.412D, 684.412D, 684.412D);
-        boolean flag1 = false;
-        boolean flag = false;
-        int l = 0;
-        int i1 = 0;
-        double d4 = 8.5D;
+    	 double d0 = 684.412D;
+         double d1 = 684.412D;
+         double d2 = 512.0D;
+         double d3 = 512.0D;
+         this.gen4 = this.noiseGen6.generateNoiseOctaves(this.gen4, x, z, 5, 5, 200.0D, 200.0D, 0.5D);
+         this.gen1 = this.noiseGen3.generateNoiseOctaves(this.gen1, x, y, z, 5, 33, 5, 8.555150000000001D, 4.277575000000001D, 8.555150000000001D);
+         this.gen2 = this.noiseGen1.generateNoiseOctaves(this.gen2, x, y, z, 5, 33, 5, 684.412D, 684.412D, 684.412D);
+         this.gen3 = this.noiseGen2.generateNoiseOctaves(this.gen3, x, y, z, 5, 33, 5, 684.412D, 684.412D, 684.412D);
+         boolean flag1 = false;
+         boolean flag = false;
+         int l = 0;
+         int i1 = 0;
+         double d4 = 8.5D;
 
-        for (int j1 = 0; j1 < 5; ++j1) {
-            for (int k1 = 0; k1 < 5; ++k1) {
-                float f = 0.0F;
-                float f1 = 0.0F;
-                float f2 = 0.0F;
-                byte b0 = 2;
-                Biome Biome = this.biomesForGeneration[j1 + 2 + (k1 + 2) * 10];
+         for (int j1 = 0; j1 < 5; ++j1) {
+             for (int k1 = 0; k1 < 5; ++k1) {
+                 float f = 0.0F;
+                 float f1 = 0.0F;
+                 float f2 = 0.0F;
+                 byte b0 = 2;
+                 Biome Biome = this.biomesForGeneration[j1 + 2 + (k1 + 2) * 10];
 
-                for (int l1 = -b0; l1 <= b0; ++l1) {
-                    for (int i2 = -b0; i2 <= b0; ++i2) {
-                        Biome Biome1 = this.biomesForGeneration[j1 + l1 + 2 + (k1 + i2 + 2) * 10];
-                        float f3 = Biome1.getBaseHeight();
-                        float f4 = Biome1.getHeightVariation();
+                 for (int l1 = -b0; l1 <= b0; ++l1) {
+                     for (int i2 = -b0; i2 <= b0; ++i2) {
+                         Biome Biome1 = this.biomesForGeneration[j1 + l1 + 2 + (k1 + i2 + 2) * 10];
+                         float f3 = 0.1F;
+                         float f4 = 0.125F;
 
-                        float f5 = this.parabolicField[l1 + 2 + (i2 + 2) * 5] / (f3 + 2.0F);
+                         float f5 = this.parabolicField[l1 + 2 + (i2 + 2) * 5] / (f3 + 2.0F);
 
-                        f += f4 * f5 / 2;
-                        f1 += f3 * f5 / 2;
-                        f2 += f5 / 2;
-                    }
-                }
+                         f += f4 * f5 / 2;
+                         f1 += f3 * f5 / 2;
+                         f2 += f5 / 2;
+                     }
+                 }
 
-                f /= f2;
-                f1 /= f2;
-                f = f * 0.9F + 0.1F;
-                f1 = (f1 * 4.0F - 1.0F) / 8.0F;
-                double d12 = this.gen4[i1] / 8000.0D;
+                 f /= f2;
+                 f1 /= f2;
+                 f = f * 0.9F + 0.1F;
+                 f1 = (f1 * 4.0F - 1.0F) / 8.0F;
+                 double d12 = this.gen4[i1] / 8000.0D;
 
-                if (d12 < 0.0D) {
-                    d12 = -d12 * 0.3D;
-                }
+                 if (d12 < 0.0D) {
+                     d12 = -d12 * 0.3D;
+                 }
 
-                d12 = d12 * 3.0D - 2.0D;
+                 d12 = d12 * 3.0D - 2.0D;
 
-                if (d12 < 0.0D) {
-                    d12 /= 2.0D;
+                 if (d12 < 0.0D) {
+                     d12 /= 2.0D;
 
-                    if (d12 < -1.0D)
-                        d12 = -1.0D;
+                     if (d12 < -1.0D)
+                         d12 = -1.0D;
 
-                    d12 /= 1.4D;
-                    d12 /= 2.0D;
-                } else {
-                    if (d12 > 1.0D)
-                        d12 = 1.0D;
+                     d12 /= 1.4D;
+                     d12 /= 2.0D;
+                 } else {
+                     if (d12 > 1.0D)
+                         d12 = 1.0D;
 
-                    d12 /= 8.0D;
-                }
+                     d12 /= 8.0D;
+                 }
 
-                ++i1;
-                double d13 = f1;
-                double d14 = f;
-                d13 += d12 * 0.2D;
-                d13 = d13 * 8.5D / 8.0D;
-                double d5 = 8.5D + d13 * 4.0D;
+                 ++i1;
+                 double d13 = f1;
+                 double d14 = f;
+                 d13 += d12 * 0.2D;
+                 d13 = d13 * 8.5D / 8.0D;
+                 double d5 = 8.5D + d13 * 4.0D;
 
-                for (int j2 = 0; j2 < 33; ++j2) {
-                    double d6 = (j2 - d5) * 12.0D * 128.0D / 256.0D / d14;
+                 for (int j2 = 0; j2 < 33; ++j2) {
+                     double d6 = (j2 - d5) * 12.0D * 128.0D / 256.0D / d14;
 
-                    if (d6 < 0.0D)
-                        d6 *= 4.0D;
+                     if (d6 < 0.0D)
+                         d6 *= 4.0D;
 
 
-                    double d7 = this.gen2[l] / 128.0D;
-                    double d8 = this.gen3[l] / 128.0D;
-                    double d9 = (this.gen1[l] / 10.0D + 1.0D) / 2.0D;
-                    double d10 = MathHelper.clampedLerp(d7, d8, d9) - d6;
+                     double d7 = this.gen2[l] / 128.0D;
+                     double d8 = this.gen3[l] / 128.0D;
+                     double d9 = (this.gen1[l] / 10.0D + 1.0D) / 2.0D;
+                     double d10 = MathHelper.clampedLerp(d7, d8, d9) - d6;
 
-                    if (j2 > 29) {
-                        double d11 = (j2 - 29) / 3.0F;
-                        d10 = d10 * (1.0D - d11) + -10.0D * d11;
-                    }
+                     if (j2 > 29) {
+                         double d11 = (j2 - 29) / 3.0F;
+                         d10 = d10 * (1.0D - d11) + -10.0D * d11;
+                     }
 
-                    this.da[l] = d10;
-                    ++l;
-                }
-            }
-        }
+                     this.da[l] = d10;
+                     ++l;
+                 }
+             }
+         }
     }
 
     @Override
