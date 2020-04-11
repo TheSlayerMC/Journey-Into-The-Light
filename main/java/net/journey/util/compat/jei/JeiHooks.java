121ab12ee3fdbda9b73eb6ae8a00bda9ecd0c1a4
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2020-04-11 10:07:24 -0700

    need to fix GUI, item arrangement, and finish other recipes

--- main/java/net/journey/util/compat/jei/JeiHooks.java
+++ main/java/net/journey/util/compat/jei/JeiHooks.java
@@ -14,6 +14,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
+@JEIPlugin
 public class JeiHooks implements IModPlugin {
 
 	static {
