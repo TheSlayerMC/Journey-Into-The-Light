a49501d966385a4ff06f5d97b59d16577512dd0c
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2020-04-19 21:31:22 -0700

    Finished dungeon loot tables, might need to adjust weight values in the future

--- src/main/java/net/journey/event/LootTableEvent.java
+++ src/main/java/net/journey/event/LootTableEvent.java
@@ -14,29 +14,29 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class LootTableEvent {
 
     /**
-     * placeholder for future resource locations
-     * TODO: replace with future resource locations for Journey loot tables to be injected into vanilla and 3rd party loot tables
-     * ex:
-     * ResourceLocation BATTLE_HEART_LOOT = new ResourceLocation("journey", "loot_tables/misc/battle_heart_loot");
+     * Resource Location for loot table to be injected into vanilla loot table
      */
-    public static final ResourceLocation TEST = new ResourceLocation("journey", "chests/test");
+    public static final ResourceLocation DUNGEON_LOOT = new ResourceLocation("journey", "chests/dungeon_loot");
+    
     /**
-     * placeholder for LootTable pool names
-     * TODO: replace with proper pool name
-     * ex:
-     * String DUNGEON_POOL = new String("journey_dungeon_loot");
+     * Name of pool to be selected from said loot table
      */
-    public static final String TESTPOOL = "journey_dungeon_loot";
+    public static final String DUNGEON_POOL = "journey_dungeon_loot";
 
     /**
-     * injects custom journey loot tables into vanilla or 3rd party loot tables
-     * currently intects "nothing" into vanilla "simple_dungeon" loot table
-     * TODO: finish loot table injection
+     * Gets names of loot tables from resource location, then adds new loot tables and pools to said loot tables
      */
     @SubscribeEvent
     public static void addLootToTable(LootTableLoadEvent event) {
-        if (event.getName().toString().equals("minecraft:chests/village_blacksmith")) {
-            event.getTable().addPool(new LootPool(new LootEntry[]{new LootEntryTable(TEST, 1, 1, new LootCondition[0], TESTPOOL)}, new LootCondition[0], new RandomValueRange(1), new RandomValueRange(0, 1), TESTPOOL));
+        if (
+        event.getName().toString().equals("minecraft:chests/simple_dungeon") || 
+        event.getName().toString().equals("minecraft:chests/abandoned_mineshaft") || 
+        event.getName().toString().equals("minecraft:chests/desert_pyramid") || 
+        event.getName().toString().equals("minecraft:chests/jungle_temple") || 
+        event.getName().toString().equals("minecraft:chests/stronghold_corridor") || 
+        event.getName().toString().equals("minecraft:chests/stronghold_crossing")|| 
+        event.getName().toString().equals("minecraft:chests/stronghold_library")) {
+            event.getTable().addPool(new LootPool(new LootEntry[]{new LootEntryTable(DUNGEON_LOOT, 1, 1, new LootCondition[0], DUNGEON_POOL)}, new LootCondition[0], new RandomValueRange(1), new RandomValueRange(0, 1), DUNGEON_POOL));
         }
     }
 }
