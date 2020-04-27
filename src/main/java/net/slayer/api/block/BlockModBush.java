9b307af2f3f18b935023ecbceac5778be825515c
The_Slayer <brock.kerley@hotmail.com>
2020-04-27 10:15:05 +0930

    made the bushs only harvest when an item not block is in hand

--- src/main/java/net/slayer/api/block/BlockModBush.java
+++ src/main/java/net/slayer/api/block/BlockModBush.java
@@ -1,5 +1,7 @@
 package net.slayer.api.block;
 
+import java.util.Random;
+
 import net.journey.init.JourneyTabs;
 import net.minecraft.block.Block;
 import net.minecraft.block.IGrowable;
@@ -10,6 +12,7 @@ import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumBlockRenderType;
@@ -23,162 +26,162 @@ import net.minecraftforge.common.EnumPlantType;
 import net.minecraftforge.common.IPlantable;
 import net.slayer.api.EnumMaterialTypes;
 
-import java.util.Random;
-
 public class BlockModBush extends BlockMod implements IPlantable, IGrowable {
 
-    public static final PropertyInteger AGE = PropertyInteger.create("age", 0, 2);
-    public static final AxisAlignedBB smallbush = new AxisAlignedBB(0.5F - 0.3F, 0.0F, 0.5F - 0.3F, 0.5F + 0.3F, 0.3F * 1.0F, 0.5F + 0.3F);
-    public static final AxisAlignedBB fullbush = new AxisAlignedBB(0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F);
-    private boolean isNether;
-    private Item berry;
+	public static final PropertyInteger AGE = PropertyInteger.create("age", 0, 2);
+	public static final AxisAlignedBB smallbush = new AxisAlignedBB(0.5F - 0.3F, 0.0F, 0.5F - 0.3F, 0.5F + 0.3F, 0.3F * 1.0F, 0.5F + 0.3F);
+	public static final AxisAlignedBB fullbush = new AxisAlignedBB(0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F);
+	private boolean isNether;
+	private Item berry;
 
-    public BlockModBush(String name, String finalName, Item berry, boolean isNether) {
-        super(EnumMaterialTypes.LEAVES, name, finalName, 1.0F);
-        this.berry = berry;
-        this.isNether = isNether;
-        this.setDefaultState(this.blockState.getBaseState().withProperty(AGE, 0));
-        this.setTickRandomly(true);
-        this.setLightOpacity(-100000);
-        this.setCreativeTab(JourneyTabs.CROPS);
-    }
+	public BlockModBush(String name, String finalName, Item berry, boolean isNether) {
+		super(EnumMaterialTypes.LEAVES, name, finalName, 1.0F);
+		this.berry = berry;
+		this.isNether = isNether;
+		this.setDefaultState(this.blockState.getBaseState().withProperty(AGE, 0));
+		this.setTickRandomly(true);
+		this.setLightOpacity(-100000);
+		this.setCreativeTab(JourneyTabs.CROPS);
+	}
 
-    @Override
-    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess access, BlockPos pos) {
-        float f = 0.3F;
-        if (state.getValue(AGE) == 0) {
-            return smallbush;
-        }
+	@Override
+	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess access, BlockPos pos) {
+		float f = 0.3F;
+		if (state.getValue(AGE) == 0) {
+			return smallbush;
+		}
 
-        if (state.getValue(AGE) == 1) {
-            return fullbush;
-        }
+		if (state.getValue(AGE) == 1) {
+			return fullbush;
+		}
 
-        if (state.getValue(AGE) == 2) {
-            return fullbush;
-        }
-        return fullbush;
-    }
+		if (state.getValue(AGE) == 2) {
+			return fullbush;
+		}
+		return fullbush;
+	}
 
-    @Override
-    public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
-        return null;
-    }
+	@Override
+	public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
+		return null;
+	}
 
-    @Override
-    public boolean canPlaceBlockAt(World w, BlockPos pos) {
-        Block block = w.getBlockState(pos.down()).getBlock();
-        if (isNether) {
-            return block == Blocks.NETHERRACK;
-        }
-        return block == Blocks.GRASS || block == Blocks.DIRT;
-    }
+	@Override
+	public boolean canPlaceBlockAt(World w, BlockPos pos) {
+		Block block = w.getBlockState(pos.down()).getBlock();
+		if (isNether) {
+			return block == Blocks.NETHERRACK;
+		}
+		return block == Blocks.GRASS || block == Blocks.DIRT;
+	}
 
-    @Override
-    public IBlockState getStateFromMeta(int meta) {
-        return this.getDefaultState().withProperty(AGE, meta);
-    }
+	@Override
+	public IBlockState getStateFromMeta(int meta) {
+		return this.getDefaultState().withProperty(AGE, meta);
+	}
 
-    @Override
-    public int getMetaFromState(IBlockState state) {
-        return state.getValue(AGE);
-    }
+	@Override
+	public int getMetaFromState(IBlockState state) {
+		return state.getValue(AGE);
+	}
 
-    @Override
-    protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, AGE);
-    }
+	@Override
+	protected BlockStateContainer createBlockState() {
+		return new BlockStateContainer(this, AGE);
+	}
 
 
-    @Override
-    public boolean canGrow(World worldIn, BlockPos pos, IBlockState state, boolean isClient) {
-        return state.getValue(AGE) < 2;
-    }
+	@Override
+	public boolean canGrow(World worldIn, BlockPos pos, IBlockState state, boolean isClient) {
+		return state.getValue(AGE) < 2;
+	}
 
-    @Override
-    public boolean canUseBonemeal(World worldIn, Random rand, BlockPos pos, IBlockState state) {
-        return true;
-    }
+	@Override
+	public boolean canUseBonemeal(World worldIn, Random rand, BlockPos pos, IBlockState state) {
+		return true;
+	}
 
-    @Override
-    public EnumPlantType getPlantType(IBlockAccess world, BlockPos pos) {
-        return null;
-    }
+	@Override
+	public EnumPlantType getPlantType(IBlockAccess world, BlockPos pos) {
+		return null;
+	}
 
-    @Override
-    public IBlockState getPlant(IBlockAccess world, BlockPos pos) {
-        return this.getDefaultState();
-    }
+	@Override
+	public IBlockState getPlant(IBlockAccess world, BlockPos pos) {
+		return this.getDefaultState();
+	}
 
-    @Override
-    public boolean isPassable(IBlockAccess worldIn, BlockPos pos) {
-        return false;
-    }
+	@Override
+	public boolean isPassable(IBlockAccess worldIn, BlockPos pos) {
+		return false;
+	}
 
-    @Override
-    public void grow(World worldIn, Random rand, BlockPos pos, IBlockState state) {
-        if (!worldIn.isRemote && state.getValue(AGE) < 2) {
-            worldIn.setBlockState(pos, state.withProperty(AGE, state.getValue(AGE) + 1), 2);
-        }
-    }
+	@Override
+	public void grow(World worldIn, Random rand, BlockPos pos, IBlockState state) {
+		if (!worldIn.isRemote && state.getValue(AGE) < 2) {
+			worldIn.setBlockState(pos, state.withProperty(AGE, state.getValue(AGE) + 1), 2);
+		}
+	}
 
-    @Override
-    public void updateTick(World w, BlockPos pos, IBlockState state, Random rand) {
-        super.updateTick(w, pos, state, rand);
-        if (w.rand.nextInt(5) == 0) {
-            int age = state.getValue(AGE);
-            if (age < 2) {
-                w.setBlockState(pos, state.withProperty(AGE, age + 1), 2);
-            }
-        }
-    }
+	@Override
+	public void updateTick(World w, BlockPos pos, IBlockState state, Random rand) {
+		super.updateTick(w, pos, state, rand);
+		if (w.rand.nextInt(5) == 0) {
+			int age = state.getValue(AGE);
+			if (age < 2) {
+				w.setBlockState(pos, state.withProperty(AGE, age + 1), 2);
+			}
+		}
+	}
 
-    @Override
-    public boolean canSustainPlant(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing direction, IPlantable plantable) {
-        if (plantable instanceof BlockModBush && state.getPropertyKeys().contains(AGE)) {
-            return (state.getValue(AGE) > 2);
-        }
+	@Override
+	public boolean canSustainPlant(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing direction, IPlantable plantable) {
+		if (plantable instanceof BlockModBush && state.getPropertyKeys().contains(AGE)) {
+			return (state.getValue(AGE) > 2);
+		}
 
-        return super.canSustainPlant(state, world, pos, direction, plantable);
-    }
+		return super.canSustainPlant(state, world, pos, direction, plantable);
+	}
 
-    @Override
-    public boolean isFullCube(IBlockState state) {
-        return false;
-    }
+	@Override
+	public boolean isFullCube(IBlockState state) {
+		return false;
+	}
 
-    @Override
-    public boolean isOpaqueCube(IBlockState state) {
-        return false;
-    }
+	@Override
+	public boolean isOpaqueCube(IBlockState state) {
+		return false;
+	}
 
-    @Override
-    public int damageDropped(IBlockState state) {
-        return 0;
-    }
+	@Override
+	public int damageDropped(IBlockState state) {
+		return 0;
+	}
 
-    @Override
-    public EnumBlockRenderType getRenderType(IBlockState state) {
-        return EnumBlockRenderType.MODEL;
-    }
+	@Override
+	public EnumBlockRenderType getRenderType(IBlockState state) {
+		return EnumBlockRenderType.MODEL;
+	}
 
-    @Override
-    public BlockRenderLayer getRenderLayer() {
-        return BlockRenderLayer.TRANSLUCENT;
-    }
+	@Override
+	public BlockRenderLayer getRenderLayer() {
+		return BlockRenderLayer.TRANSLUCENT;
+	}
 
-    @Override
-    public boolean onBlockActivated(World w, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ) {
-        double
-                x = player.posX,
-                y = player.posY,
-                z = player.posZ;
-        if (state.getValue(AGE) == 2 && !w.isRemote) {
-            EntityItem drop = new EntityItem(w, x, y, z, new ItemStack(berry));
-            w.spawnEntity(drop);
-            w.setBlockState(pos, state.withProperty(AGE, 0), 1);
-            return true;
-        }
-        return false;
-    }
+	@Override
+	public boolean onBlockActivated(World w, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ) {
+		double
+		x = player.posX,
+		y = player.posY,
+		z = player.posZ;
+		if (state.getValue(AGE) == 2 && !w.isRemote) {
+			if(!(player.getHeldItemMainhand().getItem() instanceof ItemBlock)) {
+				EntityItem drop = new EntityItem(w, x, y, z, new ItemStack(berry));
+				w.spawnEntity(drop);
+				w.setBlockState(pos, state.withProperty(AGE, 0), 1);
+				return true;
+			}
+		}
+		return false;
+	}
 }
