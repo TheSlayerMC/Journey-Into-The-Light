91045167684703da58103c314b02409e8a6a27b0
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2020-05-06 04:15:12 -0700

    pt 2

--- src/main/java/net/journey/entity/mob/boss/EntityGuardianOfDestruction.java
+++ src/main/java/net/journey/entity/mob/boss/EntityGuardianOfDestruction.java
@@ -5,6 +5,7 @@ import net.journey.entity.projectile.arrow.EntityDarknessArrow;
 import net.journey.entity.projectile.arrow.EntityFlameArrow;
 import net.journey.entity.projectile.arrow.EntityFrozenArrow;
 import net.journey.entity.projectile.arrow.EntityPoisonArrow;
+import net.journey.init.JourneySounds;
 import net.journey.init.items.JourneyItems;
 import net.journey.init.items.JourneyWeapons;
 import net.minecraft.entity.EntityLivingBase;
@@ -19,6 +20,10 @@ import net.minecraft.init.SoundEvents;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.World;
@@ -28,10 +33,12 @@ import javax.annotation.Nullable;
 
 public class EntityGuardianOfDestruction extends EntityEssenceBoss {
 
-
+	public int maxHealth = (int) MobStats.guardianofdestructionHealth;
+	
+    public int rolltimer;
+    
     public final int sleep = 0, alert = 1, lowhealth = 2;
     public int stage;
-    public int rolltimer;
 
     public EntityGuardianOfDestruction(World par1World) {
         super(par1World);
@@ -44,13 +51,15 @@ public class EntityGuardianOfDestruction extends EntityEssenceBoss {
     protected void initEntityAI() {
         super.initEntityAI();
         this.targetTasks.addTask(2, new EntityAIFindEntityNearestPlayer(this));
-        this.targetTasks.addTask(1, new EntityAILookIdle(this));
         addAttackingAI();
     }
     
 	@Override
 	protected void entityInit() {
 		super.entityInit();
+        this.prevRenderYawOffset = 180.0F;
+        this.renderYawOffset = 180.0F;
+        this.rotationYaw = 180.0F;
 		this.posX = Math.floor(this.posX + 0.5D);
 		this.posY = Math.floor(this.posY + 0.5D);
 		this.posZ = Math.floor(this.posZ + 0.5D);
@@ -74,13 +83,16 @@ public class EntityGuardianOfDestruction extends EntityEssenceBoss {
     public void onLivingUpdate() {
         super.onLivingUpdate();
         int health = (int) getHealth();
-        if (health >= 1500) stage = sleep;
-        else if (health <= 1499 && health > 500) stage = alert;
-        else if (health <= 500) stage = lowhealth;
+        if (health >= maxHealth) stage = sleep;
+        else if (health <= maxHealth - 1 && health > maxHealth / 3) stage = alert;
+        else if (health <= maxHealth / 3) stage = lowhealth;
         if (stage == sleep) {
             this.motionX = 0.0D;
             this.motionY = 0.0D;
             this.motionZ = 0.0D;
+            this.prevRenderYawOffset = 180.0F;
+            this.renderYawOffset = 180.0F;
+            this.rotationYaw = 180.0F;
         	this.setAttackTarget(null);
         } else {
         	if (stage == alert) {
@@ -117,17 +129,17 @@ public class EntityGuardianOfDestruction extends EntityEssenceBoss {
     
     @Override
     public SoundEvent setLivingSound() {
-        return SoundEvents.ENTITY_WITHER_AMBIENT;
+        return JourneySounds.EMPTY;
     }
 
     @Override
     public SoundEvent setHurtSound() {
-        return SoundEvents.ENTITY_WITHER_HURT;
+        return JourneySounds.SENTRY_AMBIENT_1;
     }
 
     @Override
     public SoundEvent setDeathSound() {
-        return SoundEvents.ENTITY_WITHER_DEATH;
+        return JourneySounds.SENTRY_AMBIENT_1;
     }
 
     @Override
