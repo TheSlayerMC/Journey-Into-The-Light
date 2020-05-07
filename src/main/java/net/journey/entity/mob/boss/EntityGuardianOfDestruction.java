1d163a588b5e899e9f177c3890531c9900087a98
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2020-05-06 18:38:53 -0700

    this sucks

--- src/main/java/net/journey/entity/mob/boss/EntityGuardianOfDestruction.java
+++ src/main/java/net/journey/entity/mob/boss/EntityGuardianOfDestruction.java
@@ -2,10 +2,16 @@ package net.journey.entity.mob.boss;
 
 import net.journey.entity.MobStats;
 import net.journey.init.JourneySounds;
+import net.journey.util.PotionEffects;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.MoverType;
 import net.minecraft.entity.ai.EntityAIFindEntityNearestPlayer;
+import net.minecraft.entity.ai.EntityAILookIdle;
 import net.minecraft.item.Item;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.SoundEvent;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import net.slayer.api.entity.EntityEssenceBoss;
 
@@ -23,7 +29,7 @@ public class EntityGuardianOfDestruction extends EntityEssenceBoss {
 
 	public EntityGuardianOfDestruction(World par1World) {
 		super(par1World);
-		setSize(2.0F, 4.0F);
+		setSize(2.0F, 2.0F);
 		this.stage = sleep;
 		this.rotationYaw = this.rotationPitch = 0.0F;
     }
@@ -31,7 +37,8 @@ public class EntityGuardianOfDestruction extends EntityEssenceBoss {
     @Override
     protected void initEntityAI() {
         super.initEntityAI();
-        this.targetTasks.addTask(2, new EntityAIFindEntityNearestPlayer(this));
+        this.targetTasks.addTask(1, new EntityAIFindEntityNearestPlayer(this));
+        this.tasks.addTask(2, new EntityAILookIdle(this));
         addAttackingAI();
     }
 
@@ -45,6 +52,15 @@ public class EntityGuardianOfDestruction extends EntityEssenceBoss {
 		this.posY = Math.floor(this.posY + 0.5D);
 		this.posZ = Math.floor(this.posZ + 0.5D);
 	}
+	
+    @Override
+    public boolean attackEntityAsMob(Entity e) {
+        boolean attacked = super.attackEntityAsMob(e);
+        if (attacked) {
+            e.addVelocity((double) (-MathHelper.sin(this.rotationYaw * (float) Math.PI / 180.0F)) * 4, 0.1D, (double) (MathHelper.cos(this.rotationYaw * (float) Math.PI / 180.0F)) * 4);
+        }
+        return attacked;
+    }
 
 	@Override
 	public void move(MoverType type, double x, double y, double z) {
@@ -96,7 +112,7 @@ public class EntityGuardianOfDestruction extends EntityEssenceBoss {
 			        this.motionY = 0.0D;
 			        this.motionZ = 0.0D;
 			        this.isMoving = false;
-			        rolltimer = 40;
+			        rolltimer = 20;
 		        }
 				if (rolltimer > 0) rolltimer--;
 				else {
@@ -126,19 +142,26 @@ public class EntityGuardianOfDestruction extends EntityEssenceBoss {
 		}
     }
 
-    @Override
-    public double setKnockbackResistance() {
-        return 3.0D;
-    }
+	@Override
+	public double setMovementSpeed() {
+		return MobStats.normalSpeed;
+	}
 
 	@Override
-    public double setMovementSpeed() {
-		if (stage == lowhealth) {
-			return 1.0D;
-		} else
-			return MobStats.normalSpeed;
-    }
+	public double setKnockbackResistance() {
+		return 3.0D;
+	}
+	
+	@Override
+	public boolean canBePushed() {
+		return false;
+	}
 
+	@Override
+	public boolean canDespawn() {
+		return false;
+	}
+	
     @Override
     public double setMaxHealth(MobStats s) {
         return MobStats.guardianofdestructionHealth;
@@ -162,6 +185,31 @@ public class EntityGuardianOfDestruction extends EntityEssenceBoss {
     @Override
     public SoundEvent setDeathSound() {
         return JourneySounds.SENTRY_AMBIENT_1;
+    }
+    
+    @Override
+	public void knockBack(Entity entity, float strength, double xRatio, double zRatio) {
+		if (this.getHealth() < 0.0F) {
+			super.knockBack(entity, strength, xRatio, zRatio);
+		}
+	}
+
+    @Override
+    public int getVerticalFaceSpeed() {
+        return 0;
+    }
+
+    @Override
+    public int getHorizontalFaceSpeed() {
+        return 0;
+    }
+
+    @Override
+    public void applyEntityCollision(Entity entityIn) {}
+
+    @Override
+    public float getCollisionBorderSize() {
+        return 2.0F;
     }
 
     @Override
