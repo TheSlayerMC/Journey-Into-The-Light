20ce8bea37fe1fddb272e54f5afc396b66371ab2
TimeConqueror <timeconqueror999@gmail.com>
2020-05-10 00:13:58 +0300

    Shortened BlockGemBlock

--- src/main/java/net/journey/blocks/BlockGemBlock.java
+++ src/main/java/net/journey/blocks/BlockGemBlock.java
@@ -1,24 +1,25 @@
 package net.journey.blocks;
 
 import net.journey.init.items.JourneyItems;
+import net.journey.util.RandHelper;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.slayer.api.EnumMaterialTypes;
 import net.slayer.api.block.BlockMod;
 
-import java.util.List;
 import java.util.Random;
 
 public class BlockGemBlock extends BlockMod {
 
-    public boolean isRare;
+    protected final boolean isRare;
 
-    public BlockGemBlock(String name, String f, boolean rare) {
+    public BlockGemBlock(String name, String f, boolean isRare) {
         super(EnumMaterialTypes.GLASS, name, f, 0.4F);
-        isRare = rare;
+        this.isRare = isRare;
     }
 
     @Override
@@ -27,25 +28,11 @@ public class BlockGemBlock extends BlockMod {
     }
 
     @Override
-    public List<ItemStack> getDrops(IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
-        List<ItemStack> drops = super.getDrops(world, pos, state, fortune);
-        Item gem = JourneyItems.greenGem;
-        switch (rand.nextInt(4)) {
-            case 0:
-                gem = JourneyItems.greenGem;
-                break;
-            case 1:
-                gem = JourneyItems.purpleGem;
-                break;
-            case 2:
-                gem = JourneyItems.blueGem;
-                break;
-            case 3:
-                gem = JourneyItems.yellowGem;
-                break;
-        }
-        if (isRare) drops.add(new ItemStack(gem, rand.nextInt(1) + 3));
-        else drops.add(new ItemStack(gem, rand.nextInt(1) + 1));
-        return drops;
+    public void getDrops(NonNullList<ItemStack> drops, IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
+        Item gem = RandHelper.chooseEqual(RANDOM, JourneyItems.greenGem, JourneyItems.purpleGem, JourneyItems.blueGem, JourneyItems.yellowGem);
+
+        int count = isRare ? RANDOM.nextInt(1) + 3 : RANDOM.nextInt(1) + 1;
+
+        drops.add(new ItemStack(gem, count));
     }
 }
