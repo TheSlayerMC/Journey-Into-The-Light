cf5b026e00f166804bbacefd64818c5d94a21006
DiamondWalker <19913710+DiamondWalker@users.noreply.github.com>
2020-05-13 13:26:48 -0400

    Fixed Blazier tasks

--- src/main/java/net/journey/entity/mob/boss/EntityBlazier.java
+++ src/main/java/net/journey/entity/mob/boss/EntityBlazier.java
@@ -42,11 +42,11 @@ public class EntityBlazier extends EntityEssenceBoss implements IRangedAttackMob
         super(w);
         this.experienceValue = 10;
         //this.tasks.addTask(1, new EntityAIArrowAttack(this, 1.0D, 40, 20.0F));
-        this.tasks.addTask(4, new EntityBlazier.AIFireballAttack());
-        this.tasks.addTask(5, new EntityAIMoveTowardsRestriction(this, 1.0D));
-        this.tasks.addTask(7, new EntityAIWander(this, 1.0D));
-        this.tasks.addTask(8, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
-        this.tasks.addTask(8, new EntityAILookIdle(this));
+        this.tasks.addTask(1, new EntityBlazier.AIFireballAttack());
+        this.tasks.addTask(3, new EntityAIMoveTowardsRestriction(this, 1.0D));
+        //this.tasks.addTask(7, new EntityAIWander(this, 1.0D));
+        this.tasks.addTask(2, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+        this.tasks.addTask(6, new EntityAILookIdle(this));
         this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
         this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, true));
         this.isImmuneToFire = true;
