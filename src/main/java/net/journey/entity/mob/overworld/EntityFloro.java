50c45c8635fae937f702a084049ddfea71665e53
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2020-05-12 21:11:01 -0700

    Improved floro attack AI

--- src/main/java/net/journey/entity/mob/overworld/EntityFloro.java
+++ src/main/java/net/journey/entity/mob/overworld/EntityFloro.java
@@ -34,11 +34,11 @@ public class EntityFloro extends EntityModMob implements IRangedAttackMob {
 
     public EntityFloro(World par1World) {
         super(par1World);
-        this.tasks.addTask(2, new EntityAIAttackRanged(this, 0.27F, 30, 10.0F));
         this.tasks.addTask(1, new EntityAISwimming(this));
+        this.tasks.addTask(2, new EntityAIAttackRanged(this, 0.27F, 50, 8.0F));
+        this.tasks.addTask(2, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
         this.tasks.addTask(3, new EntityAIAvoidEntity(this, EntityWolf.class, 6.0F, 1.0D, 1.2D));
         this.tasks.addTask(5, new EntityAIWanderAvoidWater(this, 1.0D));
-        this.tasks.addTask(6, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
         this.tasks.addTask(6, new EntityAILookIdle(this));
         this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, false, new Class[0]));
         this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, true));
@@ -60,8 +60,7 @@ public class EntityFloro extends EntityModMob implements IRangedAttackMob {
     public IEntityLivingData onInitialSpawn(DifficultyInstance difficulty, @Nullable IEntityLivingData livingdata) {
         livingdata = super.onInitialSpawn(difficulty, livingdata);
         this.setEquipmentBasedOnDifficulty(difficulty);
-        this.setEnchantmentBasedOnDifficulty(difficulty);
-        this.setCombatTask();
+        this.setEnchantmentBasedOnDifficulty(difficulty); 
         this.setCanPickUpLoot(this.rand.nextFloat() < 0.55F * difficulty.getClampedAdditionalDifficulty());
         return livingdata;
     }
