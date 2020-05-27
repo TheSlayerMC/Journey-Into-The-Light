9fbe8f90889bc3243ef1614bd4f417daa7f4a2b5
The_Slayer <brock.kerley@hotmail.com>
2020-05-27 10:24:24 +0930

    Update BlockModMobFlower.java

--- src/main/java/net/slayer/api/block/BlockModMobFlower.java
+++ src/main/java/net/slayer/api/block/BlockModMobFlower.java
@@ -21,55 +21,13 @@ import net.slayer.api.EnumMaterialTypes;
 import javax.annotation.Nullable;
 import java.util.Random;
 
-public class BlockModMobFlower extends BlockMod implements IPlantable {
-
-    protected static final AxisAlignedBB BUSH_AABB = new AxisAlignedBB(0.30000001192092896D, 0.0D, 0.30000001192092896D, 0.699999988079071D, 0.6000000238418579D, 0.699999988079071D);
-    private boolean damageWhenContact = false;
-    private boolean isFrozenPlant = false;
+public class BlockModMobFlower extends BlockModFlower implements IPlantable {
 
     public BlockModMobFlower(String name, String finalName) {
-        super(EnumMaterialTypes.PLANT, name, finalName, 0.0F);
-        this.setTickRandomly(true);
-        float f = 0.3F;
-        this.setCreativeTab(JourneyTabs.DECORATION);
+        super(name, finalName);
+      
     }
-
-    @Override
-    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
-        return BUSH_AABB;
-    }
-
-    @Override
-    @Nullable
-    public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
-        return NULL_AABB;
-    }
-
-    public BlockModMobFlower setContactDamage() {
-        damageWhenContact = true;
-        return this;
-    }
-
-    public BlockModMobFlower setFrozenPlant() {
-        isFrozenPlant = true;
-        return this;
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public void randomDisplayTick(IBlockState stateIn, World w, BlockPos pos, Random random) {
-        if (isFrozenPlant) {
-            if (random.nextInt(15) == 0) {
-                for (int i = 0; i < 6; ++i) {
-                    double d0 = (double) pos.getX() + random.nextDouble();
-                    double d1 = (double) pos.getY() + random.nextDouble() * 0.5D + 0.7D;
-                    double d2 = (double) pos.getZ() + random.nextDouble();
-                    w.spawnParticle(EnumParticleTypes.SNOW_SHOVEL, d0, d1, d2, 0.0D, 0.0D, 0.0D);
-                }
-            }
-        }
-    }
-
+    
     @Override
     public void onEntityCollision(World worldIn, BlockPos pos, IBlockState state, Entity entity) {
         if (!worldIn.isRemote) {
@@ -78,76 +36,5 @@ public class BlockModMobFlower extends BlockMod implements IPlantable {
             terra.setPosition(pos.getX(), pos.getY() + 0, pos.getZ());
             worldIn.setBlockState(pos.add(0, 0, 0), Blocks.AIR.getDefaultState());
         }
-    }
-
-    protected boolean canSustainBush(IBlockState state) {
-        return state.getBlock() == Blocks.GRASS || state.getBlock() == Blocks.DIRT || state.getBlock() == Blocks.FARMLAND;
-    }
-
-    @Override
-    public void neighborChanged(IBlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos) {
-        super.neighborChanged(state, worldIn, pos, blockIn, fromPos);
-        this.checkAndDropBlock(worldIn, pos, state);
-    }
-
-    @Override
-    public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
-        this.checkAndDropBlock(worldIn, pos, state);
-    }
-
-    protected void checkAndDropBlock(World worldIn, BlockPos pos, IBlockState state) {
-        if (!this.canBlockStay(worldIn, pos, state)) {
-            this.dropBlockAsItem(worldIn, pos, state, 0);
-            worldIn.setBlockState(pos, Blocks.AIR.getDefaultState(), 3);
-        }
-    }
-
-    public boolean canBlockStay(World worldIn, BlockPos pos, IBlockState state) {
-        if (state.getBlock() == this) {
-            IBlockState soil = worldIn.getBlockState(pos.down());
-            return soil.getBlock().canSustainPlant(soil, worldIn, pos.down(), net.minecraft.util.EnumFacing.UP, this);
-        }
-        return this.canSustainBush(worldIn.getBlockState(pos.down()));
-    }
-
-    public boolean canBlockStay(World w, BlockPos pos) {
-        return canPlaceBlockAt(w, pos);
-    }
-
-    @Override
-    public BlockModMobFlower setLightLevel(float value) {
-        this.lightValue = (int) (15.0F * value);
-        return this;
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public BlockRenderLayer getRenderLayer() {
-        return BlockRenderLayer.CUTOUT;
-    }
-
-    @Override
-    public boolean isOpaqueCube(IBlockState state) {
-        return false;
-    }
-
-    @Override
-    public boolean isFullCube(IBlockState state) {
-        return false;
-    }
-
-    @Override
-    public EnumPlantType getPlantType(IBlockAccess world, BlockPos pos) {
-        return EnumPlantType.Plains;
-    }
-
-    @Override
-    public IBlockState getPlant(IBlockAccess world, BlockPos pos) {
-        return getDefaultState();
-    }
-
-    @Override
-    public boolean isReplaceable(IBlockAccess worldIn, BlockPos pos) {
-        return true;
     }
 }
