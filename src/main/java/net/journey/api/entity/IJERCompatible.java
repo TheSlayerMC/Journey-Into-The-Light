00b6d3d9b45b3effe546494363d40d8bf46ef9cc
TimeConqueror <timeconqueror999@gmail.com>
2020-06-01 01:55:43 +0300

    Fixed mushroom generation Added comment to JERCompatible

--- src/main/java/net/journey/api/entity/IJERCompatible.java
+++ src/main/java/net/journey/api/entity/IJERCompatible.java
@@ -1,8 +1,12 @@
 package net.journey.api.entity;
 
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.ResourceLocation;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * Can be applied to any entity, that extends {@link EntityLivingBase} to provide Just Enough Resources compatibility.
+ */
 public interface IJERCompatible {
 	@Nullable
 	ResourceLocation getLootTable();
