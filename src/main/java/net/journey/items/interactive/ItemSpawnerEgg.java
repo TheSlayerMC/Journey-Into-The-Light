a76700a8768aef843b761b85e8834252f7f3a3b2
TimeConqueror <timeconqueror999@gmail.com>
2020-06-02 20:41:55 +0300

    Finished merge

--- src/main/java/net/journey/items/interactive/ItemSpawnerEgg.java
+++ src/main/java/net/journey/items/interactive/ItemSpawnerEgg.java
@@ -1,9 +1,7 @@
 package net.journey.items.interactive;
 
-import java.lang.reflect.InvocationTargetException;
-
 import net.journey.init.JourneyTabs;
-import net.minecraft.creativetab.CreativeTabs;
+import net.journey.items.base.JItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
@@ -11,9 +9,8 @@ import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.slayer.api.entity.EntityJourneyPet;
-import net.slayer.api.item.ItemMod;
 
-public class ItemSpawnerEgg extends ItemMod {
+public class ItemSpawnerEgg extends JItem {
 
 	private Class<? extends EntityJourneyPet> pet;
 
@@ -27,7 +24,7 @@ public class ItemSpawnerEgg extends ItemMod {
 	public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
 		try {
 			EntityJourneyPet spawn = pet.getConstructor(World.class, EntityPlayer.class).newInstance(worldIn, player);
-			if(!worldIn.isRemote) {
+			if (!worldIn.isRemote) {
 				player.getHeldItem(hand).shrink(1);
 				spawn.setLocationAndAngles(pos.getX() + 0.5F, pos.up(1).getY(), pos.getZ() + 0.5F, 0.0F, 0.0F);
 				worldIn.spawnEntity(spawn);
