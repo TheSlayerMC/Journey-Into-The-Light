a3aa57dc4a99c4116fc39d942c84740e15aa59e2
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2020-07-04 15:43:05 -0700

    started custom mud particle

--- src/main/java/net/journey/entity/projectile/EntityFloroDirtProjectile.java
+++ src/main/java/net/journey/entity/projectile/EntityFloroDirtProjectile.java
@@ -1,9 +1,13 @@
 package net.journey.entity.projectile;
 
+import net.journey.client.render.particles.ParticleFloroMud;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.client.FMLClientHandler;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import org.jetbrains.annotations.ApiStatus;
 
 public class EntityFloroDirtProjectile extends EntityDamagingProjectile {
@@ -15,6 +19,16 @@ public class EntityFloroDirtProjectile extends EntityDamagingProjectile {
 
 	public EntityFloroDirtProjectile(World world, EntityLivingBase thrower, float damage) {
 		super(world, thrower, damage);
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void onUpdate() {
+		super.onUpdate();
+		for (int i = 0; i < 6; ++i) {
+			ParticleFloroMud effect = new ParticleFloroMud(this.world, this.posX, this.posY, this.posZ, 0.0D, 0.0D, 0.0D);
+			FMLClientHandler.instance().getClient().effectRenderer.addEffect(effect);
+		}
 	}
 
 	@Override
