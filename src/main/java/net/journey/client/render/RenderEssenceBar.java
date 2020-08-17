ecf6de3034942711b82bcc1cfcdb8b696a168d4c
TimeConqueror <timeconqueror999@gmail.com>
2020-08-17 14:30:58 +0300

    Improved RenderEssenceBar code

--- src/main/java/net/journey/client/render/RenderEssenceBar.java
+++ src/main/java/net/journey/client/render/RenderEssenceBar.java
@@ -12,6 +12,7 @@ import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.fml.client.FMLClientHandler;
@@ -25,44 +26,52 @@ public class RenderEssenceBar {
     @SubscribeEvent
     public void onRender(RenderGameOverlayEvent.Post event) {
         if (event.getType() == RenderGameOverlayEvent.ElementType.ALL) {
-        	if (Config.showManaBar == true) {
-                onTickRender(FMLClientHandler.instance().getClientPlayerEntity());
+        	if (Config.showManaBar) {
+        		Minecraft mc = Minecraft.getMinecraft();
+                renderManaBar(mc, mc.player);
         	}
         }
     }
 
-	private void onTickRender(EntityPlayer player) {
-		Minecraft mc = Minecraft.getMinecraft();
-		if (player != null) {
-			if (mc.currentScreen == null && player.getHeldItemMainhand() != null && instanceOfEssenceItem(player.getHeldItemMainhand().getItem()) && transparency < 1.0) {
-				transparency += .02;
-			} else if (transparency > 0) transparency -= .02;
-			if (transparency > 0) {
-				IEssence mana = player.getCapability(EssenceProvider.ESSENCE_CAP, null);
-				if (mana.getEssenceValue() < 10 && player.getHeldItemMainhand() != null && !instanceOfEssenceItem(player.getHeldItemMainhand().getItem())) {
-					transparency = 0.35F;
-				}
-				if (!mc.gameSettings.hideGUI) {
-					GL11.glPushMatrix();
-					GlStateManager.enableBlend();
-					GlStateManager.enableAlpha();
-					GL11.glColor4f(1.0F, 1.0F, 1.0F, transparency);
-					GuiIngame gig = mc.ingameGUI;
-					ScaledResolution scaledresolution = new ScaledResolution(mc);
-					mc.getTextureManager().bindTexture(TEXTURE);
-					int y = scaledresolution.getScaledHeight() - 30, x = 10;
-					gig.drawTexturedModalRect(x - 10, y + 10, 0, 177, 117, 19);
-					gig.drawTexturedModalRect(x - 6, y + 17, 0, 23, 109, 5);
-					for (int i = 0; i < mana.getEssenceValue(); i++) {
-						if (!(i >= 10)) {
-							x += 11;
-							gig.drawTexturedModalRect(x - 17, y + 17, 0, 0, 10, 5);
-						}
+	private void renderManaBar(Minecraft mc, EntityPlayer player) {
+		ItemStack heldItemMainhand = player.getHeldItemMainhand();
+
+		if (mc.currentScreen == null && instanceOfEssenceItem(heldItemMainhand.getItem()) && transparency < 1.0) {
+			transparency += .02;
+		} else if (transparency > 0) {
+			transparency -= .02;
+		}
+
+		if (transparency > 0) {
+			IEssence mana = player.getCapability(EssenceProvider.ESSENCE_CAP, null);
+
+			if (mana.getEssenceValue() < 10 && !instanceOfEssenceItem(heldItemMainhand.getItem())) {
+				transparency = 0.35F;
+			}
+
+			if (!mc.gameSettings.hideGUI) {
+				GlStateManager.pushMatrix();
+				GlStateManager.enableBlend();
+				GlStateManager.enableAlpha();
+
+				GL11.glColor4f(1.0F, 1.0F, 1.0F, transparency);
+				GuiIngame gig = mc.ingameGUI;
+				ScaledResolution scaledresolution = new ScaledResolution(mc);
+				mc.getTextureManager().bindTexture(TEXTURE);
+				int y = scaledresolution.getScaledHeight() - 30;
+				int x = 10;
+				gig.drawTexturedModalRect(x - 10, y + 10, 0, 177, 117, 19);
+				gig.drawTexturedModalRect(x - 6, y + 17, 0, 23, 109, 5);
+				for (int i = 0; i < mana.getEssenceValue(); i++) {
+					if (!(i >= 10)) {
+						x += 11;
+						gig.drawTexturedModalRect(x - 17, y + 17, 0, 0, 10, 5);
 					}
-					GlStateManager.disableAlpha();
-					GlStateManager.disableBlend();
-					GL11.glPopMatrix();
 				}
+
+				GlStateManager.disableAlpha();
+				GlStateManager.disableBlend();
+				GlStateManager.popMatrix();
 			}
 		}
 	}
