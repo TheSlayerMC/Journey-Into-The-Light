83754e6ce4f74853799320f04df9ba574848cc8c
DiamondWalker <19913710+DiamondWalker@users.noreply.github.com>
2020-08-20 02:01:19 -0400

    Sentry Grenade now has a timer
    also includes new constructor variants for controlling explosion time

--- src/main/java/net/journey/entity/projectile/EntitySentryKingGrenade.java
+++ src/main/java/net/journey/entity/projectile/EntitySentryKingGrenade.java
@@ -19,6 +19,7 @@ import javax.annotation.Nullable;
 import java.util.List;
 
 public class EntitySentryKingGrenade extends EntityTippedArrow implements IProjectile {
+	private int grenadeTimer;
 
 	private static final Predicate<Entity> ARROW_TARGETS = Predicates.and(EntitySelectors.NOT_SPECTATING, EntitySelectors.IS_ALIVE, new Predicate<Entity>() {
 		public boolean apply(@Nullable Entity e) {
@@ -29,18 +30,42 @@ public class EntitySentryKingGrenade extends EntityTippedArrow implements IProje
 	public EntitySentryKingGrenade(World worldIn) {
 		super(worldIn);
 		this.pickupStatus = PickupStatus.DISALLOWED;
+		this.grenadeTimer = 200;
+
+	}
+	
+	public EntitySentryKingGrenade(World worldIn, int grenadeTime) {
+		super(worldIn);
+		this.pickupStatus = PickupStatus.DISALLOWED;
+		this.grenadeTimer = grenadeTime;
 
 	}
 
 	public EntitySentryKingGrenade(World worldIn, EntityLivingBase shooter) {
 		super(worldIn, shooter);
 		this.pickupStatus = PickupStatus.DISALLOWED;
+		this.grenadeTimer = 200;
+
+	}
+	
+	public EntitySentryKingGrenade(World worldIn, EntityLivingBase shooter, int grenadeTime) {
+		super(worldIn, shooter);
+		this.pickupStatus = PickupStatus.DISALLOWED;
+		this.grenadeTimer = grenadeTime;
 
 	}
 
 	public EntitySentryKingGrenade(World worldIn, double x, double y, double z) {
 		super(worldIn, x, y, z);
 		this.pickupStatus = PickupStatus.DISALLOWED;
+		this.grenadeTimer = 200;
+
+	}
+	
+	public EntitySentryKingGrenade(World worldIn, double x, double y, double z, int grenadeTime) {
+		super(worldIn, x, y, z);
+		this.pickupStatus = PickupStatus.DISALLOWED;
+		this.grenadeTimer = grenadeTime;
 
 	}
 
@@ -48,6 +73,31 @@ public class EntitySentryKingGrenade extends EntityTippedArrow implements IProje
 		super(worldIn);
 		Entity.setRenderDistanceWeight(10.0D);
 		this.shootingEntity = e;
+		this.grenadeTimer = 200;
+
+		this.posY = e.posY + e.getEyeHeight() - 0.10000000149011612D;
+		double d0 = eb.posX - e.posX;
+		double d1 = eb.getEntityBoundingBox().minY + eb.height / 3.0F - this.posY;
+		double d2 = eb.posZ - e.posZ;
+		double d3 = MathHelper.sqrt(d0 * d0 + d2 * d2);
+
+		if (d3 >= 1.0E-7D) {
+			float f2 = (float) (Math.atan2(d2, d0) * 180.0D / Math.PI) - 90.0F;
+			float f3 = (float) (-(Math.atan2(d1, d3) * 180.0D / Math.PI));
+			double d4 = d0 / d3;
+			double d5 = d2 / d3;
+			this.setLocationAndAngles(e.posX + d4, this.posY, e.posZ + d5, f2, f3);
+			float f4 = (float) (d3 * 0.20000000298023224D);
+			this.shoot(d0, d1 + f4, d2, f, f1);
+		}
+		this.pickupStatus = EntityArrow.PickupStatus.ALLOWED;
+	}
+	
+	public EntitySentryKingGrenade(World worldIn, EntityLivingBase e, EntityLivingBase eb, float f, float f1, int grenadeTime) {
+		super(worldIn);
+		Entity.setRenderDistanceWeight(10.0D);
+		this.shootingEntity = e;
+		this.grenadeTimer = grenadeTime;
 
 		this.posY = e.posY + e.getEyeHeight() - 0.10000000149011612D;
 		double d0 = eb.posX - e.posX;
@@ -78,6 +128,15 @@ public class EntitySentryKingGrenade extends EntityTippedArrow implements IProje
 			}
 		}
 		this.playSound(JourneySounds.BOTTLE_PLUG, 1.0F, 1.2F / (this.rand.nextFloat() * 0.2F + 0.9F));
+	}
+	
+	@Override
+	public void onUpdate() {
+		if (!world.isRemote && ticksExisted > grenadeTimer) {
+			world.createExplosion(this, posX, posY, posZ, 2.0F, true);
+			this.setDead();
+		}
+		super.onUpdate();
 	}
 
 	@Override
