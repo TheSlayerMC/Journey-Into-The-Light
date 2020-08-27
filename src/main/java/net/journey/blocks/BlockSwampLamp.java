eb6179a692becf754e0d7e19c96191414d34fc61
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2020-08-26 18:58:10 -0700

    Swamp fly bottle has particle effect

--- src/main/java/net/journey/blocks/BlockSwampLamp.java
+++ src/main/java/net/journey/blocks/BlockSwampLamp.java
@@ -1,6 +1,8 @@
 package net.journey.blocks;
 
+import net.journey.client.render.particles.ParticleSwampFly;
 import net.journey.init.JourneyTabs;
+import net.journey.util.WorldUtils;
 import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.BlockRenderLayer;
@@ -14,26 +16,39 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import net.slayer.api.EnumMaterialTypes;
 import net.slayer.api.block.BlockMod;
 
+import java.util.Random;
+
 public class BlockSwampLamp extends BlockMod {
 
-    protected String tex;
+	protected String tex;
 
-    public BlockSwampLamp(String name, String finalName, float hardness) {
-        super(EnumMaterialTypes.GLASS, name, finalName, hardness);
-        setCreativeTab(JourneyTabs.DECORATION);
-        setTickRandomly(true);
-        setLightLevel(0.5F);
-    }
+	public BlockSwampLamp(String name, String finalName, float hardness) {
+		super(EnumMaterialTypes.GLASS, name, finalName, hardness);
+		setCreativeTab(JourneyTabs.DECORATION);
+		setTickRandomly(true);
+		setLightLevel(0.5F);
+	}
 
-    @Override
-    public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
-        float f = 0.0625F;
-        return new AxisAlignedBB(pos.getX() + (5 * f), pos.getY(), pos.getZ() + (5 * f), pos.getX() + 1 - (5 * f), pos.getY() + 1.2D - (10 * f), pos.getZ() + 1 - (5 * f));
-    }
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void randomDisplayTick(IBlockState stateIn, World w, BlockPos pos, Random rand) {
+		float x = pos.getX() + .5F;
+		float y = pos.getY() + .2F;
+		float z = pos.getZ() + .5F;
+		for (int l = 0; l < 6; ++l) {
+			WorldUtils.spawnParticle(new ParticleSwampFly(w, x, y, z, 1.0F, 1.0F, 1.0F), w);
+		}
+	}
 
-    @Override
-    @SideOnly(Side.CLIENT)
-    public AxisAlignedBB getSelectedBoundingBox(IBlockState state, World worldIn, BlockPos pos) {
+	@Override
+	public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
+		float f = 0.0625F;
+		return new AxisAlignedBB(pos.getX() + (5 * f), pos.getY(), pos.getZ() + (5 * f), pos.getX() + 1 - (5 * f), pos.getY() + 1.2D - (10 * f), pos.getZ() + 1 - (5 * f));
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public AxisAlignedBB getSelectedBoundingBox(IBlockState state, World worldIn, BlockPos pos) {
         float f = 0.0625F;
         return new AxisAlignedBB(pos.getX() + (5 * f), pos.getY(), pos.getZ() + (5 * f), pos.getX() + 1 - (5 * f), pos.getY() + 1.2D - (10 * f), pos.getZ() + 1 - (5 * f));
     }
