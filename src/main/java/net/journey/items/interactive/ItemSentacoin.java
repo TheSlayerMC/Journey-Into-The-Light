35b806c361ea0fa3d5f82ca3661f3a52dab6f9f1
The_Slayer <brock.kerley@hotmail.com>
2020-09-07 08:51:46 +0930

    added update for sentacoin

--- src/main/java/net/journey/items/interactive/ItemSentacoin.java
+++ src/main/java/net/journey/items/interactive/ItemSentacoin.java
@@ -1,10 +1,12 @@
 package net.journey.items.interactive;
 
+import net.journey.api.capability.JourneyPlayer;
 import net.journey.api.capability.PlayerStats;
 import net.journey.common.capability.JCapabilityManager;
 import net.journey.items.base.JItem;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
@@ -21,7 +23,9 @@ public class ItemSentacoin extends JItem {
 			if(e instanceof EntityPlayer) {
 				EntityPlayer player = (EntityPlayer) e;
 				PlayerStats stats = JCapabilityManager.asJourneyPlayer(player).getPlayerStats();
+	            JourneyPlayer journeyPlayer = JCapabilityManager.asJourneyPlayer(player);
 				stats.addSentacoin(1);
+				journeyPlayer.sendUpdates(((EntityPlayerMP)e));
 				System.out.println(stats.getSentacoinValue());
 				player.inventory.decrStackSize(itemSlot, 1);
 			}
