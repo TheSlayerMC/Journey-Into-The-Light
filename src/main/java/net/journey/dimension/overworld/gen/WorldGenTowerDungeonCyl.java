a19a713a2ecb1795aeb9e217456a822c4fc67dd4
TimeConqueror <timeconqueror999@gmail.com>
2020-09-17 03:01:32 +0300

    Greatly optimized WorldGenTowerDungeonCyl generation

--- src/main/java/net/journey/dimension/overworld/gen/WorldGenTowerDungeonCyl.java
+++ src/main/java/net/journey/dimension/overworld/gen/WorldGenTowerDungeonCyl.java
@@ -1,88 +1,41 @@
 package net.journey.dimension.overworld.gen;
 
-import net.journey.blocks.containers.BlockJourneyChest;
-import net.journey.blocks.tileentity.TileEntityJourneyChest;
-import net.journey.entity.mob.boss.EntityTempleGuardian;
-import net.journey.init.JourneyLootTables;
 import net.journey.init.blocks.JourneyBlocks;
 import net.journey.util.RandHelper;
-import net.journey.util.handler.LogHelper;
-import net.minecraft.init.Blocks;
-import net.minecraft.tileentity.TileEntityMobSpawner;
+import net.journey.util.WorldGenHelper;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.EnumFacing;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenerator;
-import net.slayer.api.worldgen.WorldGenAPI;
 
-import java.util.ArrayList;
 import java.util.Random;
 
 public class WorldGenTowerDungeonCyl extends WorldGenerator {
 
-    @Override
-    public boolean generate(World w, Random r, BlockPos pos) {
-        int x = pos.getX(), y = pos.getY(), z = pos.getZ();
-        int levels = 4;
-        int height = levels * 4;
-        
-        WorldGenAPI.addHollowCylinder(w, 4, 6, r, x, y, z, JourneyBlocks.dungeonBrick);
+	@Override
+	public boolean generate(World worldIn, Random rand, BlockPos position) {
+		int levels = 4;
+		int height = levels * 4;
 
-        addDifferentBlocks(w, x, y, z);
-        return true;
-    }
-    
-    public void addLevel(World w, int x, int y, int z, int height, int lor, String mobName, String mobName2) {
-    	
-    }
+		WorldGenHelper.genHollowCylinder(position, 6, 4, EnumFacing.UP, mutablePos -> {
+			IBlockState state = JourneyBlocks.dungeonBrick.getDefaultState();
 
-    public void addDifferentBlocks(World w, int x, int y, int z) {
-        Random r = new Random();
-        for (int x1 = 0; x1 < 15; x1++) {
-            for (int y1 = 0; y1 < 50; y1++) {
-                for (int z1 = 0; z1 < 15; z1++) {
-                    if (w.getBlockState(new BlockPos(x + x1, y + y1, z + z1)) == JourneyBlocks.dungeonBrick.getDefaultState() && r.nextInt(15) == 0) {
-                        this.setBlockAndNotifyAdequately(w, new BlockPos(x + x1, y + y1, z + z1), JourneyBlocks.dungeonChisledBrick.getDefaultState());
-                        break;
-                    }
-                }
-            }
-        }
+			if (rand.nextInt(15) == 0) {
+				state = JourneyBlocks.dungeonChisledBrick.getDefaultState();
+			}
+			if (rand.nextInt(10) == 0) {
+				state = RandHelper.chooseEqual(rand, JourneyBlocks.dungeonBrickCarved, JourneyBlocks.dungeonCrackedBrick)
+						.getDefaultState();
+			}
 
-        for (int x1 = 0; x1 < 15; x1++) {
-            for (int y1 = 0; y1 < 50; y1++) {
-                for (int z1 = 0; z1 < 15; z1++) {
-                    if (w.getBlockState(new BlockPos(x + x1, y + y1, z + z1)) == JourneyBlocks.dungeonBrick.getDefaultState() && r.nextInt(10) == 0) {
-                        this.setBlockAndNotifyAdequately(w, new BlockPos(x + x1, y + y1, z + z1), JourneyBlocks.dungeonBrickCarved.getDefaultState());
-                        break;
-                    }
-                }
-            }
-        }
+			WorldGenHelper.setStateFast(worldIn, mutablePos, state);
+		});
 
-        for (int x1 = 0; x1 < 15; x1++) {
-            for (int y1 = 0; y1 < 50; y1++) {
-                for (int z1 = 0; z1 < 15; z1++) {
-                    if (w.getBlockState(new BlockPos(x + x1, y + y1, z + z1)) == JourneyBlocks.dungeonBrick.getDefaultState() && r.nextInt(10) == 0) {
-                        this.setBlockAndNotifyAdequately(w, new BlockPos(x + x1, y + y1, z + z1), JourneyBlocks.dungeonCrackedBrick.getDefaultState());
-                        break;
-                    }
-                }
-            }
-        }
+		return true;
+	}
 
-        for (int i = 0; i < 50; i++) {
-            for (int j = 0; j < 50; j++) {
-                for (int k = 0; k < 50; k++) {
-                    if (w.getBlockState(new BlockPos(i, j, k)) == JourneyBlocks.dungeonBrick.getDefaultState()) {
-                        if (r.nextInt(10) == 0)
-                            this.setBlockAndNotifyAdequately(w, new BlockPos(i, j, k), JourneyBlocks.dungeonCrackedBrick.getDefaultState());
-                    }
-                }
-            }
-        }
-    }
+	public void addLevel(World w, int x, int y, int z, int height, int lor, String mobName, String mobName2) {
 
-   
+	}
 }
--- src/main/java/net/journey/items/ItemTestBug.java
+++ src/main/java/net/journey/items/ItemTestBug.java
@@ -2,15 +2,13 @@ package net.journey.items;
 
 import net.journey.dialogue.DialogueManager;
 import net.journey.dialogue.DialogueNode;
+import net.journey.dimension.overworld.gen.WorldGenTowerDungeonCyl;
 import net.journey.items.base.JItem;
-import net.journey.util.WorldGenHelper;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
 import ru.timeconqueror.timecore.util.reflection.ReflectionHelper;
@@ -23,9 +21,9 @@ public class ItemTestBug extends JItem {
 	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand handIn) {
 		if (!world.isRemote) {
 			//mStartDialogue.invoke(JManagers.DIALOGUE_MANAGER, player, EntityTordo.class, JDialogues.TEST.getRootNode());
-//			new WorldGenTowerDungeonCyl().generate(world, itemRand, player.getPosition());
+			new WorldGenTowerDungeonCyl().generate(world, itemRand, player.getPosition());
 
-			WorldGenHelper.genHollowCylinder(player.getPosition(), 5, 3, EnumFacing.NORTH, mPos -> WorldGenHelper.setStateFast(world, mPos, Blocks.BEDROCK.getDefaultState()));
+//			WorldGenHelper.genHollowCylinder(player.getPosition(), 5, 3, EnumFacing.NORTH, mPos -> WorldGenHelper.setStateFast(world, mPos, Blocks.BEDROCK.getDefaultState()));
 		}
 
 		return new ActionResult<>(EnumActionResult.SUCCESS, player.getHeldItem(handIn));
