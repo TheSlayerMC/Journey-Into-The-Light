f9539e7a4a275db327a614347363bfc03401593d
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2020-11-06 14:12:04 -0700

    ore features don't feel like generating

--- src/main/java/net/jitl/Registration.java
+++ src/main/java/net/jitl/Registration.java
@@ -1,8 +1,10 @@
 package net.jitl;
 
 import net.jitl.init.JourneyItems;
+import net.jitl.world.gen.JOreGen;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+import net.minecraft.world.gen.feature.Feature;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -11,11 +13,14 @@ public class Registration {
 
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, JITL.MODID);
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, JITL.MODID);
+    public static final DeferredRegister<Feature<?>> FEATURES = DeferredRegister.create(ForgeRegistries.FEATURES, JITL.MODID);
 
     public static void register(IEventBus bus) {
+        FEATURES.register(bus);
         BLOCKS.register(bus);
         ITEMS.register(bus);
 
+        JOreGen.init();
         JourneyItems.register();
     }
 }
--- src/main/java/net/jitl/world/gen/JOreGen.java
+++ src/main/java/net/jitl/world/gen/JOreGen.java
@@ -1,23 +1,35 @@
 package net.jitl.world.gen;
 
-import net.jitl.registry.JBlocks;
+import net.jitl.JITL;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.WorldGenRegistries;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
 import net.minecraft.world.gen.feature.template.BlockMatchRuleTest;
 import net.minecraft.world.gen.feature.template.RuleTest;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
 
 public class JOreGen {
 
 	private static final RuleTest STONE = new BlockMatchRuleTest(Blocks.STONE);
 
-	public static final ConfiguredFeature<?, ?> ORE_SAPPHIRE = Feature.ORE.configured(new OreFeatureConfig(STONE, JBlocks.SAPPHIRE_ORE.defaultBlockState(), 4)).range(24).chance(16).count(4);
+	public static void init() {
+		registerConfigFeatures();
+	}
 
-	public static void register(IEventBus bus) {
-		DeferredRegister.create(ForgeRegistries.FEATURES, "gen_sapphire_ore").register(bus);
+	private static void registerConfigFeatures() {
+		Registry.register(WorldGenRegistries.CONFIGURED_FEATURE, new ResourceLocation(JITL.MODID, "sapphire_ore_feature"), defaultOreFeature(
+				Blocks.DIAMOND_ORE.defaultBlockState(),
+				STONE,
+				12,
+				128,
+				20));
+	}
+
+	private static ConfiguredFeature<?, ?> defaultOreFeature(BlockState ore, RuleTest spawnBlock, int size, int range, int count) {
+		return Feature.ORE.configured(new OreFeatureConfig(spawnBlock, ore, size)).range(range).squared().count(count);
 	}
 }
