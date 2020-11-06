32664a0db68414e4d6bc02fce8304ea8fe5afab4
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2020-11-06 14:40:44 -0700

    still nothing

--- src/main/java/net/jitl/world/gen/JOreGen.java
+++ src/main/java/net/jitl/world/gen/JOreGen.java
@@ -8,6 +8,7 @@ import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.WorldGenRegistries;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.IFeatureConfig;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
 import net.minecraft.world.gen.feature.template.BlockMatchRuleTest;
 import net.minecraft.world.gen.feature.template.RuleTest;
@@ -16,20 +17,23 @@ public class JOreGen {
 
 	private static final RuleTest STONE = new BlockMatchRuleTest(Blocks.STONE);
 
-	public static void init() {
-		registerConfigFeatures();
-	}
+	public static final ConfiguredFeature<?, ?> ORE_SAPPHIRE =
+			register("ore_sapphire", defaultOreFeature(
+					Blocks.DIAMOND_ORE.defaultBlockState(),
+					STONE,
+					12,
+					128,
+					20));
 
-	private static void registerConfigFeatures() {
-		Registry.register(WorldGenRegistries.CONFIGURED_FEATURE, new ResourceLocation(JITL.MODID, "sapphire_ore_feature"), defaultOreFeature(
-				Blocks.DIAMOND_ORE.defaultBlockState(),
-				STONE,
-				12,
-				128,
-				20));
+	public static void init() {
 	}
 
 	private static ConfiguredFeature<?, ?> defaultOreFeature(BlockState ore, RuleTest spawnBlock, int size, int range, int count) {
 		return Feature.ORE.configured(new OreFeatureConfig(spawnBlock, ore, size)).range(range).squared().count(count);
 	}
+
+	private static <FC extends IFeatureConfig> ConfiguredFeature<FC, ?> register(String name, ConfiguredFeature<FC, ?> configuredFeature_) {
+		return Registry.register(WorldGenRegistries.CONFIGURED_FEATURE, new ResourceLocation(JITL.MODID, name), configuredFeature_);
+	}
+
 }
