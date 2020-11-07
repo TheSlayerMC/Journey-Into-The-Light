77321ab27a8806c5afb83e3c8fb3432f91efa8c7
TimeConqueror <timeconqueror999@gmail.com>
2020-11-07 13:58:39 +0300

    Removed useless field

--- src/main/java/net/jitl/Registration.java
+++ src/main/java/net/jitl/Registration.java
@@ -1,7 +1,6 @@
 package net.jitl;
 
 import net.jitl.init.JourneyItems;
-import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -9,14 +8,11 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class Registration {
-
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, JITL.MODID);
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, JITL.MODID);
     public static final DeferredRegister<Feature<?>> FEATURES = DeferredRegister.create(ForgeRegistries.FEATURES, JITL.MODID);
 
     public static void register(IEventBus bus) {
         FEATURES.register(bus);
-        BLOCKS.register(bus);
         ITEMS.register(bus);
         JourneyItems.register();
     }
