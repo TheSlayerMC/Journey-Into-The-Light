be473414d6f27ae12b001750d2764ee2a611980e
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2020-11-08 14:29:47 -0700

    loot table data generator

---
+++ src/main/java/net/jitl/datagen/JLootTableSet.java
@@ -0,0 +1,19 @@
+package net.jitl.datagen;
+
+import net.jitl.init.JBlocks;
+import net.jitl.init.JItems;
+import net.minecraft.loot.ItemLootEntry;
+import net.minecraft.loot.conditions.ILootCondition;
+import net.minecraft.loot.conditions.RandomChance;
+import ru.timeconqueror.timecore.devtools.gen.loottable.BlockLootTableSet;
+
+public class JLootTableSet extends BlockLootTableSet {
+
+	private final ILootCondition.IBuilder FLIP_COIN_CHANCE = RandomChance.randomChance(.5F);
+
+	@Override
+	public void register() {
+		//test
+		registerLootTable(JBlocks.ENDERILLIUM_ORE, block -> createSelfDropDispatchTable(block, FLIP_COIN_CHANCE, ItemLootEntry.lootTableItem(JItems.LUNIUM_INGOT.get())));
+	}
+}
