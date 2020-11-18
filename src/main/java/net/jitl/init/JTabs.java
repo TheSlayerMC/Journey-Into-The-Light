9ad82d298164df380b7d50dde28b0969ed12e057
The_Slayer <brock.kerley@hotmail.com>
2020-11-19 08:17:09 +1030

    Update JTabs.java

--- src/main/java/net/jitl/init/JTabs.java
+++ src/main/java/net/jitl/init/JTabs.java
@@ -7,70 +7,70 @@ import net.minecraft.item.Items;
 
 public class JTabs {
 
-    public static final ItemGroup BLOCKS = new ItemGroup("journey.blocks") {
+    public static final ItemGroup BLOCKS = new ItemGroup("jitl.blocks") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(JBlocks.SHADIUM_ORE);
         }
     };
 
-    public static final ItemGroup DECORATION = new ItemGroup("journey.decoration") {
+    public static final ItemGroup DECORATION = new ItemGroup("jitl.decoration") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(JBlocks.DUNGEON_LAMP);
         }
     };
 
-    public static final ItemGroup PORTALS = new ItemGroup("journey.portals") {
+    public static final ItemGroup PORTALS = new ItemGroup("jitl.portals") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(Items.FLINT_AND_STEEL);
         }
     };
 
-    public static final ItemGroup ITEMS = new ItemGroup("journey.items") {
+    public static final ItemGroup ITEMS = new ItemGroup("jitl.items") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(JItems.SAPPHIRE);
         }
     };
 
-    public static final ItemGroup TOOLS = new ItemGroup("journey.tools") {
+    public static final ItemGroup TOOLS = new ItemGroup("jitl.tools") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(Items.DIAMOND_PICKAXE);
         }
     };
 
-    public static final ItemGroup ARMOR = new ItemGroup("journey.armor") {
+    public static final ItemGroup ARMOR = new ItemGroup("jitl.armor") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(Items.DIAMOND_HELMET);
         }
     };
 
-    public static final ItemGroup WEAPONS = new ItemGroup("journey.weapons") {
+    public static final ItemGroup WEAPONS = new ItemGroup("jitl.weapons") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(JItems.LUNIUM_SWORD);
         }
     };
 
-    public static final ItemGroup RANGED_WEAPONS = new ItemGroup("journey.ranged_weapons") {
+    public static final ItemGroup RANGED_WEAPONS = new ItemGroup("jitl.ranged_weapons") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(Items.BOW);
         }
     };
 
-    public static final ItemGroup MISC = new ItemGroup("journey.misc") {
+    public static final ItemGroup MISC = new ItemGroup("jitl.misc") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(JItems.TEST_BUG);
         }
     };
 
-    public static final ItemGroup SPAWNERS = new ItemGroup("journey.spawners") {
+    public static final ItemGroup SPAWNERS = new ItemGroup("jitl.spawners") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(Blocks.SPAWNER);
