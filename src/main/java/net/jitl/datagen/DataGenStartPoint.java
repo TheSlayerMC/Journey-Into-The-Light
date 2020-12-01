3fee9a111dba7a016aa91b3e3da53d5345b39e9d
TimeConqueror <timeconqueror999@gmail.com>
2020-12-01 14:33:59 +0300

    Upd TimeCore

--- src/main/java/net/jitl/datagen/DataGenStartPoint.java
+++ src/main/java/net/jitl/datagen/DataGenStartPoint.java
@@ -4,8 +4,7 @@ import net.minecraft.data.DataGenerator;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
-import ru.timeconqueror.timecore.devtools.gen.loottable.LootTableSet;
-import ru.timeconqueror.timecore.devtools.gen.loottable.TimeLootTableProvider;
+import ru.timeconqueror.timecore.devtools.gen.loottable.TimeLootTableGenerator;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class DataGenStartPoint {
@@ -14,17 +13,14 @@ public class DataGenStartPoint {
     public static void onDataGathering(GatherDataEvent event) {
         DataGenerator dataGenerator = event.getGenerator();
 
-        TimeLootTableProvider lootTableProvider = new TimeLootTableProvider(dataGenerator);
+        TimeLootTableGenerator lootTableGenerator = new TimeLootTableGenerator(dataGenerator)
+                .addSet(new JLootTableSet());
         JRecipeProvider recipeProvider = new JRecipeProvider(dataGenerator);
         JBiomeProvider biomeProvider = new JBiomeProvider(dataGenerator);
 
         //Adds data providers
-        dataGenerator.addProvider(lootTableProvider);
+        dataGenerator.addProvider(lootTableGenerator);
         dataGenerator.addProvider(recipeProvider);
         //dataGenerator.addProvider(biomeProvider);
-
-        //Adds loot table set to the loot table provider
-        LootTableSet set = new JLootTableSet();
-        lootTableProvider.addLootTableSet(set);
     }
 }
