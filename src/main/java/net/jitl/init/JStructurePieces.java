f784bba9e2922191f9d95e6648e618453654a5f4
TimeConqueror <timeconqueror999@gmail.com>
2020-12-12 22:10:32 +0300

    Upd TimeCore

--- src/main/java/net/jitl/init/JStructurePieces.java
+++ src/main/java/net/jitl/init/JStructurePieces.java
@@ -2,13 +2,14 @@ package net.jitl.init;
 
 import net.jitl.JITL;
 import net.jitl.common.world.gen.structures.GuardianTowerPieces;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.gen.feature.structure.IStructurePieceType;
 import ru.timeconqueror.timecore.registry.AutoRegistrable;
-import ru.timeconqueror.timecore.registry.newreg.StructurePieceRegister;
+import ru.timeconqueror.timecore.registry.newreg.VanillaRegister;
 
 public class JStructurePieces {
     @AutoRegistrable
-    private static final StructurePieceRegister REGISTER = new StructurePieceRegister(JITL.MODID);
+    private static final VanillaRegister<IStructurePieceType> REGISTER = new VanillaRegister<>(JITL.MODID, Registry.STRUCTURE_PIECE);
 
     public static final IStructurePieceType GUARDIAN_TOWER_PIECE = REGISTER.register("gt/p", GuardianTowerPieces.Piece::new);
     public static final IStructurePieceType GUARDIAN_TOWER_NO_GRASS_TOUCHED_PIECE = REGISTER.register("gt/ngtp", GuardianTowerPieces.NoGrassTouchedPiece::new);
