d0d4bc36b162b9326e155d055f8e861cf253c273
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2020-12-12 23:59:35 -0700

    fixed lunium sound quality

--- src/main/java/net/jitl/init/JSoundType.java
+++ src/main/java/net/jitl/init/JSoundType.java
@@ -7,7 +7,7 @@ public class JSoundType {
 
 	public static final SoundType MUD = new TimeSoundType(
 			0.8F,
-			1.2F,
+			1.3F,
 			JSounds.MUD_BLOCK_BREAK::get,
 			JSounds.MUD_BLOCK_DIG::get,
 			JSounds.MUD_BLOCK_BREAK::get,
@@ -16,7 +16,7 @@ public class JSoundType {
 
 	public static final SoundType LUNIUM_BLOCK = new TimeSoundType(
 			0.8F,
-			1.2F,
+			1.3F,
 			JSounds.LUNIUM_BLOCK_BREAK::get,
 			JSounds.LUNIUM_BLOCK_DIG::get,
 			JSounds.LUNIUM_BLOCK_BREAK::get,
