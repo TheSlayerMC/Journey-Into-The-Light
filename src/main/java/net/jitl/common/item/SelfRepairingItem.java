c73a53227603d80de1402d126b3dc95355f3cfd9
TimeConqueror <timeconqueror999@gmail.com>
2020-12-18 15:17:34 +0300

    Fixed slf repairing item

--- src/main/java/net/jitl/common/item/SelfRepairingItem.java
+++ src/main/java/net/jitl/common/item/SelfRepairingItem.java
@@ -6,37 +6,37 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
-import net.minecraft.world.LightType;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
 
 public class SelfRepairingItem extends Item {
-	public SelfRepairingItem(Properties properties) {
-		super(properties);
-	}
+    public SelfRepairingItem(Properties properties) {
+        super(properties);
+    }
 
-	//THIS IS SO FUCKING STUPID. HOLY SHIT.
-	@Override
-	public void inventoryTick(@NotNull ItemStack stack, @NotNull World worldIn, @NotNull Entity entityIn, int itemSlot, boolean isSelected) {
-		if (entityIn.tickCount % 20 == 0) {
-			System.out.println("Tick Count");
-			if (entityIn instanceof PlayerEntity) {
-				System.out.println("Entity Instance");
-				if (worldIn.isDay() && worldIn.getBrightness(LightType.SKY, entityIn.blockPosition()) > 15 /*THIS DOESN'T EVEN WORK??*/) {
-					System.out.println("Sky Light");
-					stack.hurt(-2, random, null);
-				}
-			}
-		}
-	}
+    @Override
+    public void inventoryTick(@NotNull ItemStack stack, @NotNull World worldIn, @NotNull Entity entityIn, int itemSlot, boolean isSelected) {
+        if (!worldIn.isClientSide() && worldIn.getGameTime() % 20 == 0) {
+            if (entityIn instanceof PlayerEntity) {
+                if (isDayTime(worldIn)) {
+                    stack.hurt(-2, random, null);
+                }
+            }
+        }
+    }
 
-	@Override
-	public ActionResult<ItemStack> use(World worldIn, PlayerEntity playerIn, Hand handIn) {
-		ItemStack itemstack = playerIn.getItemInHand(handIn);
-		//immediately resets to full value. what the fuck.
-		if (!worldIn.isClientSide) {
-			itemstack.setDamageValue(40);
-		}
-		return ActionResult.pass(itemstack);
-	}
+    public static boolean isDayTime(World world) {
+        final long time = world.getDayTime() % 24000L;
+        return time < 13000L || time > 23000L;
+    }
+
+    @Override
+    public ActionResult<ItemStack> use(World worldIn, PlayerEntity playerIn, Hand handIn) {
+        ItemStack itemstack = playerIn.getItemInHand(handIn);
+
+        if (!worldIn.isClientSide) {
+            itemstack.setDamageValue(40);
+        }
+        return ActionResult.pass(itemstack);
+    }
 }
