498d56b22db1acc2a962f703e56ef16a13bcb245
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2020-12-20 20:05:51 -0700

    Correctly (?) registered features, but it's not generating

--- src/main/java/net/jitl/init/JFeatures.java
+++ src/main/java/net/jitl/init/JFeatures.java
@@ -2,17 +2,19 @@ package net.jitl.init;
 
 import net.jitl.JITL;
 import net.jitl.common.world.gen.features.RuinsFeature;
-import net.minecraft.util.registry.Registry;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.Feature;
-import net.minecraft.world.gen.feature.IFeatureConfig;
 import net.minecraft.world.gen.feature.NoFeatureConfig;
-import net.minecraftforge.registries.ForgeRegistryEntry;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 
-public class JFeatures<FC extends IFeatureConfig> extends ForgeRegistryEntry<Feature<?>> {
+public class JFeatures {
 
-	public static final Feature<NoFeatureConfig> RUINS = register("overworld_ruins", new RuinsFeature(NoFeatureConfig.CODEC));
+	public static final DeferredRegister<Feature<?>> FEATURES = DeferredRegister.create(ForgeRegistries.FEATURES, JITL.MODID);
 
-	private static <C extends IFeatureConfig, F extends Feature<C>> F register(String key, F value) {
-		return Registry.register(Registry.FEATURE, JITL.MODID + key, value);
-	}
+	public static final RegistryObject<Feature<NoFeatureConfig>> RUINS = FEATURES.register(
+			"overworld_ruins", () -> new RuinsFeature(NoFeatureConfig.CODEC));
+
+	public static final ConfiguredFeature<?, ?> OVERWORLD_RUINS = RUINS.get().configured(NoFeatureConfig.INSTANCE).chance(1).count(5).squared();
 }
