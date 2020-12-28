f4f8c7a476ede571525c35859fdf968811ea21b7
TimeConqueror <timeconqueror999@gmail.com>
2020-12-29 02:07:34 +0300

    Missed overriden #use method in berry block

--- src/main/java/net/jitl/common/block/base/JBerryBushBlock.java
+++ src/main/java/net/jitl/common/block/base/JBerryBushBlock.java
@@ -4,10 +4,14 @@ import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.IGrowable;
 import net.minecraft.block.SweetBerryBushBlock;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.IItemProvider;
+import net.minecraft.item.Items;
+import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Supplier;
@@ -23,5 +27,22 @@ public class JBerryBushBlock extends SweetBerryBushBlock implements IGrowable {
 	@Override
 	public @NotNull ItemStack getCloneItemStack(@NotNull IBlockReader worldIn, @NotNull BlockPos pos, @NotNull BlockState state) {
 		return new ItemStack(berrySup.get());
+	}
+
+	//copypaste except for berrySup.get()
+	public ActionResultType use(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
+		int i = state.getValue(AGE);
+		boolean flag = i == 3;
+		if (!flag && player.getItemInHand(handIn).getItem() == Items.BONE_MEAL) {
+			return ActionResultType.PASS;
+		} else if (i > 1) {
+			int j = 1 + worldIn.random.nextInt(2);
+			popResource(worldIn, pos, new ItemStack(berrySup.get(), j + (flag ? 1 : 0)));
+			worldIn.playSound(null, pos, SoundEvents.SWEET_BERRY_BUSH_PICK_BERRIES, SoundCategory.BLOCKS, 1.0F, 0.8F + worldIn.random.nextFloat() * 0.4F);
+			worldIn.setBlock(pos, state.setValue(AGE, 1), 2);
+			return ActionResultType.sidedSuccess(worldIn.isClientSide);
+		} else {
+			return super.use(state, worldIn, pos, player, handIn, hit);
+		}
 	}
 }
