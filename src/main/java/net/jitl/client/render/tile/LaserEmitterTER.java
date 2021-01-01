d5875241e447ad1c2113b59ca397840250cd8447
TimeConqueror <timeconqueror999@gmail.com>
2021-01-01 20:07:47 +0300

    Laser: fixes and optimizing

--- src/main/java/net/jitl/client/render/tile/LaserEmitterTER.java
+++ src/main/java/net/jitl/client/render/tile/LaserEmitterTER.java
@@ -37,23 +37,33 @@ public class LaserEmitterTER extends TileEntityRenderer<LaserEmitterTile> {
 
         float gameTime = Animation.getWorldTime(world, partialTicks) * 20;
         float angleDegrees = gameTime % 360;
-        Quaternion rotationQuaternion = Vector3f.YP.rotationDegrees(angleDegrees);
+        Quaternion rotationQuaternion = Vector3f.YP.rotationDegrees(0);
 
         renderModel(Models.fullCube, JITL.rl("textures/block/laser_emitter.png"), rotationQuaternion, matrixStackIn, bufferIn, combinedLightIn, combinedOverlayIn);
 
         int maxDistance = 20;
-        Vector3f end = new Vector3f(maxDistance, 0.5F, 0);
-        end.transform(rotationQuaternion);
+        Vector3f beamEndVec = new Vector3f(maxDistance, 0.5F, 0);
+        beamEndVec.transform(rotationQuaternion);
 
-        BlockPos pos = tile.getBlockPos();
-        Vector3d posVec = VecUtils.vec3d(pos).add(0.5, 0.5, 0.5);
+        BlockPos emitterPos = tile.getBlockPos();
+        Vector3d posVec = VecUtils.vec3d(emitterPos).add(0.5, 0.5, 0.5);
 
-        Vector3d endNormalized = new Vector3d(end).normalize();
+        Vector3d endNormalized = new Vector3d(beamEndVec).normalize();
 
-        BlockRayTraceResult rayTraceResult = world.clip(new RayTraceContext(posVec.add(endNormalized.x, 0, endNormalized.z), posVec.add(end.x(), end.y(), end.z()), RayTraceContext.BlockMode.VISUAL, RayTraceContext.FluidMode.NONE, null));
+//        Vector3f testStart = new Vector3f(0.5F, 0, 0);
+//        testStart.transform(rotationQuaternion);
+//        testStart.add(0.5F, 0.5F, 0.5F);
+//        IVertexBuilder buffer = bufferIn.getBuffer(RenderType.lines());
+//        Matrix4f pose = matrixStackIn.last().pose();
+//        buffer.vertex(pose, testStart.x(), testStart.y(), testStart.z()).color(255, 255, 255, 255).endVertex();
+//        buffer.vertex(pose, testStart.x(),  testStart.y() + 16, testStart.z()).color(255, 255, 255, 255).endVertex();
+
+        BlockRayTraceResult rayTraceResult = world.clip(new RayTraceContext(posVec.add(endNormalized.x, 0, endNormalized.z), posVec.add(beamEndVec.x(), beamEndVec.y(), beamEndVec.z()), RayTraceContext.BlockMode.VISUAL, RayTraceContext.FluidMode.NONE, null));
         Vector3d endPos = rayTraceResult.getLocation();
 
-        renderBeam(pos, gameTime, matrixStackIn, bufferIn, new Vector3d(0.5, 0.5, 0.5), endPos.subtract(VecUtils.vec3d(pos)));
+//        System.out.println("endPos.subtract(VecUtils.vec3d(emitterPos)) = " + endPos.subtract(VecUtils.vec3d(emitterPos)));
+        renderBeam(emitterPos, gameTime, matrixStackIn, bufferIn, new Vector3d(0.5, 0.5, 0.5), endPos.subtract(VecUtils.vec3d(emitterPos)));
+//        renderBeam(emitterPos, gameTime, matrixStackIn, bufferIn, new Vector3d(0.5, 0.5, 0.5), new Vector3d(20.5, 1, 0.5));
     }
 
     public static void renderModel(TimeModel model, ResourceLocation texture, Quaternion rotationQuaternion, MatrixStack matrixStack, IRenderTypeBuffer buffer, int combinedLightIn, int combinedOverlayIn) {
@@ -69,7 +79,9 @@ public class LaserEmitterTER extends TileEntityRenderer<LaserEmitterTile> {
         matrixStack.popPose();
     }
 
-    public static void renderBeam(BlockPos pos, float gameTime, MatrixStack stack, IRenderTypeBuffer bufferIn, Vector3d start, Vector3d end) {
+    public static void renderBeam(BlockPos pos, float gameTime, MatrixStack stack, IRenderTypeBuffer bufferIn, Vector3d startIn, Vector3d endIn) {
+        Vector3f start = new Vector3f(startIn);
+        Vector3f end = new Vector3f(endIn);
         IVertexBuilder bufferBuilder = bufferIn.getBuffer(JRenderTypes.laserBeam(JITL.rl("textures/tile/laser_beam.png")));
 
         MatrixStack.Entry last = stack.last();
@@ -82,21 +94,33 @@ public class LaserEmitterTER extends TileEntityRenderer<LaserEmitterTile> {
         float texWidth = 32;
 
         float uPerBlock = 1 / (256 / texWidth * (radius * 2));
-        float distance = (float) start.distanceTo(end);
+        float distance = (float) startIn.distanceTo(endIn);
 
         float startU = gameTime % 5 / 5F;
         float endU = startU + distance * uPerBlock;
 
-        Vector3d startToEnd = end.subtract(start).normalize();
-        Vector3d startToCamera = camera.subtract(start).normalize();
+        Vector3f startToCamera = new Vector3f(camera);
+        startToCamera.sub(start);
+        startToCamera.normalize();// vec from start to camera with length 1
 
-        Vector3d startToLeft = startToEnd.cross(startToCamera).normalize().scale(radius);
-        Vector3d startToRight = startToLeft.scale(-1);
+        Vector3f perpendicular = end.copy();
+        perpendicular.sub(start);
+        perpendicular.normalize(); // vec from start to end with length 1
 
-        bufferBuilder.vertex(posMatrix, ((float) (start.x + startToLeft.x)), ((float) (start.y + startToLeft.y)), ((float) (start.z + startToLeft.z))).uv(endU, 0).endVertex();
-        bufferBuilder.vertex(posMatrix, ((float) (end.x + startToLeft.x)), ((float) (end.y + startToLeft.y)), ((float) (end.z + startToLeft.z))).uv(startU, 0).endVertex();
-        bufferBuilder.vertex(posMatrix, ((float) (end.x + startToRight.x)), ((float) (start.y + startToRight.y)), ((float) (end.z + startToRight.z))).uv(startU, texWidth / 256F).endVertex();
-        bufferBuilder.vertex(posMatrix, ((float) (start.x + startToRight.x)), ((float) (start.y + startToRight.y)), ((float) (start.z + startToRight.z))).uv(endU, texWidth / 256F).endVertex();
+        perpendicular.cross(startToCamera);
+        perpendicular.normalize();
+        perpendicular.mul(radius); // perpendicular to beam and camera
+
+//        Vector3d startToLeft = startToEnd.cross(startToCamera).normalize().scale(radius);
+//        Vector3d startToRight = startToLeft.scale(-1);
+
+        bufferBuilder.vertex(posMatrix, start.x() + perpendicular.x(), start.y() + perpendicular.y(), start.z() + perpendicular.z()).uv(endU, 0).endVertex();
+        bufferBuilder.vertex(posMatrix, end.x() + perpendicular.x(), end.y() + perpendicular.y(), end.z() + perpendicular.z()).uv(startU, 0).endVertex();
+
+        perpendicular.mul(-1);
+
+        bufferBuilder.vertex(posMatrix, end.x() + perpendicular.x(), end.y() + perpendicular.y(), end.z() + perpendicular.z()).uv(startU, texWidth / 256F).endVertex();
+        bufferBuilder.vertex(posMatrix, start.x() + perpendicular.x(), start.y() + perpendicular.y(), start.z() + perpendicular.z()).uv(endU, texWidth / 256F).endVertex();
     }
 
     @Override
