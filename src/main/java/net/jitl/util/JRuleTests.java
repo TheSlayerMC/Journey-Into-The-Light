9b97e458e6778a0c77f4b93cc104c1a27cf9bcff
The_Slayer <brock.kerley@hotmail.com>
2021-01-29 23:20:28 +1030

    Update JRuleTests.java

--- src/main/java/net/jitl/util/JRuleTests.java
+++ src/main/java/net/jitl/util/JRuleTests.java
@@ -1,5 +1,6 @@
 package net.jitl.util;
 
+import net.jitl.init.JBlocks;
 import net.minecraft.block.Blocks;
 import net.minecraft.world.gen.feature.template.BlockStateMatchRuleTest;
 import net.minecraft.world.gen.feature.template.RuleTest;
@@ -13,4 +14,7 @@ public class JRuleTests {
 	public static RuleTest STONE_NETHERRACK = new BlockStateMatchRuleTest(Blocks.NETHERRACK.defaultBlockState());
 	public static RuleTest STONE_BASALT = new BlockStateMatchRuleTest(Blocks.BASALT.defaultBlockState());
 	public static RuleTest STONE_END = new BlockStateMatchRuleTest(Blocks.END_STONE.defaultBlockState());
+
+	public static RuleTest STONE_EUCA = new BlockStateMatchRuleTest(JBlocks.GOLDITE_STONE.defaultBlockState());
+
 }
