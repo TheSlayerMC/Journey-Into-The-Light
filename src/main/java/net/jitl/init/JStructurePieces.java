adc8dc196207d35020816b7bc1dcb23b6e75fff1
The_Slayer <brock.kerley@hotmail.com>
2021-01-30 21:38:56 +1030

    blacksmith house added

--- src/main/java/net/jitl/init/JStructurePieces.java
+++ src/main/java/net/jitl/init/JStructurePieces.java
@@ -2,6 +2,7 @@ package net.jitl.init;
 
 import net.jitl.JITL;
 import net.jitl.common.world.gen.structures.guardian.GuardianTowerPieces;
+import net.jitl.common.world.gen.structures.overworld.BlacksmithStructure;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.gen.feature.structure.IStructurePieceType;
 import ru.timeconqueror.timecore.api.registry.SimpleVanillaRegister;
@@ -12,6 +13,10 @@ public class JStructurePieces {
     @AutoRegistrable
     private static final SimpleVanillaRegister<IStructurePieceType> REGISTER = new SimpleVanillaRegister<>(JITL.MODID, Registry.STRUCTURE_PIECE);
 
+
     public static final Promised<IStructurePieceType> GUARDIAN_TOWER_PIECE = REGISTER.register("gt/p", () -> GuardianTowerPieces.Piece::new);
     public static final Promised<IStructurePieceType> GUARDIAN_TOWER_NO_GRASS_TOUCHED_PIECE = REGISTER.register("gt/ngtp", () -> GuardianTowerPieces.NoGrassTouchedPiece::new);
+
+    public static final Promised<IStructurePieceType> BLACKSMITH = REGISTER.register("bs", () -> BlacksmithStructure.Piece::new);
+
 }
--- src/main/java/net/jitl/init/JStructures.java
+++ src/main/java/net/jitl/init/JStructures.java
@@ -2,6 +2,7 @@ package net.jitl.init;
 
 import net.jitl.JITL;
 import net.jitl.common.world.gen.structures.guardian.GuardianTowerStructure;
+import net.jitl.common.world.gen.structures.overworld.BlacksmithStructure;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.NoFeatureConfig;
 import org.apache.logging.log4j.Marker;
@@ -21,6 +22,13 @@ public class JStructures {
 
     public static final StructureHolder<NoFeatureConfig, GuardianTowerStructure> GUARDIAN_TOWER_HOLDER =
             REGISTER.register("guardian_tower", GuardianTowerStructure::new, TimeStructureSeparationSettings.create(10, 5), NoFeatureConfig.CODEC, NoFeatureConfig.NONE)
+                    .transformsSurroundingLand()
+                    .setDimensionPredicate(serverWorld -> serverWorld.dimension() == World.OVERWORLD)
+                    .addToTag(Tag.DISABLE_BREAKING_BY_LAKES)
+                    .asHolder();
+
+    public static final StructureHolder<NoFeatureConfig, BlacksmithStructure> BLACKSMITH =
+            REGISTER.register("blacksmith", BlacksmithStructure::new, TimeStructureSeparationSettings.create(10, 5), NoFeatureConfig.CODEC, NoFeatureConfig.NONE)
                     .transformsSurroundingLand()
                     .setDimensionPredicate(serverWorld -> serverWorld.dimension() == World.OVERWORLD)
                     .addToTag(Tag.DISABLE_BREAKING_BY_LAKES)
