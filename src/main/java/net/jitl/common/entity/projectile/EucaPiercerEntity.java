b727f5e6e9e982c157cdb483ad1e752b712f50d2
The_Slayer <brock.kerley@hotmail.com>
2021-01-31 13:20:29 +1030

    Update EucaPiercerEntity.java

--- src/main/java/net/jitl/common/entity/projectile/EucaPiercerEntity.java
+++ src/main/java/net/jitl/common/entity/projectile/EucaPiercerEntity.java
@@ -8,15 +8,16 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
 
-public class EntityEucaPiercer extends DamagingProjectileEntity implements IRendersAsItem {
+public class EucaPiercerEntity extends DamagingProjectileEntity implements IRendersAsItem {
 
     protected int bounces, maxBounces;
     private LivingEntity thrower;
     private float dam;
 
-    public EntityEucaPiercer(EntityType<? extends DamagingProjectileEntity> type, World world, LivingEntity thrower, float damage, int maxBounces) {
+    public EucaPiercerEntity(EntityType<? extends DamagingProjectileEntity> type, World world, LivingEntity thrower, float damage, int maxBounces) {
         super(type, world, thrower, damage);
         this.maxBounces = maxBounces;
         this.thrower = thrower;
@@ -30,11 +31,20 @@ public class EntityEucaPiercer extends DamagingProjectileEntity implements IRend
 
     @Override
     protected void onEntityImpact(RayTraceResult result, Entity target) {
+        Vector3d vector3d = this.getDeltaMovement();
+        double x = this.getX() + vector3d.x;
+        double y = this.getY() + vector3d.y;
+        double z = this.getZ() + vector3d.z;
+
         if(target != null && target != thrower) {
             target.hurt(DamageSource.thrown(this, this.getOwner()), getDamage());
             if(!level.isClientSide) this.remove();
             return;
         }
 
+        //Add the bounces
+
+        this.bounces ++;
+        if(this.bounces == this.maxBounces) this.remove();
     }
 }
