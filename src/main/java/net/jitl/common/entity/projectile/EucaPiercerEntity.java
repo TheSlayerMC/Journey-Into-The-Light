0b21e1e294e1169f5dd84f93afcc9bc43960608f
The_Slayer <brock.kerley@hotmail.com>
2021-01-31 15:06:38 +1030

    Update EucaPiercerEntity.java

--- src/main/java/net/jitl/common/entity/projectile/EucaPiercerEntity.java
+++ src/main/java/net/jitl/common/entity/projectile/EucaPiercerEntity.java
@@ -6,10 +6,14 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.IRendersAsItem;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.IPacket;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.network.NetworkHooks;
 
 public class EucaPiercerEntity extends DamagingProjectileEntity implements IRendersAsItem {
 
@@ -30,21 +34,36 @@ public class EucaPiercerEntity extends DamagingProjectileEntity implements IRend
     }
 
     @Override
-    protected void onEntityImpact(RayTraceResult result, Entity target) {
+    protected void onEntityImpact(RayTraceResult ray, Entity target) {
+
+        BlockRayTraceResult result = (BlockRayTraceResult)ray;
+        Direction faceHit = result.getDirection();
+
         Vector3d vector3d = this.getDeltaMovement();
         double x = this.getX() + vector3d.x;
         double y = this.getY() + vector3d.y;
         double z = this.getZ() + vector3d.z;
 
         if(target != null && target != thrower) {
-            target.hurt(DamageSource.thrown(this, this.getOwner()), getDamage());
+            target.hurt(DamageSource.thrown(this, this.getOwner()), this.dam);
             if(!level.isClientSide) this.remove();
             return;
         }
 
-        //Add the bounces
+        if(faceHit == Direction.UP || faceHit == Direction.DOWN) {
+            //this.motionY *= -1.0D;
+        } else if (faceHit == Direction.SOUTH || faceHit == Direction.NORTH) {
+           // this.motionZ *= -1.0D;
+        } else if (faceHit == Direction.EAST || faceHit == Direction.WEST) {
+           // this.motionX *= -1.0D;
+        }
 
         this.bounces ++;
         if(this.bounces == this.maxBounces) this.remove();
+    }
+
+    @Override
+    public IPacket<?> getAddEntityPacket() {
+        return NetworkHooks.getEntitySpawningPacket(this);
     }
 }
