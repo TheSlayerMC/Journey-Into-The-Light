cc7bdde3eb8a2b42643ee6eaf38e162ff02dcf81
TimeConqueror <timeconqueror999@gmail.com>
2021-02-01 14:52:18 +0300

    Slightly changed JItemProperties, fixed wrong item group for ranged weapons

--- src/main/java/net/jitl/util/JItemProperties.java
+++ src/main/java/net/jitl/util/JItemProperties.java
@@ -5,11 +5,11 @@ import net.minecraft.item.Item;
 import ru.timeconqueror.timecore.api.registry.util.ItemPropsFactory;
 
 public class JItemProperties {
-    private static final ItemPropsFactory ITEM_GROUP = new ItemPropsFactory(JTabs.ITEMS);
-    private static final ItemPropsFactory TOOLS_GROUP = new ItemPropsFactory(JTabs.TOOLS);
-    private static final ItemPropsFactory WEAPONS_GROUP = new ItemPropsFactory(JTabs.WEAPONS);
-    private static final ItemPropsFactory MISC_GROUP = new ItemPropsFactory(JTabs.MISC);
-    private static final ItemPropsFactory RANGED_WEAPONS_GROUP = new ItemPropsFactory(JTabs.MISC);
+    private static final ItemPropsFactory ITEM_GROUP = ItemPropsFactory.of(JTabs.ITEMS);
+    private static final ItemPropsFactory TOOLS_GROUP = ItemPropsFactory.of(JTabs.TOOLS);
+    private static final ItemPropsFactory WEAPONS_GROUP = ItemPropsFactory.of(JTabs.WEAPONS);
+    private static final ItemPropsFactory MISC_GROUP = ItemPropsFactory.of(JTabs.MISC);
+    private static final ItemPropsFactory RANGED_WEAPONS_GROUP = ItemPropsFactory.of(JTabs.RANGED_WEAPONS);
 
     public static Item.Properties itemGrouped() {
         return ITEM_GROUP.create();
