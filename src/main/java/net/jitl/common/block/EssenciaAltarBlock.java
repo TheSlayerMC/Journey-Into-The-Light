52cc6561daf008d7acd08194a090e227833503a7
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2021-01-31 18:55:57 -0700

    altar can only be activated at night

--- src/main/java/net/jitl/common/block/EssenciaAltarBlock.java
+++ src/main/java/net/jitl/common/block/EssenciaAltarBlock.java
@@ -19,7 +19,7 @@ public class EssenciaAltarBlock extends JTileContainerBlock {
 
     @Override
     public ActionResultType use(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
-        if (!worldIn.isClientSide() && handIn == Hand.MAIN_HAND) {
+        if (!worldIn.isClientSide() && handIn == Hand.MAIN_HAND && worldIn.isNight()) {
             WorldUtils.forTileWithReqt(worldIn, pos, EssenciaAltarTile.class, tile -> {
                 tile.onRightClick(((ServerPlayerEntity) player), player.getItemInHand(handIn));
             });
@@ -27,4 +27,24 @@ public class EssenciaAltarBlock extends JTileContainerBlock {
 
         return ActionResultType.sidedSuccess(worldIn.isClientSide());
     }
+
+    /*@Override
+    public void animateTick(BlockState stateIn, World worldIn, BlockPos pos, Random rand) {
+        WorldUtils.forTileWithReqt(worldIn, pos, EssenciaAltarTile.class, tile -> {
+            if (tile.isActivated()) {
+                for (int i = 0; i < 2; i++) {
+                    IParticleData particle = JParticleManager.ESSENCIA_LIGHTNING.get();
+                    int posThreshold = 16;
+                    float posRandom0 = (float) rand.nextInt(5 + i) / posThreshold;
+                    float posRandom1 = (float) rand.nextInt(5 + i) / posThreshold;
+                    float posRandom2 = (float) rand.nextInt(5 + i) / posThreshold;
+                    float posRandom3 = (float) rand.nextInt(5 + i) / posThreshold;
+                    worldIn.addParticle(particle, (pos.getX() + posRandom0) - 2.5F, pos.getY() + posRandom0 + 2, (pos.getZ() - posRandom0) - 2.5F, 0, 0, 0);
+                    worldIn.addParticle(particle, (pos.getX() - posRandom1) + 2.5F, pos.getY() - posRandom1 + 2, (pos.getZ() + posRandom1) + 2.5F, 0, 0, 0);
+                    worldIn.addParticle(particle, (pos.getX() - posRandom2) - 2.5F, pos.getY() + posRandom2 + 2, (pos.getZ() - posRandom2) + 2.5F, 0, 0, 0);
+                    worldIn.addParticle(particle, (pos.getX() + posRandom3) + 2.5F, pos.getY() - posRandom3 + 2, (pos.getZ() + posRandom3) - 2.5F, 0, 0, 0);
+                }
+            }
+        });
+    } */
 }
