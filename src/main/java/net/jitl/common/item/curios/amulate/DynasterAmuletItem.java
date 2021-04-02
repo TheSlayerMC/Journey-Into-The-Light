f957b228b76faf87ef79994bc8782ab341db3673
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2021-04-01 21:12:44 -0700

    "fixed" dynaster amulet

--- src/main/java/net/jitl/common/item/curios/amulate/DynasterAmuletItem.java
+++ src/main/java/net/jitl/common/item/curios/amulate/DynasterAmuletItem.java
@@ -1,9 +1,11 @@
 package net.jitl.common.item.curios.amulate;
 
+import net.jitl.JITL;
 import net.jitl.common.item.curios.JCurioItem;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.Direction;
+import net.minecraft.util.math.vector.Vector3d;
 
 public class DynasterAmuletItem extends JCurioItem {
     public DynasterAmuletItem(Properties properties) {
@@ -13,12 +15,14 @@ public class DynasterAmuletItem extends JCurioItem {
 
     @Override
     public void curioTick(String identifier, int index, LivingEntity livingEntity, ItemStack stack) {
-        if (!livingEntity.level.isClientSide()) {
-            if (livingEntity.getMotionDirection().getAxisDirection() == Direction.AxisDirection.NEGATIVE
-                    && !livingEntity.isOnGround() && !livingEntity.isInLava() && !livingEntity.isInWaterOrBubble() && livingEntity.isCrouching()) {
-                if (livingEntity.level.getBlockState(livingEntity.blockPosition().below(2)).isCollisionShapeFullBlock(livingEntity.level, livingEntity.blockPosition().below(5))) {
-                    livingEntity.lerpMotion(0, 0.55, 0);
+        if (livingEntity instanceof PlayerEntity) {
+            Vector3d vector3d = livingEntity.getDeltaMovement();
+            if (vector3d.y < 0.0D && !livingEntity.isOnGround() && !livingEntity.isInLava() && !livingEntity.isInWaterOrBubble() && livingEntity.isCrouching()) {
+                if (livingEntity.level.getBlockState(livingEntity.blockPosition().below(5)).isCollisionShapeFullBlock(livingEntity.level, livingEntity.blockPosition().below(5))) {
+                    livingEntity.setDeltaMovement(vector3d.multiply(1, 0.75, 1));
                     livingEntity.fallDistance = -1.0F;
+                    livingEntity.hurtMarked = true;
+                    JITL.LOGGER.info(livingEntity.getDeltaMovement().y);
                 }
             }
         }
