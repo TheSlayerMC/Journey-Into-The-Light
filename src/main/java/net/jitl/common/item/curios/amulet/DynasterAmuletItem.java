572931b5345278f16fbe6be39dddcefe5984843d
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2021-04-02 20:23:36 -0700

    Actually fixed dynaster amulet

--- src/main/java/net/jitl/common/item/curios/amulet/DynasterAmuletItem.java
+++ src/main/java/net/jitl/common/item/curios/amulet/DynasterAmuletItem.java
@@ -1,14 +1,9 @@
 package net.jitl.common.item.curios.amulet;
 
-import net.jitl.JITL;
 import net.jitl.common.item.curios.JCurioItem;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.potion.EffectInstance;
-import net.minecraft.potion.Effects;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
 
 public class DynasterAmuletItem extends JCurioItem {
@@ -19,11 +14,12 @@ public class DynasterAmuletItem extends JCurioItem {
 
     @Override
     public void curioTick(String identifier, int index, LivingEntity livingEntity, ItemStack stack) {
-        if (livingEntity.isCrouching()) {
+        if (livingEntity.isShiftKeyDown()) {
             if (!livingEntity.isOnGround() && !livingEntity.isInLava() && !livingEntity.isInWaterOrBubble()) {
                 if (isFloatReady(livingEntity.level, livingEntity.blockPosition().below())) {
+                    //TODO: "drain" essence, once essence is added
+                    livingEntity.fallDistance = 0.0F;
                     livingEntity.setDeltaMovement(livingEntity.getDeltaMovement().multiply(1, 0.75, 1));
-                    livingEntity.fallDistance = -1.0F;
                 }
             }
         }
