aabc5faef3fcba7b7e3019eb0455dc1b11ca7f38
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2021-04-04 15:22:57 -0700

    Some Tower Guardian tweaks

--- src/main/java/net/jitl/common/entity/overworld/TowerGuardianEntity.java
+++ src/main/java/net/jitl/common/entity/overworld/TowerGuardianEntity.java
@@ -8,7 +8,10 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.monster.MonsterEntity;
+import net.minecraft.entity.monster.ZombifiedPiglinEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
 import ru.timeconqueror.timecore.animation.AnimationStarter;
@@ -28,7 +31,7 @@ public class TowerGuardianEntity extends MonsterEntity implements AnimatedObject
 
 		animationSystem = AnimationSystemBuilder.forEntity(this, world, builder ->
 				builder.addLayer(LAYER_WALKING, BlendType.ADDING, 1F), predefinedAnimations ->
-				predefinedAnimations.setWalkingAnimation(new AnimationStarter(JAnimations.towerGuardianWalk).setSpeed(3F), LAYER_WALKING));
+				predefinedAnimations.setWalkingAnimation(new AnimationStarter(JAnimations.towerGuardianWalk).setSpeed(1F), LAYER_WALKING));
 	}
 
 	@Override
@@ -42,6 +45,19 @@ public class TowerGuardianEntity extends MonsterEntity implements AnimatedObject
 	@Override
 	public @NotNull AnimationSystem<TowerGuardianEntity> getSystem() {
 		return animationSystem;
+	}
+
+	@Override
+	protected void registerGoals() {
+		this.goalSelector.addGoal(8, new LookAtGoal(this, PlayerEntity.class, 8.0F));
+		this.goalSelector.addGoal(8, new LookRandomlyGoal(this));
+		this.addBehaviourGoals();
+	}
+
+	protected void addBehaviourGoals() {
+		this.goalSelector.addGoal(7, new WaterAvoidingRandomWalkingGoal(this, 1.0D));
+		this.targetSelector.addGoal(1, (new HurtByTargetGoal(this)).setAlertOthers(ZombifiedPiglinEntity.class));
+		this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, PlayerEntity.class, true));
 	}
 
 	public static AttributeModifierMap.MutableAttribute createAttributes() {
