95d7e957c5e2b5186d728d4c8021182a2c43a5a5
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2021-04-08 00:34:17 -0700

    Added proper Essence regen effect texture

--- src/main/java/net/jitl/init/JAttributes.java
+++ src/main/java/net/jitl/init/JAttributes.java
@@ -14,7 +14,7 @@ public class JAttributes {
 
     //TODO: During development, default values are set so we don't have to keep equipping curios to test essence. In public builds, they should be set to zero.
     public static final RegistryObject<Attribute> MAX_ESSENCE =
-            REGISTER.register("max_essence", () -> new RangedAttribute("jitl.max_essence", 1.0F, 0.0F, 10.0F).setSyncable(true));
+            REGISTER.register("max_essence", () -> new RangedAttribute("jitl.max_essence", 1.0F, 0.0F, 20.0F).setSyncable(true));
 
     public static final RegistryObject<Attribute> ESSENCE_REGEN_SPEED =
             REGISTER.register("essence_regen_speed", () -> new RangedAttribute("jitl.essence_regen_speed", 0.012F, 0.0F, 0.065F).setSyncable(true));
