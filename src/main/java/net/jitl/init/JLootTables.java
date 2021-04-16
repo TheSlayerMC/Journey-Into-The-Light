c3cb66030fa4c2c1e3a77dec307ea60a69665caa
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2021-04-16 15:31:16 -0700

    Added proper loot table for Overworld loot modifier

--- src/main/java/net/jitl/init/JLootTables.java
+++ src/main/java/net/jitl/init/JLootTables.java
@@ -4,13 +4,15 @@ import net.jitl.JITL;
 import net.minecraft.loot.LootTables;
 import net.minecraft.util.ResourceLocation;
 
-public class JLootTables {
+public class JLootTables extends LootTables {
 
     public static final ResourceLocation LOOT_POUCH_BASIC = LootTables.ABANDONED_MINESHAFT;//register("loot/pouch_basic");
     public static final ResourceLocation LOOT_POUCH_GOLD = LootTables.DESERT_PYRAMID;//register("loot/pouch_gold");
     public static final ResourceLocation LOOT_POUCH_DIAMOND = LootTables.WOODLAND_MANSION;//register("loot/pouch_diamond");
 
+    public static final ResourceLocation OVERWORLD_CHESTS = register("chests/overworld_chests");
+
     private static ResourceLocation register(String id) {
-        return JITL.rl(id);
+        return JITL.rl("loot_tables/" + id);
     }
 }
