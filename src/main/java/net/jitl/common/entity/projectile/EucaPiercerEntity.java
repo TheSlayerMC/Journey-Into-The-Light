8b895007f32392135e211bc750da144c53214b01
DiamondWalker <19913710+DiamondWalker@users.noreply.github.com>
2021-04-29 00:03:19 -0400

    Piercer is finished

--- src/main/java/net/jitl/common/entity/projectile/EucaPiercerEntity.java
+++ src/main/java/net/jitl/common/entity/projectile/EucaPiercerEntity.java
@@ -1,15 +1,21 @@
 package net.jitl.common.entity.projectile;
 
 import com.sun.javafx.geom.Vec3d;
+import net.jitl.init.JItems;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.IRendersAsItem;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.entity.projectile.AbstractArrowEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.network.IPacket;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.EntityRayTraceResult;
 import net.minecraft.util.math.RayTraceContext;
@@ -17,17 +23,18 @@ import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.network.NetworkHooks;
+import org.lwjgl.system.CallbackI;
 
 import java.util.List;
 
 public class EucaPiercerEntity extends AbstractArrowEntity implements IRendersAsItem {
-    ItemStack stack;
+    private static final DataParameter<ItemStack> STACK = EntityDataManager.defineId(EucaPiercerEntity.class, DataSerializers.ITEM_STACK);
     int bounces;
     float damage;
 
     public EucaPiercerEntity(EntityType<? extends AbstractArrowEntity> type, LivingEntity shooter, World worldIn, ItemStack stack, float damage) {
         super(type, shooter, worldIn);
-        this.stack = stack;
+        setStack(stack.copy());
         this.damage = damage;
     }
 
@@ -44,7 +51,7 @@ public class EucaPiercerEntity extends AbstractArrowEntity implements IRendersAs
                 entity.hurt(DamageSource.thrown(this, this.getOwner()), damage);
                 if (getOwner() instanceof ServerPlayerEntity) {
                     ServerPlayerEntity player = (ServerPlayerEntity) getOwner();
-                    stack.hurt(1, player.getRandom(), player);
+                    getStack().hurt(1, player.getRandom(), player);
                 }
                 if (++bounces <= 5) {
                     List<LivingEntity> entitiesNear = this.level.getEntitiesOfClass(LivingEntity.class, this.getBoundingBox().inflate(20D));
@@ -58,7 +65,7 @@ public class EucaPiercerEntity extends AbstractArrowEntity implements IRendersAs
                 }
                 Entity target = bounceTo != null ? bounceTo : getOwner();
                 if (target != null) {
-                    Vector3d movement = new Vector3d(target.getX() - this.getX(), target.getY() + target.getBbHeight() - this.getY(), target.getZ() - this.getZ());
+                    Vector3d movement = new Vector3d(target.getX() - this.getX(), target.getEyeY() - this.getY(), target.getZ() - this.getZ());
                     this.setDeltaMovement(movement.scale((1 / movement.length()) * this.getDeltaMovement().length()));
                 }
             }
@@ -72,9 +79,9 @@ public class EucaPiercerEntity extends AbstractArrowEntity implements IRendersAs
     }
 
     @Override
-    public void addAdditionalSaveData(CompoundNBT nbt) { //TODO: don't eat the world
+    public void addAdditionalSaveData(CompoundNBT nbt) {
         super.addAdditionalSaveData(nbt);
-        nbt.put("stack", stack.save(nbt));
+        nbt.put("stack", getStack().save(new CompoundNBT()));
         nbt.putInt("bounces", bounces);
         nbt.putFloat("damage", damage);
     }
@@ -82,23 +89,47 @@ public class EucaPiercerEntity extends AbstractArrowEntity implements IRendersAs
     @Override
     public void readAdditionalSaveData(CompoundNBT nbt) {
         super.readAdditionalSaveData(nbt);
-        stack = ItemStack.of(nbt.getCompound("stack"));
-        bounces = nbt.getInt("bounces");
-        damage = nbt.getFloat("damage");
+        setStack(ItemStack.of(nbt.getCompound("stack")));
+        if (getStack().isEmpty()) remove();
+        nbt.getInt("bounces");
+        nbt.getFloat("damage");
+    }
+
+    private void setStack(ItemStack stack) {
+        this.getEntityData().set(STACK, stack);
+    }
+
+    private ItemStack getStack() {
+        return this.getEntityData().get(STACK);
     }
 
     @Override
     protected ItemStack getPickupItem() {
-        return stack;
+        return getStack().copy();
     }
 
     @Override
     public ItemStack getItem() {
-        return stack;
+        ItemStack stack = getStack();
+        return stack.isEmpty() ? new ItemStack(JItems.EUCA_PIERCER) : stack;
     }
 
     @Override
     public IPacket<?> getAddEntityPacket() {
         return NetworkHooks.getEntitySpawningPacket(this);
+    }
+
+    @Override
+    protected void defineSynchedData() {
+        this.getEntityData().define(STACK, ItemStack.EMPTY);
+        super.defineSynchedData();
+    }
+
+    @Override
+    public void onSyncedDataUpdated(DataParameter<?> key) {
+        if (key == STACK) {
+            getStack().setEntityRepresentation(this);
+        }
+        super.onSyncedDataUpdated(key);
     }
 }
