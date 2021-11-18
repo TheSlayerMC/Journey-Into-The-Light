d27fecf0d37651ed8f5ec38672ac29d046a874cf
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2021-11-18 00:09:46 -0700

    Working jigsaw structure gen

--- src/main/java/net/jitl/common/world/gen/structures/overworld/FrozenFortressStructure.java
+++ src/main/java/net/jitl/common/world/gen/structures/overworld/FrozenFortressStructure.java
@@ -8,6 +8,8 @@ import net.minecraft.util.registry.DynamicRegistries;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.Heightmap;
 import net.minecraft.world.gen.feature.NoFeatureConfig;
 import net.minecraft.world.gen.feature.jigsaw.JigsawManager;
 import net.minecraft.world.gen.feature.structure.AbstractVillagePiece;
@@ -26,6 +28,10 @@ public class FrozenFortressStructure extends Structure<NoFeatureConfig> {
         return FrozenFortressStructure.Start::new;
     }
 
+    public GenerationStage.Decoration step() {
+        return GenerationStage.Decoration.SURFACE_STRUCTURES;
+    }
+
     public static class Start extends StructureStart<NoFeatureConfig> {
 
         public Start(Structure<NoFeatureConfig> structure_, int int_, int int1_, MutableBoundingBox mutableBoundingBox_, int int2_, long long_) {
@@ -34,20 +40,24 @@ public class FrozenFortressStructure extends Structure<NoFeatureConfig> {
 
         @Override
         public void generatePieces(DynamicRegistries dynamicRegistries_, ChunkGenerator chunkGenerator_, TemplateManager templateManager_, int x, int z, Biome biome_, NoFeatureConfig featureConfig_) {
-            BlockPos pos = new BlockPos(x * 16, 0, z * 16);
-            JigsawManager.addPieces(dynamicRegistries_,
-                    new VillageConfig(() -> dynamicRegistries_.registryOrThrow(Registry.TEMPLATE_POOL_REGISTRY).get(JITL.rl("overworld/blacksmith")), 100),
-                    AbstractVillagePiece::new,
-                    chunkGenerator_,
-                    templateManager_,
-                    pos,
-                    pieces,
-                    random,
-                    false,
-                    false);
-            pieces.forEach(piece -> piece.move(1, 1, 1));
+            int chunkX = (x << 4) + 7;
+            int chunkZ = (z << 4) + 7;
+            BlockPos pos = new BlockPos(chunkX, 0, chunkZ);
+            if (chunkGenerator_.getBaseHeight(chunkX, chunkZ, Heightmap.Type.WORLD_SURFACE_WG) > 0) {
+                JigsawManager.addPieces(dynamicRegistries_,
+                        new VillageConfig(() -> dynamicRegistries_.registryOrThrow(Registry.TEMPLATE_POOL_REGISTRY).get(JITL.rl("pool")), 300),
+                        AbstractVillagePiece::new,
+                        chunkGenerator_,
+                        templateManager_,
+                        pos,
+                        pieces,
+                        random,
+                        true,
+                        true);
+            }
+            pieces.forEach(piece -> piece.move(0, 5, 0));
             calculateBoundingBox();
-            moveInsideHeights(random, 0, 256);
+            //moveInsideHeights(random, 0, 256);
         }
     }
 }
