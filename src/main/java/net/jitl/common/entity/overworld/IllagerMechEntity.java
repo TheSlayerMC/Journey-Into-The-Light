2a849dbce821ed7809c35a82357f2a70e7b3fd84
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2021-11-23 17:30:30 -0700

    added villager mech throwing animation, tweaked model and texture

--- src/main/java/net/jitl/common/entity/overworld/IllagerMechEntity.java
+++ src/main/java/net/jitl/common/entity/overworld/IllagerMechEntity.java
@@ -1,5 +1,6 @@
 package net.jitl.common.entity.overworld;
 
+import net.jitl.JITL;
 import net.jitl.init.JAnimations;
 import net.jitl.init.JSounds;
 import net.minecraft.block.BlockState;
@@ -9,6 +10,8 @@ import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.merchant.villager.VillagerEntity;
 import net.minecraft.entity.monster.AbstractRaiderEntity;
+import net.minecraft.entity.monster.CreeperEntity;
+import net.minecraft.entity.monster.IMob;
 import net.minecraft.entity.passive.IronGolemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.DamageSource;
@@ -16,31 +19,44 @@ import net.minecraft.util.SoundEvent;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.common.util.Lazy;
 import org.jetbrains.annotations.NotNull;
 import ru.timeconqueror.timecore.animation.AnimationStarter;
 import ru.timeconqueror.timecore.animation.AnimationSystem;
+import ru.timeconqueror.timecore.animation.component.DelayedAction;
+import ru.timeconqueror.timecore.animation.util.StandardDelayPredicates;
+import ru.timeconqueror.timecore.api.animation.ActionManager;
 import ru.timeconqueror.timecore.api.animation.AnimatedObject;
 import ru.timeconqueror.timecore.api.animation.BlendType;
 import ru.timeconqueror.timecore.api.animation.builders.AnimationSystemBuilder;
 
 public class IllagerMechEntity extends AbstractRaiderEntity implements AnimatedObject<IllagerMechEntity> {
 
+    private static final Lazy<DelayedAction<IllagerMechEntity, Object>> THROWING_ACTION;
+
     private final AnimationSystem<IllagerMechEntity> animationSystem;
     private static final String LAYER_WALKING = "walking";
+    private static final String LAYER_THROWING = "throwing";
+
+    static {
+        THROWING_ACTION = Lazy.of(() -> new DelayedAction<IllagerMechEntity, Object>(JITL.rl("illager_mech/throw"), new AnimationStarter(JAnimations.illagerMechThrow).setSpeed(2.0F).setTransitionTime(0), LAYER_THROWING)
+                .setDelayPredicate(StandardDelayPredicates.onEnd()));
+    }
 
     public IllagerMechEntity(EntityType<? extends IllagerMechEntity> entityType, World world) {
         super(entityType, world);
         animationSystem = AnimationSystemBuilder.forEntity(this, world,
                 builder -> {
                     builder.addLayer(LAYER_WALKING, BlendType.ADDING, 1F);
+                    builder.addLayer(LAYER_THROWING, BlendType.OVERRIDE, 1F);
                 },
                 predefinedAnimations -> {
-                    predefinedAnimations.setWalkingAnimation(new AnimationStarter(JAnimations.illagerMechWalk).setSpeed(1.0F), LAYER_WALKING);
+                    predefinedAnimations.setWalkingAnimation(new AnimationStarter(JAnimations.illagerMechWalk).setSpeed(2.25F), LAYER_WALKING);
                 });
     }
 
     protected void registerGoals() {
-        this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 1.0D, false));
+        this.goalSelector.addGoal(0, new ThrowingGoal(this, 1.0D, false)); //mutex 1
         this.goalSelector.addGoal(0, new SwimGoal(this));
         this.goalSelector.addGoal(7, new LookAtGoal(this, PlayerEntity.class, 6.0F));
         this.goalSelector.addGoal(8, new LookRandomlyGoal(this));
@@ -48,6 +64,7 @@ public class IllagerMechEntity extends AbstractRaiderEntity implements AnimatedO
         this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, PlayerEntity.class, true));
         this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, VillagerEntity.class, true));
         this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, IronGolemEntity.class, true));
+        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, MobEntity.class, 5, false, false, (livingEntity_) -> livingEntity_ instanceof IMob && !(livingEntity_ instanceof CreeperEntity)));
     }
 
     @Override
@@ -63,8 +80,9 @@ public class IllagerMechEntity extends AbstractRaiderEntity implements AnimatedO
     public static AttributeModifierMap.MutableAttribute createAttributes() {
         return MobEntity.createMobAttributes()
                 .add(Attributes.MAX_HEALTH, 75.0D)
-                .add(Attributes.MOVEMENT_SPEED, 0.15D)
+                .add(Attributes.MOVEMENT_SPEED, 0.2D)
                 .add(Attributes.ATTACK_DAMAGE, 20.0D)
+                .add(Attributes.FOLLOW_RANGE, 32.0D)
                 .add(Attributes.KNOCKBACK_RESISTANCE, 1.0D);
     }
 
@@ -95,16 +113,54 @@ public class IllagerMechEntity extends AbstractRaiderEntity implements AnimatedO
         float f1 = (int) f > 0 ? f / 2.0F + (float) this.random.nextInt((int) f) : f;
         boolean flag = entityIn.hurt(DamageSource.mobAttack(this), f1);
         if (flag) {
-            entityIn.setDeltaMovement(entityIn.getDeltaMovement().add(0.0D, 0.4F, 0.0D));
+            entityIn.setDeltaMovement(entityIn.getDeltaMovement().add(0.0D, 0.65F, 0.0D));
             this.doEnchantDamageEffects(this, entityIn);
         }
-
-        this.playSound(JSounds.ILLAGER_MECH_THROW.get(), 1.0F, 1.0F);
         return flag;
     }
 
     @Override
     public @NotNull AnimationSystem<IllagerMechEntity> getSystem() {
         return animationSystem;
+    }
+
+    private class ThrowingGoal extends MeleeAttackGoal {
+        private final IllagerMechEntity mechEntity;
+
+        private ThrowingGoal(IllagerMechEntity mechEntity, double speed, boolean useLongMemory) {
+            super(mechEntity, speed, useLongMemory);
+            this.mechEntity = mechEntity;
+        }
+
+        @Override
+        public void start() {
+            super.start();
+        }
+
+        @Override
+        public void stop() {
+            super.stop();
+            this.mechEntity.setAggressive(false);
+        }
+
+        @Override
+        public boolean canUse() {
+            LivingEntity livingEntity = getTarget();
+            return livingEntity != null && livingEntity.isAlive();
+        }
+
+        @Override
+        protected void checkAndPerformAttack(LivingEntity enemy, double distToEnemySqr) {
+            double d0 = this.getAttackReachSqr(enemy);
+            ActionManager<IllagerMechEntity> actionManager = getActionManager();
+            if (distToEnemySqr <= d0) {
+                this.resetAttackCooldown();
+                this.mob.doHurtTarget(enemy);
+                if (!actionManager.isActionEnabled(THROWING_ACTION.get())) {
+                    actionManager.enableAction(THROWING_ACTION.get(), null);
+                    mechEntity.playSound(JSounds.ILLAGER_MECH_THROW.get(), 1.0F, 1.0F);
+                }
+            }
+        }
     }
 }
--- src/main/java/net/jitl/init/JAnimations.java
+++ src/main/java/net/jitl/init/JAnimations.java
@@ -13,32 +13,34 @@ public class JAnimations {
 	public static Animation floroShoot;
 	public static Animation floroReveal;
 	public static Animation floroHidden;
-	public static Animation floroHide;
-	public static Animation floroIdle;
+    public static Animation floroHide;
+    public static Animation floroIdle;
 
-	public static Animation witherspineWalk;
-	public static Animation witherspineIdle;
+    public static Animation witherspineWalk;
+    public static Animation witherspineIdle;
 
-	public static Animation towerGuardianWalk;
-	public static Animation towerGuardianSmash;
+    public static Animation towerGuardianWalk;
+    public static Animation towerGuardianSmash;
 
-	public static Animation illagerMechWalk;
+    public static Animation illagerMechWalk;
+    public static Animation illagerMechThrow;
 
-	@SubscribeEvent
-	public static void registerAnimations(FMLCommonSetupEvent event) {
-		floroWalk = AnimationAPI.loadAndRegisterAnimation(JITL.rl("animations/floro.walk.json"));
-		floroShoot = AnimationAPI.loadAndRegisterAnimation(JITL.rl("animations/floro.shoot.json"));
-		floroReveal = AnimationAPI.loadAndRegisterAnimation(JITL.rl("animations/floro.showing.json"));
-		floroHidden = AnimationAPI.loadAndRegisterAnimation(JITL.rl("animations/floro.hidden.json"));
-		floroIdle = AnimationAPI.loadAndRegisterAnimation(JITL.rl("animations/floro.idle.json"));
-		floroHide = AnimationAPI.register(AnimationAPI.reverse(floroReveal));
+    @SubscribeEvent
+    public static void registerAnimations(FMLCommonSetupEvent event) {
+        floroWalk = AnimationAPI.loadAndRegisterAnimation(JITL.rl("animations/floro.walk.json"));
+        floroShoot = AnimationAPI.loadAndRegisterAnimation(JITL.rl("animations/floro.shoot.json"));
+        floroReveal = AnimationAPI.loadAndRegisterAnimation(JITL.rl("animations/floro.showing.json"));
+        floroHidden = AnimationAPI.loadAndRegisterAnimation(JITL.rl("animations/floro.hidden.json"));
+        floroIdle = AnimationAPI.loadAndRegisterAnimation(JITL.rl("animations/floro.idle.json"));
+        floroHide = AnimationAPI.register(AnimationAPI.reverse(floroReveal));
 
-		witherspineWalk = AnimationAPI.loadAndRegisterAnimation(JITL.rl("animations/witherspine.walk.json"));
-		witherspineIdle = AnimationAPI.loadAndRegisterAnimation(JITL.rl("animations/witherspine.idle.json"));
+        witherspineWalk = AnimationAPI.loadAndRegisterAnimation(JITL.rl("animations/witherspine.walk.json"));
+        witherspineIdle = AnimationAPI.loadAndRegisterAnimation(JITL.rl("animations/witherspine.idle.json"));
 
-		towerGuardianWalk = AnimationAPI.loadAndRegisterAnimation(JITL.rl("animations/tower_guardian.walk.json"));
-		towerGuardianSmash = AnimationAPI.loadAndRegisterAnimation(JITL.rl("animations/tower_guardian.smash.json"));
+        towerGuardianWalk = AnimationAPI.loadAndRegisterAnimation(JITL.rl("animations/tower_guardian.walk.json"));
+        towerGuardianSmash = AnimationAPI.loadAndRegisterAnimation(JITL.rl("animations/tower_guardian.smash.json"));
 
-		illagerMechWalk = AnimationAPI.loadAndRegisterAnimation(JITL.rl("animations/illager_mech.walk.json"));
-	}
+        illagerMechWalk = AnimationAPI.loadAndRegisterAnimation(JITL.rl("animations/illager_mech.walk.json"));
+        illagerMechThrow = AnimationAPI.loadAndRegisterAnimation(JITL.rl("animations/illager_mech.throw.json"));
+    }
 }
