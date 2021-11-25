f4b58c6d8cb36ec26d0fb0d021faaa739339dbaa
The_Slayer <brock.kerley@hotmail.com>
2021-11-25 18:20:19 +1030

    Update ClientEventHandler.java

--- src/main/java/net/jitl/client/eventhandler/ClientEventHandler.java
+++ src/main/java/net/jitl/client/eventhandler/ClientEventHandler.java
@@ -5,6 +5,7 @@ import net.jitl.init.JItems;
 import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.client.event.DrawHighlightEvent;
+import net.minecraftforge.client.event.EntityViewRenderEvent;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import ru.timeconqueror.timecore.api.util.client.ClientProxy;
@@ -13,6 +14,7 @@ public class ClientEventHandler {
     public static void regToBus(IEventBus modBus, IEventBus forgeBus) {
         forgeBus.addListener(ClientEventHandler::onKeyEvent);
         forgeBus.addListener(ClientEventHandler::onBlockHighlight);
+        forgeBus.addListener(ClientEventHandler::onFogDensityEvent);
     }
 
     private static void onKeyEvent(InputEvent.KeyInputEvent event) {
@@ -25,5 +27,11 @@ public class ClientEventHandler {
         if (state.getBlock() == JBlocks.GUARDIAN_TOWER_BRAIN && !JItems.TEST_BUG.isInMainHand(ClientProxy.player())) {
             event.setCanceled(true);
         }
+    }
+
+    public static void onFogDensityEvent(EntityViewRenderEvent.FogDensity event) {
+        //CHECK DIM
+        event.setDensity(1F);
+        event.setCanceled(true);
     }
 }
