64933a0b2a85119b5abb5d965ce76b8e8e284daf
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2021-11-26 21:14:43 -0700

    fixed frost crystal models

--- src/main/java/net/jitl/client/render/JBlockStateResources.kt
+++ src/main/java/net/jitl/client/render/JBlockStateResources.kt
@@ -5,6 +5,7 @@ import ru.timeconqueror.timecore.api.client.resource.BlockStateResource
 import ru.timeconqueror.timecore.api.client.resource.JSONTimeResource
 import ru.timeconqueror.timecore.api.client.resource.location.BlockModelLocation
 import ru.timeconqueror.timecore.api.util.json
+import ru.timeconqueror.timecore.api.util.x
 import ru.timeconqueror.timecore.api.util.y
 
 object JBlockStateResources {
@@ -61,50 +62,34 @@ object JBlockStateResources {
     }
 
     @JvmStatic
-    fun orientableStateAllSides(location: BlockModelLocation): BlockStateResource {
-        val json = JSONTimeResource.`object`(
-                null, JSONTimeResource.listOf(
-                JSONTimeResource.`object`(
-                        "variants", JSONTimeResource.listOf(
-                        JSONTimeResource.`object`(
-                                "facing=north", JSONTimeResource.listOf(
-                                JSONTimeResource.property("model", location.toString())
-                        )
-                        ),
-                        JSONTimeResource.`object`(
-                                "facing=south", JSONTimeResource.listOf(
-                                JSONTimeResource.property("model", location.toString()),
-                                JSONTimeResource.property("y", 180)
-                        )
-                        ),
-                        JSONTimeResource.`object`(
-                                "facing=west", JSONTimeResource.listOf(
-                                JSONTimeResource.property("model", location.toString()),
-                                JSONTimeResource.property("y", 270)
-                        )
-                        ),
-                        JSONTimeResource.`object`(
-                                "facing=east", JSONTimeResource.listOf(
-                                JSONTimeResource.property("model", location.toString()),
-                                JSONTimeResource.property("y", 90)
-                        )
-                        ),
-                        JSONTimeResource.`object`(
-                                "facing=up", JSONTimeResource.listOf(
-                                JSONTimeResource.property("model", location.toString())
-                        )
-                        ),
-                        JSONTimeResource.`object`(
-                                "facing=down", JSONTimeResource.listOf(
-                                JSONTimeResource.property("model", location.toString()),
-                                JSONTimeResource.property("x", 180)
-                        )
-                        ),
-                )
-                )
-        )
-        )
-        return BlockStateResource.fromJson(json)
+    fun orientableStateAllSides(modelLocation: BlockModelLocation): BlockStateResource {
+        return BlockStateResource.fromJson(json {
+            "variants" {
+                "facing=north" {
+                    "model" set modelLocation.toString()
+                    x = 90
+                }
+                "facing=south" {
+                    "model" set modelLocation.toString()
+                    x = -90
+                }
+                "facing=east" {
+                    "model" set modelLocation.toString()
+                    y = 90
+                    x = 90
+                }
+                "facing=west" {
+                    "model" set modelLocation.toString()
+                    y = 270
+                    x = 90
+                }
+                "facing=up" { "model" set modelLocation.toString() }
+                "facing=down" {
+                    "model" set modelLocation.toString()
+                    x = 180
+                }
+            }
+        })
     }
 
     @JvmStatic
