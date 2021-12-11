d9992534105c5bdf0d6835ba1b440684742e9a54
The_Slayer <brock.kerley@hotmail.com>
2021-12-11 19:59:34 +1030

    lil bit

--- src/main/java/net/jitl/common/entity/overworld/MageEntity.java
+++ src/main/java/net/jitl/common/entity/overworld/MageEntity.java
@@ -7,8 +7,15 @@ import net.jitl.common.entity.base.JVillagerEntity;
 import net.jitl.common.entity.base.trades.CurrencyForItemsTrade;
 import net.jitl.init.JItems;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.ai.attributes.AttributeModifierMap;
+import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.merchant.villager.VillagerTrades;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
 
 public class MageEntity extends JVillagerEntity {
@@ -32,7 +39,19 @@ public class MageEntity extends JVillagerEntity {
     }
 
     @Override
+    protected void registerGoals() {
+        this.goalSelector.addGoal(7, new LookAtGoal(this, PlayerEntity.class, 6.0F));
+        this.goalSelector.addGoal(8, new LookRandomlyGoal(this));
+    }
+
+    @Override
     protected Int2ObjectMap<VillagerTrades.ITrade[]> getVillagerTrades() {
         return TRADES;
+    }
+
+    public static AttributeModifierMap.MutableAttribute createAttributes() {
+        return MobEntity.createMobAttributes()
+                .add(Attributes.MAX_HEALTH, 60.0D)
+                .add(Attributes.MOVEMENT_SPEED, 0.25D);
     }
 }
