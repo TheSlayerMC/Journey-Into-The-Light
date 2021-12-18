cea6a745d0bedeaf73c2b54aa30707ac82c67819
The_Slayer <brock.kerley@hotmail.com>
2021-12-18 11:06:15 +1030

    Frozen Guardian works but just spawns alot of items??

--- src/main/java/net/jitl/common/entity/frozen/FrozenGuardianEntity.java
+++ src/main/java/net/jitl/common/entity/frozen/FrozenGuardianEntity.java
@@ -13,6 +13,7 @@ import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ItemStack;
@@ -21,6 +22,7 @@ import net.minecraft.network.IPacket;
 import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
@@ -35,14 +37,18 @@ import java.util.Random;
 public class FrozenGuardianEntity extends CreatureEntity {
     private static final DataParameter<Boolean> DATA_IS_ACTIVATED = EntityDataManager.defineId(FrozenGuardianEntity.class, DataSerializers.BOOLEAN);
 
+    private int DEATH_TIMER;
+    private int COUNTED_FULL_PEDESTALS;
+
     public FrozenGuardianEntity(EntityType<? extends FrozenGuardianEntity> entityType, World world) {
         super(entityType, world);
+        this.DEATH_TIMER = 50;
+        this.COUNTED_FULL_PEDESTALS = 0;
     }
 
     @Override
-    protected void registerGoals() {
+    protected void registerGoals() { }
 
-    }
     public static boolean canSpawn(EntityType<? extends CreatureEntity> entityType, IWorld worldIn, SpawnReason reason, BlockPos pos, Random random) {
         return false;
     }
@@ -67,6 +73,24 @@ public class FrozenGuardianEntity extends CreatureEntity {
     }
 
     @Override
+    public void tick() {
+        super.tick();
+        if(isActivated()) {
+            this.DEATH_TIMER--;
+            this.level.addParticle(ParticleTypes.CLOUD, this.getX() - 0.5D + random.nextDouble(), this.getY() + 0.5D + random.nextDouble(), this.getZ() - 0.5D + random.nextDouble(), this.random.nextGaussian() * 0.05D, 0.15D, this.random.nextGaussian() * 0.05D);
+            if(DEATH_TIMER <= 0){
+                this.kill();
+                boolean spawnedItem = false;
+                if(!level.isClientSide && spawnedItem == false) {
+                    this.level.addFreshEntity(new ItemEntity(level, this.position().x + 0.5F, this.position().y + 1.4F, this.position().z + 0.5F, new ItemStack(JItems.STAFF_OF_CONJURING)));
+                    spawnedItem = true;
+                }
+                this.COUNTED_FULL_PEDESTALS = 0;
+            }
+        }
+    }
+
+    @Override
     protected void defineSynchedData() {
         super.defineSynchedData();
         this.entityData.define(DATA_IS_ACTIVATED, false);
@@ -76,12 +100,16 @@ public class FrozenGuardianEntity extends CreatureEntity {
     public void addAdditionalSaveData(CompoundNBT compound) {
         super.addAdditionalSaveData(compound);
         compound.putBoolean("activated", this.entityData.get(DATA_IS_ACTIVATED));
+        compound.putInt("counted_fill_pedestals", 0);
+        compound.putInt("death_timer", 5);
     }
 
     @Override
     public void readAdditionalSaveData(CompoundNBT compound) {
         super.readAdditionalSaveData(compound);
         compound.getBoolean("activated");
+        compound.getInt("counted_fill_pedestals");
+        compound.getInt("death_timer");
     }
 
     public void setActivated(boolean activated) {
@@ -95,7 +123,6 @@ public class FrozenGuardianEntity extends CreatureEntity {
     @Override
     protected ActionResultType mobInteract(PlayerEntity playerEntity, Hand hand) {
         int check_radius = 8;
-        int countedFullPedestals = 0;
         final World world = this.level;
         final BlockPos entityPos = new BlockPos(this.position());
         for (int x = -check_radius; x <= check_radius; x++) {
@@ -105,38 +132,27 @@ public class FrozenGuardianEntity extends CreatureEntity {
                     final Block block = world.getBlockState(pos).getBlock();
                     if (block == JBlocks.FROZEN_PEDISTAL) {
                         PedestalTile tile = (PedestalTile) world.getBlockEntity(pos);
-
                         if (tile != null && tile.getItem(0).getItem().equals(JItems.SAPPHIRE)) {
-                            countedFullPedestals++;
-                            System.out.println(pos);
-
-                            // KILL THE ENTITY AND SPAWN THE ITEM AND DESTROY THE ITEMS ON THE PEDESTAL
-
-                            if (!world.isClientSide) {
+                            this.COUNTED_FULL_PEDESTALS++;
                                 EssenciaBoltEntity bolt = new EssenciaBoltEntity(JEntities.ESSENCIA_BOLT_TYPE, level);
                                 bolt.setPos(pos.getX(), pos.getY() + 1.2, pos.getZ());
                                 bolt.setARGB(0x5acbff);
                                 bolt.setVisualOnly(true);
-                                if (isActivated()) {
+                                if(!level.isClientSide)
                                     this.level.addFreshEntity(bolt);
-                                }
-                            }
+                            tile.setItem(0, ItemStack.EMPTY);
                         }
                     }
                 }
             }
         }
-        if (countedFullPedestals == 8) {
+        if(this.COUNTED_FULL_PEDESTALS >= 8) {
             this.playSound(JSounds.FROZEN_GUARDIAN_DEATH.get(), 1.5F, 1.0F);
             setActivated(true);
-
-            //SET TIMER, SPAWN PARTICLES AND REMOVE FROM WORLD
-
-            // this.level.addFreshEntity(new ItemEntity(level, this.position().x + 0.5F, this.position().y + 1.4F, this.position().z + 0.5F, new ItemStack(JItems.STAFF_OF_CONJURING)));
-            // this.kill();
-
         }
-        countedFullPedestals = 0;
+        System.out.println(COUNTED_FULL_PEDESTALS);
         return super.mobInteract(playerEntity, hand);
     }
+
+
 }
