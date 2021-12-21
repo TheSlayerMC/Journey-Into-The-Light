216ede23ec1e6be1f03a15a734d8dbd715f9d957
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2021-12-21 04:21:18 -0700

    Frostburn effect now slows and weakens nearby mobs

--- src/main/java/net/jitl/common/effect/FrostburnEffect.java
+++ src/main/java/net/jitl/common/effect/FrostburnEffect.java
@@ -1,41 +1,33 @@
 package net.jitl.common.effect;
 
-import net.jitl.JITL;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.ai.attributes.AttributeModifier;
-import net.minecraft.entity.ai.attributes.AttributeModifierManager;
-import net.minecraft.entity.ai.attributes.Attributes;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.passive.TameableEntity;
 import net.minecraft.potion.Effect;
+import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.EffectType;
+import net.minecraft.potion.Effects;
+import net.minecraft.util.math.AxisAlignedBB;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Objects;
-import java.util.UUID;
-
 public class FrostburnEffect extends Effect {
-    private static final UUID FROSTBURN_UUID = UUID.fromString("00795fc8-af20-4374-a0a2-3d63123145c6");
-
     public FrostburnEffect(EffectType type, int liquidColor) {
         super(type, liquidColor);
     }
 
     @Override
-    public void removeAttributeModifiers(@NotNull LivingEntity entityLivingBaseIn, @NotNull AttributeModifierManager attributeMapIn, int amplifier) {
-        if (entityLivingBaseIn instanceof PlayerEntity) {
-            PlayerEntity player = (PlayerEntity) entityLivingBaseIn;
-            Objects.requireNonNull(player.getAttribute(Attributes.MOVEMENT_SPEED)).removeModifier(FROSTBURN_UUID);
-        }
-        super.removeAttributeModifiers(entityLivingBaseIn, attributeMapIn, amplifier);
+    public boolean isDurationEffectTick(int duration, int amplifier) {
+        return true;
     }
 
     @Override
-    public void addAttributeModifiers(@NotNull LivingEntity entityLivingBaseIn, @NotNull AttributeModifierManager attributeMapIn, int amplifier) {
-        if (entityLivingBaseIn instanceof PlayerEntity) {
-            PlayerEntity player = (PlayerEntity) entityLivingBaseIn;
-            Objects.requireNonNull(player.getAttribute(Attributes.MOVEMENT_SPEED)).addTransientModifier(
-                    new AttributeModifier(FROSTBURN_UUID, JITL.MODID + ":frostburn_modifier", player.getAttributeValue(Attributes.MOVEMENT_SPEED) + (float) (0.01 * (amplifier + 1)), AttributeModifier.Operation.ADDITION));
+    public void applyEffectTick(@NotNull LivingEntity entityLivingBaseIn, int amplifier) {
+        int playerArea = 4;
+        AxisAlignedBB axisalignedbb = AxisAlignedBB.unitCubeFromLowerCorner(entityLivingBaseIn.position()).inflate(playerArea, playerArea, playerArea);
+        for (LivingEntity livingEntity : entityLivingBaseIn.level.getEntitiesOfClass(LivingEntity.class, axisalignedbb)) {
+            if (livingEntity != entityLivingBaseIn && !(livingEntity instanceof TameableEntity)) {
+                livingEntity.addEffect(new EffectInstance(Effects.MOVEMENT_SLOWDOWN, 100 * (amplifier / 2), amplifier + 1));
+                livingEntity.addEffect(new EffectInstance(Effects.WEAKNESS, 100 * (amplifier), amplifier));
+            }
         }
-        super.addAttributeModifiers(entityLivingBaseIn, attributeMapIn, amplifier);
     }
 }
--- src/main/java/net/jitl/init/JFoods.java
+++ src/main/java/net/jitl/init/JFoods.java
@@ -8,14 +8,13 @@ public class JFoods {
 
     public static final Food CRYSTAL_APPLE = (new Food.Builder()).nutrition(8).saturationMod(2.2F)
             .effect(() -> new EffectInstance(Effects.REGENERATION, 800, 2), 1.0F)
-            .effect(() -> new EffectInstance(Effects.DAMAGE_RESISTANCE, 8000, 0), 1.0F)
-            .effect(() -> new EffectInstance(Effects.FIRE_RESISTANCE, 8000, 0), 1.0F)
-            .effect(() -> new EffectInstance(Effects.FIRE_RESISTANCE, 8000, 0), 1.0F)
-            .effect(() -> new EffectInstance(Effects.MOVEMENT_SPEED, 8000, 1), 1.0F)
-            .effect(() -> new EffectInstance(Effects.DAMAGE_BOOST, 8000, 2), 1.0F)
+            .effect(() -> new EffectInstance(Effects.DAMAGE_RESISTANCE, 4000, 0), 1.0F)
+            .effect(() -> new EffectInstance(Effects.FIRE_RESISTANCE, 4000, 0), 1.0F)
+            .effect(() -> new EffectInstance(Effects.MOVEMENT_SPEED, 600, 1), 1.0F)
+            .effect(() -> new EffectInstance(Effects.DAMAGE_BOOST, 4000, 2), 1.0F)
             .effect(() -> new EffectInstance(Effects.HEAL, 10, 1), 1.0F)
-            .effect(() -> new EffectInstance(Effects.ABSORPTION, 3400, 4), 1.0F)
-            .effect(() -> new EffectInstance(JEffects.FROSTBURN.get(), 3400, 4), 1.0F)
+            .effect(() -> new EffectInstance(Effects.ABSORPTION, 1400, 5), 1.0F)
+            .effect(() -> new EffectInstance(JEffects.FROSTBURN.get(), 600, 4), 1.0F)
             .alwaysEat().build();
 
     public static final Food BRADBERRY = (new Food.Builder()).nutrition(1).saturationMod(0.25F).fast().alwaysEat().build();
