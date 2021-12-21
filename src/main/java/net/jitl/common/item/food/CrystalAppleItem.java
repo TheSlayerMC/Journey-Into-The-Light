6cdfa528b816579d8bd2289f50cd7764feb6bd1c
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2021-12-21 16:17:30 -0700

    removed old crystal apple method

--- src/main/java/net/jitl/common/item/food/CrystalAppleItem.java
+++ src/main/java/net/jitl/common/item/food/CrystalAppleItem.java
@@ -1,24 +1,12 @@
 package net.jitl.common.item.food;
 
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
 
 public class CrystalAppleItem extends Item {
     public CrystalAppleItem(Properties properties) {
         super(properties);
-    }
-
-    @Override
-    public @NotNull ItemStack finishUsingItem(@NotNull ItemStack stack, @NotNull World worldIn, @NotNull LivingEntity entityLiving) {
-        super.finishUsingItem(stack, worldIn, entityLiving);
-        if (entityLiving instanceof PlayerEntity) {
-            //worldIn.playSound((PlayerEntity) entityLiving, entityLiving.blockPosition(), JSounds.CRYSTAL_APPLE_FREEZE.get(), SoundCategory.PLAYERS, 1.0F, 1.0F);
-        }
-        return stack;
     }
 
     @Override
