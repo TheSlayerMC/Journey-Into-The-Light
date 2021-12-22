2fe81732a8257475b2da91b2bf825915e5685ce9
The_Slayer <brock.kerley@hotmail.com>
2021-12-22 19:19:12 +1030

    Till sound

--- src/main/java/net/jitl/common/eventhandler/ItemEventHandler.java
+++ src/main/java/net/jitl/common/eventhandler/ItemEventHandler.java
@@ -6,6 +6,8 @@ import net.jitl.init.JBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.HoeItem;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -21,6 +23,7 @@ public class ItemEventHandler {
         boolean usedOnEucaSoil = blockUsedOn == JBlocks.EUCA_GOLD_GRASS_BLOCK || blockUsedOn == JBlocks.GOLDITE_DIRT || blockUsedOn == JBlocks.GOLDITE_GRASS_BLOCK;
         if(isHoe && usedOnEucaSoil) {
             event.getWorld().setBlock(event.getPos(), JBlocks.GOLDITE_FARMLAND.defaultBlockState(), 0);
+            event.getWorld().playSound(player, event.getPos(), SoundEvents.HOE_TILL, SoundCategory.BLOCKS, 1.0F, 1.0F);
         }
     }
 }
