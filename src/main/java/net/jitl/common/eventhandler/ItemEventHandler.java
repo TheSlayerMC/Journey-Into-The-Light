0c73535f47ee0443bef70c0074f05e2e3989c54e
The_Slayer <brock.kerley@hotmail.com>
2021-12-23 17:31:47 +1030

    Update ItemEventHandler.java

--- src/main/java/net/jitl/common/eventhandler/ItemEventHandler.java
+++ src/main/java/net/jitl/common/eventhandler/ItemEventHandler.java
@@ -1,7 +1,6 @@
 package net.jitl.common.eventhandler;
 
 import net.jitl.JITL;
-import net.jitl.common.block.base.JBlock;
 import net.jitl.init.JBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.PlayerEntity;
@@ -19,7 +18,7 @@ public class ItemEventHandler {
     public static void itemUsedEvent(PlayerInteractEvent.RightClickBlock event) {
         PlayerEntity player = event.getPlayer();
         Block blockUsedOn = event.getWorld().getBlockState(event.getPos()).getBlock();
-        boolean isHoe = player.getMainHandItem().getItem() instanceof HoeItem && player.getMainHandItem().getItem() != null;
+        boolean isHoe = player.getMainHandItem().getItem() != null && player.getMainHandItem().getItem() instanceof HoeItem ;
         boolean usedOnEucaSoil = blockUsedOn == JBlocks.EUCA_GOLD_GRASS_BLOCK || blockUsedOn == JBlocks.GOLDITE_DIRT || blockUsedOn == JBlocks.GOLDITE_GRASS_BLOCK;
         if(isHoe && usedOnEucaSoil) {
             event.getWorld().setBlock(event.getPos(), JBlocks.GOLDITE_FARMLAND.defaultBlockState(), 0);
