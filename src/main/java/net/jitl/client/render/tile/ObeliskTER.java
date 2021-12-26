9d32625c2f63bb35068caea5283fb04e794f1f97
The_Slayer <brock.kerley@hotmail.com>
2021-12-27 10:25:24 +1030

    Obelisky no dully

--- src/main/java/net/jitl/client/render/tile/ObeliskTER.java
+++ src/main/java/net/jitl/client/render/tile/ObeliskTER.java
@@ -5,6 +5,7 @@ import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.jitl.JITL;
 import net.jitl.client.render.model.block.ObeliskModel;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.RenderState;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
@@ -14,6 +15,8 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.vector.Vector3f;
+import net.minecraft.world.LightType;
+import net.minecraft.world.World;
 
 public class ObeliskTER extends TileEntityRenderer {
 
@@ -29,24 +32,30 @@ public class ObeliskTER extends TileEntityRenderer {
 
         IVertexBuilder ivertexbuilder = bufferIn.getBuffer(obelisk(OBELISK_LOCATION));
         float timeD = (float) (360.0 * (System.currentTimeMillis() & 0x3FFFL) / 0x3FFFL) / 16;
+        int lightL = getProperLightLevel(tileEntityIn.getLevel(), tileEntityIn.getBlockPos());
 
         matrixStackIn.pushPose();
         matrixStackIn.translate(0.5F, 1.75, 0.5F);
         matrixStackIn.mulPose(Vector3f.YP.rotation(timeD));
-        obelisk.render(matrixStackIn, ivertexbuilder, combinedLightIn, combinedOverlayIn, true);
+        obelisk.render(matrixStackIn, ivertexbuilder, lightL, lightL, true);
         matrixStackIn.popPose();
 
         matrixStackIn.pushPose();
         matrixStackIn.translate(0.5F, -0.5, 0.5F);
         matrixStackIn.mulPose(Vector3f.YP.rotation(-timeD));
-        obelisk.render(matrixStackIn, ivertexbuilder, combinedLightIn, combinedOverlayIn, false);
+        obelisk.render(matrixStackIn, ivertexbuilder, combinedOverlayIn, lightL, false);
         matrixStackIn.popPose();
 
     }
 
     public static RenderType obelisk(ResourceLocation resourceLocation) {
         RenderState.TextureState textureState = new RenderState.TextureState(OBELISK_LOCATION, false, false);
-
         return RenderType.create("obelisk", DefaultVertexFormats.BLOCK, 7, 256, false, true, RenderType.State.builder().setTextureState(textureState).createCompositeState(true));
+    }
+
+    private int getProperLightLevel(World w, BlockPos pos) {
+        int blockLight = w.getBrightness(LightType.BLOCK, pos);
+        int skyLevel = w.getBrightness(LightType.SKY, pos);
+        return LightTexture.pack(blockLight, skyLevel);
     }
 }
