87035ab239c4ef8b1c0ad22bf8b1ef95b85deeed
The_Slayer <brock.kerley@hotmail.com>
2022-01-07 20:02:23 +1030

    Tower brain

--- src/main/java/net/jitl/common/tile/GuardianTowerBrainTile.java
+++ src/main/java/net/jitl/common/tile/GuardianTowerBrainTile.java
@@ -1,16 +1,14 @@
 package net.jitl.common.tile;
 
 import net.jitl.init.JTiles;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.TickableBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
 import ru.timeconqueror.timecore.api.common.tile.SyncableTile;
 
-public class GuardianTowerBrainTile extends SyncableTile implements TickableBlockEntity {
-    public GuardianTowerBrainTile() {
-        super(JTiles.GUARDIAN_TOWER_BRAIN);
+public class GuardianTowerBrainTile extends SyncableTile {
+    public GuardianTowerBrainTile(BlockPos pos, BlockState state) {
+        super(JTiles.GUARDIAN_TOWER_BRAIN, pos, state);
     }
 
-    @Override
-    public void tick() {
-
-    }
 }
