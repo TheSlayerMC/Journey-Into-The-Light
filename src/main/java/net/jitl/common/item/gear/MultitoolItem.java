1db69126a02cf0c8998764b456ef80ac827e55e2
The_Slayer <brock.kerley@hotmail.com>
2022-01-09 14:21:50 +1030

    Update MultitoolItem.java

--- src/main/java/net/jitl/common/item/gear/MultitoolItem.java
+++ src/main/java/net/jitl/common/item/gear/MultitoolItem.java
@@ -1,14 +1,20 @@
 package net.jitl.common.item.gear;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
+import com.mojang.datafixers.util.Pair;
 import net.jitl.common.helper.JToolTiers;
 import net.jitl.common.item.gear.abilities.IAbility;
 import net.jitl.init.JTabs;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.Tag;
+import net.minecraft.world.item.*;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.material.Material;
-import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.InteractionResult;
@@ -19,68 +25,92 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Registry;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.common.Tags;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
-import net.minecraft.world.item.DiggerItem;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
 
 public class MultitoolItem extends DiggerItem implements JGear {
     private IAbility ability;
 
-    public MultitoolItem(JToolTiers tier, IAbility multiAbility) {
-        super((int) tier.getShovelDam(), tier.getAttackSpeed(), tier, BlockTags.MINEABLE_WITH_PICKAXE, new Properties().tab(JTabs.TOOLS)
-                .addToolType(ToolType.HOE, tier.getLevel())
-                .addToolType(ToolType.AXE, tier.getLevel())
-                .addToolType(ToolType.PICKAXE, tier.getLevel())
-                .addToolType(ToolType.SHOVEL, tier.getLevel()));
+    private final Set<Tag.Named<Block>> tags = Sets.newHashSet(BlockTags.MINEABLE_WITH_PICKAXE, BlockTags.MINEABLE_WITH_AXE, BlockTags.MINEABLE_WITH_SHOVEL);
+    protected static final Map<Block, Pair<Predicate<UseOnContext>, Consumer<UseOnContext>>> TILLABLES = Maps.newHashMap(ImmutableMap.of(Blocks.GRASS_BLOCK, Pair.of(HoeItem::onlyIfAirAbove, changeIntoState(Blocks.FARMLAND.defaultBlockState())), Blocks.DIRT_PATH, Pair.of(HoeItem::onlyIfAirAbove, changeIntoState(Blocks.FARMLAND.defaultBlockState())), Blocks.DIRT, Pair.of(HoeItem::onlyIfAirAbove, changeIntoState(Blocks.FARMLAND.defaultBlockState())), Blocks.COARSE_DIRT, Pair.of(HoeItem::onlyIfAirAbove, changeIntoState(Blocks.DIRT.defaultBlockState())), Blocks.ROOTED_DIRT, Pair.of((useOnContext_) -> {
+        return true;
+    }, changeIntoStateAndDropItem(Blocks.DIRT.defaultBlockState(), Items.HANGING_ROOTS))));
 
+    public MultitoolItem(JToolTiers tier, IAbility multiAbility) {
+        super(tier.getShovelDam(), tier.getAttackSpeed(), tier, BlockTags.MINEABLE_WITH_PICKAXE, new Properties().tab(JTabs.TOOLS));
         this.ability = multiAbility;
     }
 
     @Override
-    public boolean isCorrectToolForDrops(BlockState blockIn) {
-        if (blockIn.is(Blocks.SNOW) || blockIn.is(Blocks.SNOW_BLOCK)) return true;
-        int i = this.getTier().getLevel();
-        if (blockIn.getHarvestTool() == net.minecraftforge.common.ToolType.PICKAXE)
-            return i >= blockIn.getHarvestLevel();
-        Material material = blockIn.getMaterial();
-        return material == Material.STONE || material == Material.METAL || material == Material.HEAVY_METAL;
+    public boolean isCorrectToolForDrops(ItemStack stack, BlockState state) {
+        return state.is(BlockTags.MINEABLE_WITH_PICKAXE) || state.is(BlockTags.MINEABLE_WITH_AXE) || state.is(BlockTags.MINEABLE_WITH_SHOVEL) && net.minecraftforge.common.TierSortingRegistry.isCorrectTierForDrops(getTier(), state);
     }
 
-    @Override
+    /*@Override
     public float getDestroySpeed(ItemStack stack, BlockState state) {
         Material material = state.getMaterial();
         float value = material != Material.METAL && material != Material.HEAVY_METAL && material != Material.STONE ? super.getDestroySpeed(stack, state) : this.speed;
         return ability.blockBreakSpeed(stack, state, value);
+    }*/
+
+    @Override
+    public float getDestroySpeed(ItemStack stack_, BlockState state_) {
+        return this.tags.contains(state_.getBlock()) ? this.speed : 1.0F;
     }
 
     @Override
-    public InteractionResult useOn(UseOnContext context) {
-        Level world = context.getLevel();
-        BlockPos blockpos = context.getClickedPos();
-        int hook = net.minecraftforge.event.ForgeEventFactory.onHoeUse(context);
+    public @NotNull InteractionResult useOn(UseOnContext context_) {
+        Level level = context_.getLevel();
+        BlockPos blockpos = context_.getClickedPos();
+        Pair<Predicate<UseOnContext>, Consumer<UseOnContext>> pair = TILLABLES.get(level.getBlockState(blockpos).getBlock());
+        int hook = net.minecraftforge.event.ForgeEventFactory.onHoeUse(context_);
         if (hook != 0) return hook > 0 ? InteractionResult.SUCCESS : InteractionResult.FAIL;
-        if (context.getClickedFace() != Direction.DOWN && world.isEmptyBlock(blockpos.above())) {
-            BlockState blockstate = world.getBlockState(blockpos).getToolModifiedState(world, blockpos, context.getPlayer(), context.getItemInHand(), net.minecraftforge.common.ToolType.HOE);
-            if (blockstate != null) {
-                Player playerentity = context.getPlayer();
-                world.playSound(playerentity, blockpos, SoundEvents.HOE_TILL, SoundSource.BLOCKS, 1.0F, 1.0F);
-                if (!world.isClientSide) {
-                    world.setBlock(blockpos, blockstate, 11);
-                    if (playerentity != null) {
-                        context.getItemInHand().hurtAndBreak(1, playerentity, (playerEntity_) -> {
-                            playerEntity_.broadcastBreakEvent(context.getHand());
-                        });
+        if (context_.getClickedFace() != Direction.DOWN && level.isEmptyBlock(blockpos.above())) {
+            if (pair == null) {
+                return InteractionResult.PASS;
+            } else {
+                Predicate<UseOnContext> predicate = pair.getFirst();
+                Consumer<UseOnContext> consumer = pair.getSecond();
+                if (predicate.test(context_)) {
+                    Player player = context_.getPlayer();
+                    level.playSound(player, blockpos, SoundEvents.HOE_TILL, SoundSource.BLOCKS, 1.0F, 1.0F);
+                    if (!level.isClientSide) {
+                        consumer.accept(context_);
+                        if (player != null) {
+                            context_.getItemInHand().hurtAndBreak(1, player, (contextEntity_) -> {
+                                contextEntity_.broadcastBreakEvent(context_.getHand());
+                            });
+                        }
                     }
+                    return InteractionResult.sidedSuccess(level.isClientSide);
+                } else {
+                    return InteractionResult.PASS;
                 }
-                return InteractionResult.sidedSuccess(world.isClientSide);
             }
         }
-
         return InteractionResult.PASS;
+    }
+
+    public static Consumer<UseOnContext> changeIntoState(BlockState state_) {
+        return (context1_) -> {
+            context1_.getLevel().setBlock(context1_.getClickedPos(), state_, 11);
+        };
+    }
+
+    public static Consumer<UseOnContext> changeIntoStateAndDropItem(BlockState state_, ItemLike itemToDrop_) {
+        return (context2_) -> {
+            context2_.getLevel().setBlock(context2_.getClickedPos(), state_, 11);
+            Block.popResourceFromFace(context2_.getLevel(), context2_.getClickedPos(), context2_.getClickedFace(), new ItemStack(itemToDrop_));
+        };
     }
 
     @Override
