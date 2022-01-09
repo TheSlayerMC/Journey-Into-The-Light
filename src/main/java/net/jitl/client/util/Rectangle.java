b44798f29c24ada2bc55529e0c81b25960a20692
The_Slayer <brock.kerley@hotmail.com>
2022-01-09 15:58:03 +1030

    Update Rectangle.java

--- src/main/java/net/jitl/client/util/Rectangle.java
+++ src/main/java/net/jitl/client/util/Rectangle.java
@@ -1,6 +1,18 @@
 package net.jitl.client.util;
 
-public record Rectangle(int left, int top, int right, int bottom) {
+public class Rectangle {
+
+    private final int left;
+    private final int top;
+    private final int right;
+    private final int bottom;
+
+    public Rectangle(int left, int top, int right, int bottom) {
+        this.left = left;
+        this.top = top;
+        this.right = right;
+        this.bottom = bottom;
+    }
 
     public static Rectangle createWithWidthHeight(int left, int top, int width, int height) {
         return new Rectangle(left, top, left + width, top + height);
@@ -12,5 +24,21 @@ public record Rectangle(int left, int top, int right, int bottom) {
 
     public int height() {
         return bottom - top;
+    }
+
+    public int right() {
+        return right;
+    }
+
+    public int left() {
+        return left;
+    }
+
+    public int top() {
+        return top;
+    }
+
+    public int bottom() {
+        return bottom;
     }
 }
