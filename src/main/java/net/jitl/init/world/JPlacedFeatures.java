9b7f23f2161fefeb329e22491046eeb0b0c335c0
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2022-01-10 01:50:53 -0700

    Started placed features

--- src/main/java/net/jitl/init/world/JPlacedFeatures.java
+++ src/main/java/net/jitl/init/world/JPlacedFeatures.java
@@ -1,10 +1,28 @@
 package net.jitl.init.world;
 
 import net.jitl.JITL;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraft.world.level.levelgen.placement.*;
 import ru.timeconqueror.timecore.api.registry.PlacedFeatureRegister;
+import ru.timeconqueror.timecore.api.registry.util.AutoRegistrable;
+
+import java.util.List;
 
 public class JPlacedFeatures {
 
+    @AutoRegistrable
     public static final PlacedFeatureRegister REGISTER = new PlacedFeatureRegister(JITL.MODID);
 
+    public static final PlacedFeature GENERIC_OVERWORLD_RUINS = REGISTER.register(
+                    "generic_overworld_ruins",
+                    GenerationStep.Decoration.SURFACE_STRUCTURES,
+                    () -> JConfiguredFeatures.DEFAULT_OVERWORLD_RUINS.get()
+                            .placed(patch(10)))
+            .asPromise()
+            .get();
+
+    private static List<PlacementModifier> patch(int count) {
+        return List.of(CountPlacement.of(count), InSquarePlacement.spread(), PlacementUtils.FULL_RANGE, BiomeFilter.biome());
+    }
 }
