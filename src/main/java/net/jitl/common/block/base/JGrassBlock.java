5158fa6a8d97dc928a9b711d53d794875bdd55c5
The_Slayer <brock.kerley@hotmail.com>
2022-01-21 20:14:18 +1030

    grass work

--- src/main/java/net/jitl/common/block/base/JGrassBlock.java
+++ src/main/java/net/jitl/common/block/base/JGrassBlock.java
@@ -5,16 +5,20 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.data.worldgen.placement.VegetationPlacements;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.FluidTags;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.BonemealableBlock;
+import net.minecraft.world.level.block.SnowLayerBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.configurations.RandomPatchConfiguration;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+import net.minecraft.world.level.lighting.LayerLightEngine;
 import net.minecraftforge.common.IPlantable;
 
 import java.util.List;
@@ -22,9 +26,11 @@ import java.util.Random;
 
 public class JGrassBlock extends Block implements BonemealableBlock {
 
-    //Make this work like normal grass (can spread to specific dirt and can bonemeal)
-    public JGrassBlock(Properties properties) {
+    private final Block dirt;
+
+    public JGrassBlock(Properties properties, Block dirt) {
         super(properties);
+        this.dirt = dirt;
     }
 
     @Override
@@ -80,6 +86,36 @@ public class JGrassBlock extends Block implements BonemealableBlock {
                     placedfeature.place(level, level.getChunkSource().getGenerator(), rand, blockpos1);
             }
         }
+    }
 
+    private static boolean canBeGrass(BlockState state, LevelReader levelReader, BlockPos pos) {
+        BlockPos blockpos = pos.above();
+        BlockState blockstate = levelReader.getBlockState(blockpos);
+        int i = LayerLightEngine.getLightBlockInto(levelReader, state, pos, blockstate, blockpos, Direction.UP, blockstate.getLightBlock(levelReader, blockpos));
+        return i < levelReader.getMaxLightLevel();
+    }
+
+    private static boolean canPropagate(BlockState state, LevelReader level, BlockPos pos) {
+        BlockPos blockpos = pos.above();
+        return canBeGrass(state, level, pos) && !level.getFluidState(blockpos).is(FluidTags.WATER);
+    }
+
+    @Override
+    public void randomTick(BlockState state, ServerLevel level, BlockPos pos, Random random_) {
+        if(!canBeGrass(state, level, pos)) {
+            if(!level.isAreaLoaded(pos, 1)) return;
+            level.setBlockAndUpdate(pos, this.dirt.defaultBlockState());
+        } else {
+            if (!level.isAreaLoaded(pos, 3)) return;
+            if (level.getMaxLocalRawBrightness(pos.above()) >= 9) {
+                BlockState blockstate = this.defaultBlockState();
+                for(int i = 0; i < 4; ++i) {
+                    BlockPos blockpos = pos.offset(random_.nextInt(3) - 1, random_.nextInt(5) - 3, random_.nextInt(3) - 1);
+                    if (level.getBlockState(blockpos).is(this.dirt) && canPropagate(blockstate, level, blockpos)) {
+                        level.setBlockAndUpdate(blockpos, blockstate);
+                    }
+                }
+            }
+        }
     }
 }
