69c0fcebf8606bcdbdcf7633abb321ea0109ff9f
The_Slayer <brock.kerley@hotmail.com>
2022-01-23 12:17:56 +1030

    Update ShattererModel.java

--- src/main/java/net/jitl/client/render/model/frozen/ShattererModel.java
+++ src/main/java/net/jitl/client/render/model/frozen/ShattererModel.java
@@ -18,22 +18,27 @@ public class ShattererModel <T extends Entity> extends ListModel<T> {
     public static LayerDefinition createBodyLayer() {
         MeshDefinition meshdefinition = new MeshDefinition();
         PartDefinition partdefinition = meshdefinition.getRoot();
-
-        PartDefinition body = partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0).addBox(-5.0F, -4.9531F, -1.5F, 10.0F, 10.0F, 3.0F, new CubeDeformation(0.0F))
-                .texOffs(16, 13).addBox(-3.0F, 13.0469F, -0.5F, 6.0F, 6.0F, 1.0F, new CubeDeformation(0.0F))
-                .texOffs(16, 13).addBox(13.0F, -2.9531F, -0.5F, 6.0F, 6.0F, 1.0F, new CubeDeformation(0.0F))
-                .texOffs(16, 13).addBox(-19.0F, -2.9531F, -0.5F, 6.0F, 6.0F, 1.0F, new CubeDeformation(0.0F))
-                .texOffs(16, 13).addBox(-3.0F, -18.9531F, -0.5F, 6.0F, 6.0F, 1.0F, new CubeDeformation(0.0F))
-                .texOffs(16, 32).addBox(5.0F, -0.9531F, -0.5F, 8.0F, 2.0F, 1.0F, new CubeDeformation(0.0F))
-                .texOffs(3, 35).addBox(-1.0F, -12.9531F, -0.5F, 2.0F, 8.0F, 1.0F, new CubeDeformation(0.0F))
-                .texOffs(16, 32).addBox(-13.0F, -0.9531F, -0.5F, 8.0F, 2.0F, 1.0F, new CubeDeformation(0.0F))
-                .texOffs(3, 35).addBox(-1.0F, 5.0469F, -0.5F, 2.0F, 8.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 4.9531F, 1.5F));
-
-        PartDefinition bRStalk_r1 = body.addOrReplaceChild("bRStalk_r1", CubeListBuilder.create().texOffs(3, 35).addBox(-1.6275F, 3.6275F, -0.5F, 2.0F, 8.0F, 1.0F, new CubeDeformation(0.0F))
-                .texOffs(3, 35).addBox(-1.6275F, -11.3725F, -0.5F, 2.0F, 8.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -0.6395F, 0.0F, 0.0F, 0.0F, -0.7854F));
-
+        PartDefinition body = partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0).addBox(-4.952F, -4.9995F, -1.5021F, 10.0F, 10.0F, 3.0F, new CubeDeformation(0.0F))
+                .texOffs(16, 13).addBox(-2.952F, 13.0005F, -0.5021F, 6.0F, 6.0F, 1.0F, new CubeDeformation(0.0F))
+                .texOffs(16, 13).addBox(13.048F, -2.9995F, -0.5021F, 6.0F, 6.0F, 1.0F, new CubeDeformation(0.0F))
+                .texOffs(16, 13).addBox(-18.952F, -2.9995F, -0.5021F, 6.0F, 6.0F, 1.0F, new CubeDeformation(0.0F))
+                .texOffs(16, 13).addBox(-2.952F, -18.9995F, -0.5021F, 6.0F, 6.0F, 1.0F, new CubeDeformation(0.0F))
+                .texOffs(16, 32).addBox(5.048F, -0.9995F, -0.5021F, 8.0F, 2.0F, 1.0F, new CubeDeformation(0.0F))
+                .texOffs(3, 35).addBox(-0.952F, -12.9995F, -0.5021F, 2.0F, 8.0F, 1.0F, new CubeDeformation(0.0F))
+                .texOffs(16, 32).addBox(-12.952F, -0.9995F, -0.5021F, 8.0F, 2.0F, 1.0F, new CubeDeformation(0.0F))
+                .texOffs(3, 35).addBox(-0.952F, 5.0005F, -0.5021F, 2.0F, 8.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(-0.048F, 4.9995F, 1.5021F));
+        PartDefinition s8_r1 = body.addOrReplaceChild("s8_r1", CubeListBuilder.create().texOffs(3, 35).addBox(1.0F, 0.0F, -0.49F, 2.0F, 4.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-9.8515F, -1.2127F, -0.0021F, 0.0F, 0.0F, 0.7854F));
+        PartDefinition s1_r1 = body.addOrReplaceChild("s1_r1", CubeListBuilder.create().texOffs(3, 35).addBox(-2.0F, -0.8725F, -0.4955F, 2.0F, 4.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(9.4431F, 0.1114F, 0.0034F, 0.0F, 0.0F, -0.7854F));
+        PartDefinition s2_r1 = body.addOrReplaceChild("s2_r1", CubeListBuilder.create().texOffs(3, 35).addBox(-2.1275F, -3.0F, -0.5045F, 2.0F, 4.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(9.4431F, 0.1114F, 0.0034F, 0.0F, 0.0F, 0.7854F));
+        PartDefinition s3_r1 = body.addOrReplaceChild("s3_r1", CubeListBuilder.create().texOffs(3, 35).addBox(-1.0F, -2.0F, -0.49F, 2.0F, 4.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(1.4623F, -9.698F, -0.0021F, 0.0F, 0.0F, 0.7854F));
+        PartDefinition s4_r1 = body.addOrReplaceChild("s4_r1", CubeListBuilder.create().texOffs(3, 35).addBox(-3.0F, -4.0F, -0.499F, 2.0F, 4.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(1.4623F, -9.698F, -0.0021F, 0.0F, 0.0F, -0.7854F));
+        PartDefinition s5_r1 = body.addOrReplaceChild("s5_r1", CubeListBuilder.create().texOffs(3, 35).addBox(-1.0F, -2.0F, -0.49F, 2.0F, 4.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-1.3662F, 9.6868F, -0.0021F, 0.0F, 0.0F, 0.7854F));
+        PartDefinition s6_r1 = body.addOrReplaceChild("s6_r1", CubeListBuilder.create().texOffs(3, 35).addBox(-1.0F, -2.0F, -0.5F, 2.0F, 4.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(1.4623F, 9.6868F, -0.0011F, 0.0F, 0.0F, -0.7854F));
+        PartDefinition s7_r1 = body.addOrReplaceChild("s7_r1", CubeListBuilder.create().texOffs(3, 35).addBox(-1.0F, -2.0F, -0.499F, 2.0F, 4.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-9.8515F, -1.2127F, -0.0021F, 0.0F, 0.0F, -0.7854F));
+        PartDefinition rTPin_r1 = body.addOrReplaceChild("rTPin_r1", CubeListBuilder.create().texOffs(3, 35).addBox(-1.6275F, -11.3725F, -0.5F, 2.0F, 8.0F, 1.0F, new CubeDeformation(0.0F))
+                .texOffs(3, 35).addBox(-1.6275F, 3.6275F, -0.5F, 2.0F, 8.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.048F, -0.6859F, -0.0021F, 0.0F, 0.0F, -0.7854F));
         PartDefinition bLStalk_r1 = body.addOrReplaceChild("bLStalk_r1", CubeListBuilder.create().texOffs(3, 35).addBox(-0.3725F, 3.6275F, -0.5F, 2.0F, 8.0F, 1.0F, new CubeDeformation(0.0F))
-                .texOffs(3, 35).addBox(-0.3725F, -11.3725F, -0.5F, 2.0F, 8.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -0.6395F, 0.0F, 0.0F, 0.0F, 0.7854F));
+                .texOffs(3, 35).addBox(-0.3725F, -11.3725F, -0.5F, 2.0F, 8.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.048F, -0.6859F, -0.0021F, 0.0F, 0.0F, 0.7854F));
 
         return LayerDefinition.create(meshdefinition, 64, 64);
     }
