b71aa07a67b2af2b9949dcfe4537028b04d13352
The_Slayer <brock.kerley@hotmail.com>
2022-01-26 16:08:53 +1030

    Update EnumKnowledgeType.java

--- src/main/java/net/jitl/common/helper/EnumKnowledgeType.java
+++ src/main/java/net/jitl/common/helper/EnumKnowledgeType.java
@@ -8,38 +8,38 @@ import net.minecraft.network.FriendlyByteBuf;
 import java.util.HashMap;
 
 public enum EnumKnowledgeType {
-    OVERWORLD("overworld", new KnowledgeXPDisplay("Overworld", JItems.OVERWORLD_KNOWLEDGE, false)
-            , new KnowledgeXPDisplay("Overworld", JItems.OVERWORLD_KNOWLEDGE, true)),
+    OVERWORLD("overworld", new KnowledgeXPDisplay("overworld", JItems.OVERWORLD_KNOWLEDGE, false)
+            , new KnowledgeXPDisplay("overworld", JItems.OVERWORLD_KNOWLEDGE, true)),
 
-    NETHER("nether", new KnowledgeXPDisplay("The Nether", JItems.NETHER_KNOWLEDGE, false)
-            , new KnowledgeXPDisplay("The Nether", JItems.NETHER_KNOWLEDGE, true)),
+    NETHER("nether", new KnowledgeXPDisplay("the_nether", JItems.NETHER_KNOWLEDGE, false)
+            , new KnowledgeXPDisplay("the_nether", JItems.NETHER_KNOWLEDGE, true)),
 
-    END("end", new KnowledgeXPDisplay("The End", JItems.END_KNOWLEDGE, false)
-            , new KnowledgeXPDisplay("The End", JItems.END_KNOWLEDGE, true)),
+    END("end", new KnowledgeXPDisplay("the_end", JItems.END_KNOWLEDGE, false)
+            , new KnowledgeXPDisplay("the_end", JItems.END_KNOWLEDGE, true)),
 
-    EUCA("euca", new KnowledgeXPDisplay("Euca", JItems.EUCA_KNOWLEDGE, false)
-            , new KnowledgeXPDisplay("Euca", JItems.EUCA_KNOWLEDGE, true)),
+    EUCA("euca", new KnowledgeXPDisplay("euca", JItems.EUCA_KNOWLEDGE, false)
+            , new KnowledgeXPDisplay("euca", JItems.EUCA_KNOWLEDGE, true)),
 
-    BOIL("boil", new KnowledgeXPDisplay("Boiling Point", JItems.BOIL_KNOWLEDGE, false)
-            , new KnowledgeXPDisplay("Boiling Point", JItems.BOIL_KNOWLEDGE, true)),
+    BOIL("boil", new KnowledgeXPDisplay("boiling_point", JItems.BOIL_KNOWLEDGE, false)
+            , new KnowledgeXPDisplay("boiling_point", JItems.BOIL_KNOWLEDGE, true)),
 
-    FROZEN("frozen", new KnowledgeXPDisplay("Frozen", JItems.FROZEN_KNOWLEDGE, false)
-            , new KnowledgeXPDisplay("Frozen", JItems.FROZEN_KNOWLEDGE, true)),
+    FROZEN("frozen", new KnowledgeXPDisplay("frozen", JItems.FROZEN_KNOWLEDGE, false)
+            , new KnowledgeXPDisplay("frozen", JItems.FROZEN_KNOWLEDGE, true)),
 
-    DEPTHS("depths", new KnowledgeXPDisplay("The Depths", JItems.DEPTHS_KNOWLEDGE, false)
-            , new KnowledgeXPDisplay("The Depths", JItems.DEPTHS_KNOWLEDGE, true)),
+    DEPTHS("depths", new KnowledgeXPDisplay("the_depths", JItems.DEPTHS_KNOWLEDGE, false)
+            , new KnowledgeXPDisplay("the_depths", JItems.DEPTHS_KNOWLEDGE, true)),
 
-    CORBA("corba", new KnowledgeXPDisplay("Corba", JItems.CORBA_KNOWLEDGE, false)
-            , new KnowledgeXPDisplay("Corba", JItems.CORBA_KNOWLEDGE, true)),
+    CORBA("corba", new KnowledgeXPDisplay("corba", JItems.CORBA_KNOWLEDGE, false)
+            , new KnowledgeXPDisplay("corba", JItems.CORBA_KNOWLEDGE, true)),
 
-    CLOUDIA("cloudia", new KnowledgeXPDisplay("Cloudia", JItems.CLOUDIA_KNOWLEDGE, false)
-            , new KnowledgeXPDisplay("Cloudia", JItems.CLOUDIA_KNOWLEDGE, true)),
+    CLOUDIA("cloudia", new KnowledgeXPDisplay("cloudia", JItems.CLOUDIA_KNOWLEDGE, false)
+            , new KnowledgeXPDisplay("cloudia", JItems.CLOUDIA_KNOWLEDGE, true)),
 
-    TERRANIA("terrania", new KnowledgeXPDisplay("Terrania", JItems.TERRANIA_KNOWLEDGE, false)
-            , new KnowledgeXPDisplay("Terrania", JItems.TERRANIA_KNOWLEDGE, true)),
+    TERRANIA("terrania", new KnowledgeXPDisplay("terrania", JItems.TERRANIA_KNOWLEDGE, false)
+            , new KnowledgeXPDisplay("terrania", JItems.TERRANIA_KNOWLEDGE, true)),
 
-    SENTERIAN("senterian", new KnowledgeXPDisplay("Senterian", JItems.SENTERIAN_KNOWLEDGE, false)
-            , new KnowledgeXPDisplay("Senterian", JItems.SENTERIAN_KNOWLEDGE, true));
+    SENTERIAN("senterian", new KnowledgeXPDisplay("senterian", JItems.SENTERIAN_KNOWLEDGE, false)
+            , new KnowledgeXPDisplay("senterian", JItems.SENTERIAN_KNOWLEDGE, true));
 
     private static final HashMap<String, EnumKnowledgeType> BY_NAME = new HashMap<>();
 
