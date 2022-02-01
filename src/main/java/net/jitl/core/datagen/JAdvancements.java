19b319ce2f216bc09dbadb0de41b323fba0537ff
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2022-02-01 09:48:36 -0700

    Translated advancements, added Frozen Troll advancement

--- src/main/java/net/jitl/core/datagen/JAdvancements.java
+++ src/main/java/net/jitl/core/datagen/JAdvancements.java
@@ -7,10 +7,7 @@ import net.jitl.core.init.JItems;
 import net.jitl.core.init.world.Dimensions;
 import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.FrameType;
-import net.minecraft.advancements.critereon.ChangeDimensionTrigger;
-import net.minecraft.advancements.critereon.EntityPredicate;
-import net.minecraft.advancements.critereon.StartRidingTrigger;
-import net.minecraft.advancements.critereon.TickTrigger;
+import net.minecraft.advancements.critereon.*;
 import net.minecraft.network.chat.TranslatableComponent;
 
 import java.util.function.Consumer;
@@ -26,8 +23,8 @@ public class JAdvancements implements Consumer<Consumer<Advancement>> {
         Advancement root = Advancement.Builder.advancement()
                 .display(
                         JBlocks.EUCA_GOLD_GRASS_BLOCK,
-                        new TranslatableComponent("jitl.advancement.overworld.root.title"),
-                        new TranslatableComponent("jitl.advancement.overworld.root.desc"),
+                        new TranslatableComponent("advancement.jitl.overworld.root.title"),
+                        new TranslatableComponent("advancement.jitl.overworld.root.desc"),
                         JITL.rl("textures/block/dungeon_bricks.png"),
                         FrameType.TASK,
                         false,
@@ -42,8 +39,8 @@ public class JAdvancements implements Consumer<Consumer<Advancement>> {
         Advancement root = Advancement.Builder.advancement()
                 .display(
                         JBlocks.FROZEN_PORTAL_FRAME,
-                        new TranslatableComponent("jitl.advancement.frozen.root.title"),
-                        new TranslatableComponent("jitl.advancement.frozen.root.desc"),
+                        new TranslatableComponent("advancement.jitl.frozen.root.title"),
+                        new TranslatableComponent("advancement.jitl.frozen.root.desc"),
                         JITL.rl("textures/block/permafrost.png"),
                         FrameType.TASK,
                         true,
@@ -56,16 +53,33 @@ public class JAdvancements implements Consumer<Consumer<Advancement>> {
                 .parent(root)
                 .display(
                         JItems.REDCURRANT_BERRY,
-                        new TranslatableComponent("jitl.advancement.frozen.ride_capybara.title"),
-                        new TranslatableComponent("jitl.advancement.frozen.ride_capybara.desc"),
+                        new TranslatableComponent("advancement.jitl.frozen.ride_capybara.title"),
+                        new TranslatableComponent("advancement.jitl.frozen.ride_capybara.desc"),
                         null,
-                        FrameType.TASK,
+                        FrameType.GOAL,
                         true,
                         true,
                         false
                 )
                 .addCriterion("ride_capybara", StartRidingTrigger.TriggerInstance.playerStartsRiding(EntityPredicate.Builder.entity().vehicle(EntityPredicate.Builder.entity().of(JEntities.CAPYBARA_TYPE).build())))
                 .save(advancementConsumer, frozenID("ride_capybara"));
+        Advancement barter_troll = Advancement.Builder.advancement()
+                .parent(root)
+                .display(
+                        JItems.RIMESTONE,
+                        new TranslatableComponent("advancement.jitl.frozen.barter_troll.title"),
+                        new TranslatableComponent("advancement.jitl.frozen.barter_troll.desc"),
+                        null,
+                        FrameType.TASK,
+                        true,
+                        true,
+                        false
+                )
+                .addCriterion("barter_troll", PlayerInteractTrigger.TriggerInstance.itemUsedOnEntity(
+                        EntityPredicate.Composite.ANY,
+                        ItemPredicate.Builder.item().of(JItems.RIMESTONE),
+                        EntityPredicate.Composite.wrap(EntityPredicate.Builder.entity().of(JEntities.FROZEN_TROLL_TYPE).build())))
+                .save(advancementConsumer, frozenID("barter_troll"));
     }
 
     private String overworldID(String name) {
