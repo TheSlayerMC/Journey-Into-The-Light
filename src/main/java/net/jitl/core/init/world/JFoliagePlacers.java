7aec90786fe9f8b93b46bd7bab21df5b701f51ce
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2022-02-07 10:51:42 -0700

    Switched back to Forge registry for foliage placers and trunk decorators

--- src/main/java/net/jitl/core/init/world/JFoliagePlacers.java
+++ src/main/java/net/jitl/core/init/world/JFoliagePlacers.java
@@ -2,17 +2,16 @@ package net.jitl.core.init.world;
 
 import net.jitl.common.world.gen.foliageplacer.SphericalFoliagePlacer;
 import net.jitl.core.JITL;
-import net.minecraft.core.Registry;
 import net.minecraft.world.level.levelgen.feature.foliageplacers.FoliagePlacerType;
-import ru.timeconqueror.timecore.api.registry.SimpleVanillaRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import ru.timeconqueror.timecore.api.registry.SimpleForgeRegister;
 import ru.timeconqueror.timecore.api.registry.util.AutoRegistrable;
-import ru.timeconqueror.timecore.api.registry.util.Promised;
 
 public class JFoliagePlacers {
 
     @AutoRegistrable
-    //TODO: change back to forge registry once they get their shit together
-    public static final SimpleVanillaRegister<FoliagePlacerType<?>> REGISTER = new SimpleVanillaRegister<>(JITL.MODID, Registry.FOLIAGE_PLACER_TYPES);
+    public static final SimpleForgeRegister<FoliagePlacerType<?>> REGISTER = new SimpleForgeRegister<>(ForgeRegistries.FOLIAGE_PLACER_TYPES, JITL.MODID);
 
-    public static final Promised<FoliagePlacerType<SphericalFoliagePlacer>> SPHERICAL_FOLIAGE_PLACER = REGISTER.register("spherical_foliage_placer", () -> new FoliagePlacerType<>(SphericalFoliagePlacer.CODEC));
+    public static final RegistryObject<FoliagePlacerType<SphericalFoliagePlacer>> SPHERICAL_FOLIAGE_PLACER = REGISTER.register("spherical_foliage_placer", () -> new FoliagePlacerType<>(SphericalFoliagePlacer.CODEC));
 }
--- src/main/java/net/jitl/core/init/world/JTreeDecorators.java
+++ src/main/java/net/jitl/core/init/world/JTreeDecorators.java
@@ -2,29 +2,26 @@ package net.jitl.core.init.world;
 
 import net.jitl.common.world.gen.treedecorator.*;
 import net.jitl.core.JITL;
-import net.minecraft.core.Registry;
 import net.minecraft.world.level.levelgen.feature.treedecorators.TreeDecoratorType;
-import ru.timeconqueror.timecore.api.registry.SimpleVanillaRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import ru.timeconqueror.timecore.api.registry.SimpleForgeRegister;
 import ru.timeconqueror.timecore.api.registry.util.AutoRegistrable;
-import ru.timeconqueror.timecore.api.registry.util.Promised;
 
 public class JTreeDecorators {
-
-    //FIXME forge tree decorator registry is broken. currently using vanilla registry
     @AutoRegistrable
-    private static final SimpleVanillaRegister<TreeDecoratorType<?>> REGISTER = new SimpleVanillaRegister<>(JITL.MODID, Registry.TREE_DECORATOR_TYPES);
+    private static final SimpleForgeRegister<TreeDecoratorType<?>> REGISTER = new SimpleForgeRegister<>(ForgeRegistries.TREE_DECORATOR_TYPES, JITL.MODID);
 
-    public static final Promised<TreeDecoratorType<FrozenTreeDecorator>> FROZEN_DECORATOR =
+    public static final RegistryObject<TreeDecoratorType<FrozenTreeDecorator>> FROZEN_DECORATOR =
             REGISTER.register("frozen_tree_decorator", () -> new TreeDecoratorType<>(FrozenTreeDecorator.CODEC));
 
-    public static final Promised<TreeDecoratorType<CrystalFruitTreeDecorator>> CRYSTAL_FRUIT_DECORATOR = REGISTER.register("crystal_fruit_tree_decorator", () -> new TreeDecoratorType<>(CrystalFruitTreeDecorator.CODEC));
+    public static final RegistryObject<TreeDecoratorType<CrystalFruitTreeDecorator>> CRYSTAL_FRUIT_DECORATOR = REGISTER.register("crystal_fruit_tree_decorator", () -> new TreeDecoratorType<>(CrystalFruitTreeDecorator.CODEC));
 
-    public static final Promised<TreeDecoratorType<GlimmerRootTreeDecorator>> GLIMMER_ROOT_DECORATOR = REGISTER.register("glimmer_root_tree_decorator", () -> new TreeDecoratorType<>(GlimmerRootTreeDecorator.CODEC));
+    public static final RegistryObject<TreeDecoratorType<GlimmerRootTreeDecorator>> GLIMMER_ROOT_DECORATOR = REGISTER.register("glimmer_root_tree_decorator", () -> new TreeDecoratorType<>(GlimmerRootTreeDecorator.CODEC));
 
-    public static final Promised<TreeDecoratorType<IceShroomTreeDecorator>> ICE_SHROOM_TREE_DECORATOR = REGISTER.register("ice_shroom_tree_decorator", () -> new TreeDecoratorType<>(IceShroomTreeDecorator.CODEC));
+    public static final RegistryObject<TreeDecoratorType<IceShroomTreeDecorator>> ICE_SHROOM_TREE_DECORATOR = REGISTER.register("ice_shroom_tree_decorator", () -> new TreeDecoratorType<>(IceShroomTreeDecorator.CODEC));
 
-    public static final Promised<TreeDecoratorType<IcyBrushTreeDecorator>> ICY_BRUSH_TREE_DECORATOR = REGISTER.register("icy_brush_tree_decorator", () -> new TreeDecoratorType<>(IcyBrushTreeDecorator.CODEC));
+    public static final RegistryObject<TreeDecoratorType<IcyBrushTreeDecorator>> ICY_BRUSH_TREE_DECORATOR = REGISTER.register("icy_brush_tree_decorator", () -> new TreeDecoratorType<>(IcyBrushTreeDecorator.CODEC));
 
-    public static final Promised<TreeDecoratorType<CharredBrushTreeDecorator>> CHARRED_BRUSH_TREE_DECORATOR = REGISTER.register("charred_brush_tree_decorator", () -> new TreeDecoratorType<>(CharredBrushTreeDecorator.CODEC));
-
+    public static final RegistryObject<TreeDecoratorType<CharredBrushTreeDecorator>> CHARRED_BRUSH_TREE_DECORATOR = REGISTER.register("charred_brush_tree_decorator", () -> new TreeDecoratorType<>(CharredBrushTreeDecorator.CODEC));
 }
