ed8d1e7c46b85f55b1c65e7eed6273f4709fca4a
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2022-02-17 22:30:47 -0700

    Made "Rectangle" a record

--- src/main/java/net/jitl/client/util/Rectangle.java
+++ src/main/java/net/jitl/client/util/Rectangle.java
@@ -1,18 +1,6 @@
 package net.jitl.client.util;
 
-public class Rectangle {
-
-    private final int left;
-    private final int top;
-    private final int right;
-    private final int bottom;
-
-    public Rectangle(int left, int top, int right, int bottom) {
-        this.left = left;
-        this.top = top;
-        this.right = right;
-        this.bottom = bottom;
-    }
+public record Rectangle(int left, int top, int right, int bottom) {
 
     public static Rectangle fromWidthAndHeight(int left, int top, int width, int height) {
         return new Rectangle(left, top, left + width, top + height);
@@ -24,21 +12,5 @@ public class Rectangle {
 
     public int height() {
         return bottom - top;
-    }
-
-    public int right() {
-        return right;
-    }
-
-    public int left() {
-        return left;
-    }
-
-    public int top() {
-        return top;
-    }
-
-    public int bottom() {
-        return bottom;
     }
 }
