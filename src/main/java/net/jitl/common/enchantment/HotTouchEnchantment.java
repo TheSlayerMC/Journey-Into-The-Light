591f423af8abeabc9a94187427baf4d7fa9f26bc
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2022-02-18 03:31:57 -0700

    Made Hot Touch incompatible with silk touch, and also made it a treasure enchant

--- src/main/java/net/jitl/common/enchantment/HotTouchEnchantment.java
+++ src/main/java/net/jitl/common/enchantment/HotTouchEnchantment.java
@@ -3,6 +3,7 @@ package net.jitl.common.enchantment;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
+import net.minecraft.world.item.enchantment.UntouchingEnchantment;
 
 public class HotTouchEnchantment extends Enchantment {
 
@@ -23,5 +24,25 @@ public class HotTouchEnchantment extends Enchantment {
     @Override
     public int getMaxCost(int enchantmentLevel) {
         return 50;
+    }
+
+    @Override
+    public boolean isTreasureOnly() {
+        return true;
+    }
+
+    @Override
+    public boolean isTradeable() {
+        return false;
+    }
+
+    @Override
+    public boolean isDiscoverable() {
+        return false;
+    }
+
+    @Override
+    public boolean checkCompatibility(Enchantment enchantment) {
+        return !(enchantment instanceof UntouchingEnchantment) && super.checkCompatibility(enchantment);
     }
 }
