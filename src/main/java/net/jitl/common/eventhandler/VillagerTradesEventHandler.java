bacb2d01724202f2907f031851d4a6c0dd30cf67
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2022-02-26 12:23:27 -0700

    Added back bunker trade

--- src/main/java/net/jitl/common/eventhandler/VillagerTradesEventHandler.java
+++ src/main/java/net/jitl/common/eventhandler/VillagerTradesEventHandler.java
@@ -1,8 +1,12 @@
 package net.jitl.common.eventhandler;
 
+import net.jitl.common.entity.base.trades.MapTrade;
 import net.jitl.core.JITL;
+import net.jitl.core.init.world.JStructures;
 import net.minecraft.world.entity.npc.VillagerProfession;
 import net.minecraft.world.entity.npc.VillagerTrades;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.saveddata.maps.MapDecoration;
 import net.minecraftforge.event.village.VillagerTradesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -16,8 +20,7 @@ public class VillagerTradesEventHandler {
     public static void addTrades(VillagerTradesEvent event) {
         if (event.getType() == VillagerProfession.CARTOGRAPHER) {
             List<VillagerTrades.ItemListing> level5Trades = event.getTrades().get(5);
-            //level5Trades.add(new MapTrade(Items.EMERALD, 15, Items.COMPASS, 1, JStructures.ILlAGER_BUNKER.getStructure(), MapDecoration.Type.BANNER_LIGHT_GRAY, 12, 5));
-            //trades.add(new BasicTrade(new ItemStack(Items.EMERALD, 1), new ItemStack(JItems.SAPPHIRE, 1), 1, 1, 0));
+            level5Trades.add(new MapTrade(Items.EMERALD, 15, Items.COMPASS, 1, JStructures.ILlAGER_BUNKER.getStructure(), MapDecoration.Type.BANNER_LIGHT_GRAY, 12, 5));
         }
     }
 }
