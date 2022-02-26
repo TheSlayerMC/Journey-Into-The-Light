2c5a1987ad1fad7d6d2cf67c5279ed12c562709e
The_Slayer <brock.kerley@hotmail.com>
2022-02-26 18:35:19 +1030

    started knowledge tags

--- src/main/java/net/jitl/client/eventhandler/KnowledgeLevelEvent.java
+++ src/main/java/net/jitl/client/eventhandler/KnowledgeLevelEvent.java
@@ -4,7 +4,7 @@ import net.jitl.common.capability.player.JPlayer;
 import net.jitl.common.capability.player.data.Knowledge;
 import net.jitl.common.helper.EnumKnowledgeType;
 import net.jitl.core.JITL;
-import net.jitl.core.init.JEntities;
+import net.jitl.core.init.JTags;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
@@ -22,19 +22,7 @@ public class KnowledgeLevelEvent {
     EntityType.PIG, EntityType.PUFFERFISH, EntityType.RABBIT, EntityType.RAVAGER, EntityType.SALMON, EntityType.SHEEP, EntityType.SKELETON, EntityType.SILVERFISH, EntityType.SKELETON_HORSE,
     EntityType.SLIME, EntityType.SNOW_GOLEM, EntityType.SPIDER, EntityType.SQUID, EntityType.STRAY, EntityType.STRIDER, EntityType.TRADER_LLAMA, EntityType.TROPICAL_FISH,
     EntityType.TURTLE, EntityType.VILLAGER, EntityType.WANDERING_TRADER, EntityType.IRON_GOLEM, EntityType.WITCH, EntityType.WOLF, EntityType.WOLF, EntityType.ZOMBIE,
-    EntityType.ZOMBIE_HORSE, EntityType.ZOMBIE_VILLAGER, JEntities.FLORO_TYPE, JEntities.HONGO_TYPE, JEntities.BROWN_HONGO_TYPE, JEntities.MINI_BOOM_TYPE, JEntities.WITHERSHROOM_TYPE,
-            JEntities.HONGLOW_TYPE, JEntities.TOWER_GUARDIAN_TYPE, JEntities.GLUMP_TYPE, JEntities.ILLAGER_MECH_TYPE};
-
-    private static final EntityType<?>[] NETHER = {EntityType.WITHER_SKELETON, EntityType.WITHER, EntityType.ZOMBIFIED_PIGLIN, EntityType.PIGLIN, EntityType.PIGLIN_BRUTE, EntityType.BLAZE,
-            EntityType.GHAST, EntityType.MAGMA_CUBE, JEntities.WITHERSPINE_TYPE, JEntities.SOUL_WATCHER_TYPE, JEntities.MINI_GHAST_TYPE};
-
-    private static final EntityType<?>[] END = {EntityType.ENDER_DRAGON, EntityType.ENDERMAN, EntityType.ENDERMITE, EntityType.END_CRYSTAL};
-
-    private static final EntityType<?>[] EUCA = {JEntities.EUCA_HOPPER_TYPE, JEntities.GOLD_BOT_TYPE, JEntities.EUCA_CHARGER_TYPE, JEntities.DYNASTER_TYPE, JEntities.GOLDER_TYPE,
-            JEntities.SHIMMERER_TYPE};
-
-    private static final EntityType<?>[] FROZEN = {JEntities.ESKIMO_TYPE, JEntities.FROZEN_TROLL_TYPE, JEntities.PHANTASM_TYPE, JEntities.SHIVERING_RAM_TYPE, JEntities.CAPYBARA_TYPE,
-            JEntities.SHATTERER_TYPE};
+    EntityType.ZOMBIE_HORSE, EntityType.ZOMBIE_VILLAGER};//add these to overworld knowledge tag
 
     @SubscribeEvent
     public static void gainXP(LivingDeathEvent event) {
@@ -42,36 +30,32 @@ public class KnowledgeLevelEvent {
         float amount = r.nextFloat(1) + 0.5F;
         if(event.getSource().getEntity() instanceof Player p) {
             JPlayer player = JPlayer.from(p);
+            EntityType<?> type = event.getEntityLiving().getType();
             Knowledge knowledge = player.knowledge;
-            if(player != null) {
-               for(EntityType<?> type : OVERWORLD) {
-                   if(type == event.getEntityLiving().getType()) {
-                       knowledge.addXP(EnumKnowledgeType.OVERWORLD, amount);
-                   }
+
+            if(player != null && type != null) {
+                if(type.is(JTags.OVERWORLD_MOBS)) {
+                    knowledge.addXP(EnumKnowledgeType.OVERWORLD, amount);
                 }
 
-                for(EntityType<?> type : NETHER) {
-                    if(type == event.getEntityLiving().getType()) {
-                        knowledge.addXP(EnumKnowledgeType.NETHER, amount);
-                    }
+                if(type.is(JTags.NETHER_MOBS)) {
+                    knowledge.addXP(EnumKnowledgeType.NETHER, amount);
                 }
 
-                for(EntityType<?> type : END) {
-                    if(type == event.getEntityLiving().getType()) {
-                        knowledge.addXP(EnumKnowledgeType.END, amount);
-                    }
+                if(type.is(JTags.END_MOBS)) {
+                    knowledge.addXP(EnumKnowledgeType.END, amount);
                 }
 
-                for(EntityType<?> type : EUCA) {
-                    if(type == event.getEntityLiving().getType()) {
-                        knowledge.addXP(EnumKnowledgeType.EUCA, amount);
-                    }
+                if(type.is(JTags.EUCA_MOBS)) {
+                    knowledge.addXP(EnumKnowledgeType.EUCA, amount);
                 }
 
-                for(EntityType<?> type : FROZEN) {
-                    if(type == event.getEntityLiving().getType()) {
-                        knowledge.addXP(EnumKnowledgeType.FROZEN, amount);
-                    }
+                if(type.is(JTags.FROZEN_MOBS)) {
+                    knowledge.addXP(EnumKnowledgeType.FROZEN, amount);
+                }
+
+                if(type.is(JTags.BOIL_MOBS)) {
+                    knowledge.addXP(EnumKnowledgeType.BOIL, amount);
                 }
                 player.detectAndSendChanges();
             }
--- src/main/java/net/jitl/core/init/JTags.java
+++ src/main/java/net/jitl/core/init/JTags.java
@@ -1,20 +1,31 @@
 package net.jitl.core.init;
 
 import net.jitl.core.JITL;
-import net.minecraft.tags.BlockTags;
-import net.minecraft.tags.ItemTags;
-import net.minecraft.tags.SetTag;
-import net.minecraft.tags.Tag;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.*;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
 public class JTags {
+
+    protected static final StaticTagHelper<EntityType<?>> HELPER = StaticTags.create(Registry.ENTITY_TYPE_REGISTRY, "tags/knowledge");
+
     public static final SetTag.Named<Item> FROZEN_TROLL_LOVED_ITEMS = tagItem("frozen_troll_loved_items");
     public static final SetTag.Named<Block> EUCA_STONE_ORE_REPLACEABLES = tagBlock("euca_stone_ore_replaceables");
     public static final SetTag.Named<Block> BOIL_STONE_ORE_REPLACEABLES = tagBlock("boil_stone_ore_replaceables");
     public static final SetTag.Named<Block> FROZEN_STONE_ORE_REPLACEABLES = tagBlock("frozen_stone_ore_replaceables");
-
     public static final SetTag.Named<Block> JLOGS = tagBlock("jlogs");
+
+    public static final SetTag.Named<EntityType<?>> OVERWORLD_MOBS = tagKnowledge("overworld");
+    public static final SetTag.Named<EntityType<?>> END_MOBS = tagKnowledge("end");
+    public static final SetTag.Named<EntityType<?>> NETHER_MOBS = tagKnowledge("nether");
+    public static final SetTag.Named<EntityType<?>> EUCA_MOBS = tagKnowledge("euca");
+    public static final SetTag.Named<EntityType<?>> FROZEN_MOBS = tagKnowledge("frozen");
+    public static final SetTag.Named<EntityType<?>> BOIL_MOBS = tagKnowledge("boil");
+
 
     private static Tag.Named<Item> tagItem(String name) {
         return ItemTags.createOptional(JITL.rl(name));
@@ -24,4 +35,15 @@ public class JTags {
         return BlockTags.createOptional(JITL.rl(name));
     }
 
+    private static Tag.Named<EntityType<?>> tagKnowledge(String name) {
+        return createOptional(JITL.rl(name));
+    }
+
+    public static net.minecraftforge.common.Tags.IOptionalNamedTag<EntityType<?>> createOptional(ResourceLocation name) {
+        return HELPER.createOptional(name, null);
+    }
+
+    private static <T> ResourceKey<Registry<T>> createRegistryKey(String registryName) {
+        return ResourceKey.createRegistryKey(new ResourceLocation(registryName));
+    }
 }
