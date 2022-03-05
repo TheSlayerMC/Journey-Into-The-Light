050449246d24e51ea5977557dcee8fda5cdb8931
The_Slayer <brock.kerley@hotmail.com>
2022-03-05 15:40:31 +1030

    Update JChestTER.java

--- src/main/java/net/jitl/client/render/tile/JChestTER.java
+++ src/main/java/net/jitl/client/render/tile/JChestTER.java
@@ -4,6 +4,10 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Vector3f;
 import net.jitl.client.render.JModelLayers;
+import net.jitl.common.block.base.JChestBlock;
+import net.jitl.common.tile.JChestBlockEntity;
+import net.jitl.core.JITL;
+import net.jitl.core.init.JBlocks;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.CubeListBuilder;
@@ -12,44 +16,35 @@ import net.minecraft.client.model.geom.builders.MeshDefinition;
 import net.minecraft.client.model.geom.builders.PartDefinition;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.Sheets;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.blockentity.BrightnessCombiner;
 import net.minecraft.client.resources.model.Material;
 import net.minecraft.core.Direction;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DoubleBlockCombiner;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.ChestBlockEntity;
 import net.minecraft.world.level.block.entity.LidBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.ChestType;
 
-import java.util.Calendar;
-
 public class JChestTER <T extends BlockEntity & LidBlockEntity> implements BlockEntityRenderer<T> {
 
-    private static final String BOTTOM = "bottom";
-    private static final String LID = "lid";
-    private static final String LOCK = "lock";
-    private final ModelPart lid;
-    private final ModelPart bottom;
-    private final ModelPart lock;
-    private final ModelPart doubleLeftLid;
-    private final ModelPart doubleLeftBottom;
-    private final ModelPart doubleLeftLock;
-    private final ModelPart doubleRightLid;
-    private final ModelPart doubleRightBottom;
-    private final ModelPart doubleRightLock;
-    private boolean xmasTextures;
+    public static final ResourceLocation CHEST_SHEET = new ResourceLocation("textures/atlas/chest.png");
+
+    protected final ModelPart lid;
+    protected final ModelPart bottom;
+    protected final ModelPart lock;
+    protected final ModelPart doubleLeftLid;
+    protected final ModelPart doubleLeftBottom;
+    protected final ModelPart doubleLeftLock;
+    protected final ModelPart doubleRightLid;
+    protected final ModelPart doubleRightBottom;
+    protected final ModelPart doubleRightLock;
 
     public JChestTER(BlockEntityRendererProvider.Context context) {
-        Calendar calendar = Calendar.getInstance();
-        if (calendar.get(2) + 1 == 12 && calendar.get(5) >= 24 && calendar.get(5) <= 26) {
-            this.xmasTextures = true;
-        }
-
         ModelPart modelpart = context.bakeLayer(JModelLayers.JCHEST);
         this.bottom = modelpart.getChild("bottom");
         this.lid = modelpart.getChild("lid");
@@ -91,32 +86,31 @@ public class JChestTER <T extends BlockEntity & LidBlockEntity> implements Block
         return LayerDefinition.create(meshdefinition, 64, 64);
     }
 
+    @Override
     public void render(T blockEntity, float partialTick, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight, int packedOverlay) {
         Level level = blockEntity.getLevel();
         boolean flag = level != null;
-        BlockState blockstate = flag ? blockEntity.getBlockState() : Blocks.CHEST.defaultBlockState().setValue(ChestBlock.FACING, Direction.SOUTH);
-        ChestType chesttype = blockstate.hasProperty(ChestBlock.TYPE) ? blockstate.getValue(ChestBlock.TYPE) : ChestType.SINGLE;
+        BlockState blockstate = flag ? blockEntity.getBlockState() : JBlocks.EUCA_CHEST.defaultBlockState().setValue(JChestBlock.FACING, Direction.SOUTH);
+        ChestType chesttype = blockstate.hasProperty(JChestBlock.TYPE) ? blockstate.getValue(JChestBlock.TYPE) : ChestType.SINGLE;
         Block block = blockstate.getBlock();
-        if (block instanceof AbstractChestBlock) {
-            AbstractChestBlock<?> abstractchestblock = (AbstractChestBlock)block;
+            JChestBlock chest = (JChestBlock)block;
             boolean flag1 = chesttype != ChestType.SINGLE;
             poseStack.pushPose();
-            float f = blockstate.getValue(ChestBlock.FACING).toYRot();
+            float f = blockstate.getValue(JChestBlock.FACING).toYRot();
             poseStack.translate(0.5D, 0.5D, 0.5D);
             poseStack.mulPose(Vector3f.YP.rotationDegrees(-f));
             poseStack.translate(-0.5D, -0.5D, -0.5D);
-            DoubleBlockCombiner.NeighborCombineResult<? extends ChestBlockEntity> neighborcombineresult;
+            DoubleBlockCombiner.NeighborCombineResult<? extends JChestBlockEntity> neighborcombineresult;
             if (flag) {
-                neighborcombineresult = abstractchestblock.combine(blockstate, level, blockEntity.getBlockPos(), true);
+                neighborcombineresult = chest.combine(blockstate, level, blockEntity.getBlockPos(), true);
             } else {
                 neighborcombineresult = DoubleBlockCombiner.Combiner::acceptNone;
             }
-
-            float f1 = neighborcombineresult.apply(ChestBlock.opennessCombiner(blockEntity)).get(partialTick);
+            float f1 = neighborcombineresult.apply(JChestBlock.opennessCombiner(blockEntity)).get(partialTick);
             f1 = 1.0F - f1;
             f1 = 1.0F - f1 * f1 * f1;
             int i = neighborcombineresult.apply(new BrightnessCombiner<>()).applyAsInt(packedLight);
-            Material material = this.getMaterial(blockEntity, chesttype);
+            Material material = this.getMaterial(blockEntity, chesttype, getNameFromBlock(chest));
             VertexConsumer vertexconsumer = material.buffer(bufferSource, RenderType::entityCutout);
             if (flag1) {
                 if (chesttype == ChestType.LEFT) {
@@ -127,9 +121,15 @@ public class JChestTER <T extends BlockEntity & LidBlockEntity> implements Block
             } else {
                 this.render(poseStack, vertexconsumer, this.lid, this.lock, this.bottom, f1, i, packedOverlay);
             }
-
             poseStack.popPose();
+    }
+
+    public String getNameFromBlock(JChestBlock chest) {
+        String name = "";
+        if(chest == JBlocks.EUCA_CHEST) {
+            name = "euca_chest";
         }
+        return name;
     }
 
     private void render(PoseStack poseStack, VertexConsumer consumer, ModelPart lidPart, ModelPart lockPart, ModelPart bottomPart, float lidAngle, int packedLight, int packedOverlay) {
@@ -140,7 +140,7 @@ public class JChestTER <T extends BlockEntity & LidBlockEntity> implements Block
         bottomPart.render(poseStack, consumer, packedLight, packedOverlay);
     }
 
-    protected Material getMaterial(T blockEntity, ChestType chestType) {
-        return Sheets.chooseMaterial(blockEntity, chestType, this.xmasTextures);
+    protected Material getMaterial(T blockEntity, ChestType chestType, String name) {
+        return new Material(CHEST_SHEET, JITL.rl("textures/block/chest/" + name));
     }
 }
